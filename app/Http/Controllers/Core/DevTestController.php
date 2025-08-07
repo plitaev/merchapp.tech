a1cf285d06da9bd52afaf9444ab49db627c430ff
Evgenii Plita <evgeniiplita@gmail.com>
2025-08-07 13:38:29 +0700

    add in composer

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -3,14 +3,20 @@ namespace App\Http\Controllers\Core;
 use App\Http\Controllers\Controller;
 use App\Models\Core\TelegramWebhook;
 
+use Telegram\Bot\Api;
+
 class DevTestController extends Controller
 {
     public function devtest() {
-        $res = TelegramWebhook::orderBy('created_at')->skip(0)->take(5)->get();
-        foreach ($res as $data) {
-            $res = TelegramWebhook::orderBy('created_at')->skip(0)->take(2)->get();
-        }
-
-        return $res;
+        $telegram = new Api("7248655346:AAGLhHiGz4za7SsZKQ-Q47C5Nfvn5QFD4Is");
+        return $telegram->sendInvoice([
+            'provider_token' => '390540012:LIVE:75160',
+            'chat_id' => 247632034,
+            'title' => 'Тестовый продукт',
+            'description' => 'Описание продукта',
+            'payload' => 'payload_text',
+            'currency' => 'RUB',
+            'prices' => 3000
+        ]);
     }
 }
