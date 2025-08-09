a9bab11082381ae2d13fa2b702114321ab0b0df9
Evgenii Plita <evgeniiplita@gmail.com>
2025-08-09 23:19:33 +0700

    TelegramCabinet

--- app/Actions/Core/Telegram/TelegramChatJoinRequest.php
+++ app/Actions/Core/Telegram/TelegramChatJoinRequest.php
@@ -15,8 +15,18 @@ class TelegramChatJoinRequest
         $telegramChatJoinRequestCreate = new TelegramChatJoinRequestCreate();
 
         if (isset($json['chat_join_request']['chat']['id']) && isset($json['chat_join_request']['user_chat_id'])) {
+
+            $first_name = (isset($webhook['chat_join_request']['invite_link']['creator']['first_name'])?$webhook['chat_join_request']['invite_link']['creator']['first_name']:'none');
+            $last_name = (isset($webhook['chat_join_request']['invite_link']['creator']['last_name'])?$webhook['chat_join_request']['invite_link']['creator']['last_name']:'none');
+            $username = (isset($webhook['chat_join_request']['invite_link']['creator']['username'])?$webhook['chat_join_request']['invite_link']['creator']['username']:'none');
+            $language_code = (isset($webhook['chat_join_request']['invite_link']['creator']['language_code'])?$webhook['chat_join_request']['invite_link']['creator']['language_code']:'no');
+
+            \App\Models\Core\BotUser::updateOrCreate(
+                ['telegram_chat_id' => $json['chat_join_request']['user_chat_id'], 'bot_id' => $bot_id],
+                ['first_name' => $first_name, 'last_name' => $last_name, 'username' => $username, 'language_code' => $language_code]
+            );
+
             $bot_user = $botUserGetFromTelegram->handle($bot_id, $json['chat_join_request']['user_chat_id']);
-            return $bot_id." | ".$json['chat_join_request']['user_chat_id'];
 
             if ($bot_user) {
 
