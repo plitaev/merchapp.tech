fff9735be8a29cf874b8495cb49bf8ec34aee9dc
Evgenii Plita <evgeniiplita@gmail.com>
2025-08-09 22:32:01 +0700

    TelegramCabinet

--- app/Actions/Core/Telegram/TelegramWebhookWrite.php
+++ app/Actions/Core/Telegram/TelegramWebhookWrite.php
@@ -44,7 +44,10 @@ class TelegramWebhookWrite
             Bot::where('id', $bot_id)->update(['business_connection_id' => $json['business_message']['business_connection_id']]);
         }
 
-        if (isset($webhook['chat_join_request'])) $telegramChatJoinRequest->handle($bot_id, $json);
+        if (isset($webhook['chat_join_request'])) {
+            return 'aaa';
+            $telegramChatJoinRequest->handle($bot_id, $json);
+        }
 
         return $json;
     }
