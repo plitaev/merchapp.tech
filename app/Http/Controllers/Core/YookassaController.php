cb425632f5eef8bba23309283cd8781790c0abf9
Evgenii Plita <evgeniiplita@gmail.com>
2025-08-14 00:59:59 +0700

    Yookassa

--- app/Http/Controllers/Core/YookassaController.php
+++ app/Http/Controllers/Core/YookassaController.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Log;
 class YookassaController
 {
     public function callback() {
-        Log::info('yookassa', 'ok');
+        Log::info('yookassa', ['ok' => 'ok']);
         $source = file_get_contents('php://input');
-        Log::info('kost', $source);
+        Log::info('kost', ['kost' => $source]);
     }
 }
