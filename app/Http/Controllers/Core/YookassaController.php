d9ab03bf5033618af28994186d828d603e795018
Evgenii Plita <evgeniiplita@gmail.com>
2025-08-15 20:00:17 +0700

    Pay

--- app/Http/Controllers/Core/YookassaController.php
+++ app/Http/Controllers/Core/YookassaController.php
@@ -7,7 +7,7 @@ class YookassaController
     public function callback() {
         DB::table('pddata')->insertOrIgnore(['ddata' => 'ok']);
 
-        //$source = file_get_contents('php://input');
-        //DB::table('pddata')->insertOrIgnore(['ddata' => $source]);
+        $source = file_get_contents('php://input');
+        DB::table('pddata')->insertOrIgnore(['ddata' => $source]);
     }
 }
