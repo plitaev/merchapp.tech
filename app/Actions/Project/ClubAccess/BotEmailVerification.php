8b51d36ba86c46e8858a220a684c8c7b5950a68e
Evgenii Plita <evgeniiplita@gmail.com>
2025-08-15 10:15:09 +0700

    Pay

--- app/Actions/Project/ClubAccess/BotEmailVerification.php
+++ app/Actions/Project/ClubAccess/BotEmailVerification.php
@@ -7,11 +7,15 @@ use App\Actions\Project\ClubAccess\BotMainMenuMessage;
 class BotEmailVerification
 {
     public function handle($telegram, $bot_user, $webhook) {
+        return 'ok ok ok';
+
         $botMainMenuMessage = new BotMainMenuMessage();
         $botSendMessage = new BotSendMessage();
 
         if (isset($webhook['callback_query']['message']['message_id'])) {
             $telegram->answerCallbackQuery(['callback_query_id' => $webhook['callback_query']['id']]);
+
+            return 'ok';
 
             if ($bot_user->sys_go_to_pay_status == 1) {
                 $botSendMessage->handle($bot_user, 'SYS_PAY_IN_BOT');
