0aaba2bfeb648378670173ea38ec57248df56963
Evgenii Plita <evgeniiplita@gmail.com>
2025-08-15 11:14:08 +0700

    Pay

--- routes/core/pay.php
+++ routes/core/pay.php
@@ -3,5 +3,5 @@ use App\Http\Controllers\Core\PayController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(YookassaController::class)->group(function() {
-    Route::post('/pay/create/{pay_system_alias}/{product_id}', 'create');
+    Route::get('/pay/create/{pay_system_alias}/{product_id}', 'create');
 });
