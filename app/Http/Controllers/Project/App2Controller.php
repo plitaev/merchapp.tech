8286b26e40479163655cab11c22c82dee64e36e0
Evgenii Plita <evgeniiplita@gmail.com>
2025-08-17 21:07:25 +0700

    GoToStart

--- app/Http/Controllers/Project/App2Controller.php
+++ app/Http/Controllers/Project/App2Controller.php
@@ -10,8 +10,8 @@ class App2Controller extends Controller
     public function app2(int $no_rdr = 0) {
         $miniAppBannerListByClassID = new MiniAppBannerListByClassID();
         return view('project.app2.app2', [
-            'banners_big' => $miniAppBannerListByClassID->handle(2, 1),
-            'banners_medium' => $miniAppBannerListByClassID->handle(2, 2),
+            'banners_big' => $miniAppBannerListByClassID->handle(5, 1),
+            'banners_medium' => $miniAppBannerListByClassID->handle(5, 2),
             'no_rdr' => $no_rdr
         ]);
     }
