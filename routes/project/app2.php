ffe7bf972f3cd0b24c51a27ba86fb559bfa8c570
Evgenii Plita <evgeniiplita@gmail.com>
2025-08-18 22:09:06 +0700

    PDFApp

--- routes/project/app2.php
+++ routes/project/app2.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\Project\App2Controller;
 
 Route::controller(App2Controller::class)->group(function() {
     Route::get('/app2/{nordr?}', 'app2');
-    Route::post('/app2/pdf_rights_check', 'pdf_rights_check');
+    Route::post('/pdf/rights_check', 'pdf_rights_check');
 });
