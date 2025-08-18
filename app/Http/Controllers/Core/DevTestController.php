2958407cc7862fc6d2f9a388706f01f2c9b367a9
Evgenii Plita <evgeniiplita@gmail.com>
2025-08-18 17:16:32 +0700

    Recurrent

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -20,13 +20,17 @@ class DevTestController extends Controller
         $client = new Client();
         $client->setAuth($bot->yookassa_shop_id, $bot->yookassa_shop_secret);
 
+        $products = [];
+        $products[]=["description" => $product->name, "quantity" => "1.00", "amount" => ["value" => $product->price, "currency" => "RUB", "vat_code" => "1", "payment_mode" => "full_payment", "payment_subject" => "service"]];
+
         $payment = $client->createPayment(
             array(
                 'amount' => array(
-                    'value' => 100,
+                    'value' => $product->price,
                     'currency' => 'RUB',
                 ),
                 'capture' => true,
+                'receipt' => array('customer' => array('full_name' => (isset($bot_user->first_name)?$bot_user->first_name:'').' '.(isset($bot_user->last_name)?$bot_user->last_name:''), 'email' => $bot_user->email), 'items' => $products),
                 'payment_method_id' => '30350d4f-000f-5000-b000-11da1ea9d023',
                 'description' => 'Заказ №90',
             ),
