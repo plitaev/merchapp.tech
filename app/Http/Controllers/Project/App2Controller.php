9351c92b51596798e1bfb6349159c936d5c278d1
Evgenii Plita <evgeniiplita@gmail.com>
2025-08-18 21:18:35 +0700

    JS

--- app/Http/Controllers/Project/App2Controller.php
+++ app/Http/Controllers/Project/App2Controller.php
@@ -8,6 +8,8 @@ use App\Actions\Core\MiniAppBanner\MiniAppBannerListByClassID;
 class App2Controller extends Controller
 {
     public function app2(int $no_rdr = 0) {
+        return $_SERVER['HTTP_HOST'];
+
         $miniAppBannerListByClassID = new MiniAppBannerListByClassID();
         return view('project.app2.app2', [
             'banners_big' => $miniAppBannerListByClassID->handle(5, 1),
