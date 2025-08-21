e7e80ad1016b7e51fb8ff982b69c2c2bcb62a438
Evgenii Plita <evgeniiplita@gmail.com>
2025-08-21 20:35:06 +0700

    App2Engine

--- resources/views/project/app2/all.blade.php
+++ resources/views/project/app2/all.blade.php
@@ -15,13 +15,14 @@
     <script src="https://telegram.org/js/telegram-web-app.js?56"></script>
 
     <script>
-        window.onload = function() {
+
+        $(document).ready(function() {
             let telegram = window.Telegram.WebApp;
             telegram.ready();
 
             let first_name = telegram.initDataUnsafe.user.first_name;
             if (first_name!="undefined") document.getElementById('username').innerHTML = "😎 "+first_name;
-        };
+        });
     </script>
 
     @hasSection('head') @yield('head') @endif
