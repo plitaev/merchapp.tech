94f7450f797fa4ecd0dbd8e946503716576d4621
Evgenii Plita <evgeniiplita@gmail.com>
2025-08-24 22:42:29 +0700

    BotUserGetByEmail Fix

--- app/Actions/Core/BotUser/BotUserGetByEmail.php
+++ app/Actions/Core/BotUser/BotUserGetByEmail.php
@@ -1,11 +1,13 @@
 <?php
-
 namespace App\Actions\Core\BotUser;
+
+use App\Models\Core\BotUser;
 
 class BotUserGetByEmail
 {
-    public function handle(string $email) {
-        return \App\Models\Core\BotUser::select('telegram_chat_id', 'first_name', 'last_name', 'hand_name', 'username', 'email', 'listen_email_status')
+    public function handle(int $bot_id, string $email) {
+        return BotUser::select('telegram_chat_id', 'first_name', 'last_name', 'hand_name', 'username', 'email', 'listen_email_status')
+            ->where('bot_id', $bot_id)
             ->where('email', $email)
             ->first();
     }
