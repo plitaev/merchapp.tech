2f19b2e0a0797cf5d18fee90159642043312f4fa
Evgenii Plita <evgeniiplita@gmail.com>
2025-08-25 23:19:33 +0700

    Mailing

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -21,8 +21,8 @@ class DevTestController extends Controller
     public function devtest() {
         $botSendMessage = new BotSendMessage();
 
-        $bot_users = BotUser::whereNull('date_end')->get();
-        return $bot_users;
+        $bot_users = BotUser::whereNull('date_end')->skip(0)->take(30)->get();
+
         foreach ($bot_users as $bot_user) {
             $botSendMessage->handle($bot_user, 'PROJECT_MAILING_1');
         }
