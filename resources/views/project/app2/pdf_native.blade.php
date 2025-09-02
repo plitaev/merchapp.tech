1977768f501fb47bbdec1604c0031d8f520b7812
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-02 16:04:42 +0700

    iframe fix

--- resources/views/project/app2/pdf_native.blade.php
+++ resources/views/project/app2/pdf_native.blade.php
@@ -22,9 +22,8 @@
 
     </script>
 
-    <iframe
-        src="{{env("APP_URL")}}/content/miniapp_pdf/{{$pdf}}.pdf"
-        style="width: 100%; height: 100vh; overflow-y: scroll; -webkit-overflow-scrolling: touch"/>
-    </iframe>
+    <div style="position: relative; width: 100%; padding-top: 56.25%;">
+        <iframe src="{{env("APP_URL")}}/content/miniapp_pdf/{{$pdf}}.pdf" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
+    </div>
 
 @endsection
