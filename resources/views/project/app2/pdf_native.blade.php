2a8e6ca0929f79e9d54bbd29c66d48f100df047c
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-02 16:17:07 +0700

    iframe fix

--- resources/views/project/app2/pdf_native.blade.php
+++ resources/views/project/app2/pdf_native.blade.php
@@ -22,7 +22,7 @@
 
     </script>
 
-    <div style="position: relative; width: 100%; padding-top: 56.25%;">
+    <div style="position: relative; width: 100%; height: 100vh">
         <iframe src="{{env("APP_URL")}}/content/miniapp_pdf/{{$pdf}}.pdf" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
     </div>
 
