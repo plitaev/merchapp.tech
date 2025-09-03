bd06dbe2da34b09803404c92683a77859a9e31c8
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-03 12:57:38 +0700

    MainApp

--- app/Http/Controllers/Project/App1Controller.php
+++ app/Http/Controllers/Project/App1Controller.php
@@ -10,8 +10,8 @@ class App1Controller extends Controller
     public function app1() {
         $miniAppBannerListByClassID = new MiniAppBannerListByClassID();
         return view('project.app1.app1', [
-            'banners_big' => $miniAppBannerListByClassID->handle(1, 1),
-            'banners_small' => $miniAppBannerListByClassID->handle(1, 3)
+            'banners_big' => $miniAppBannerListByClassID->handle(6, 1),
+            'banners_small' => $miniAppBannerListByClassID->handle(6, 3)
         ]);
     }
 
