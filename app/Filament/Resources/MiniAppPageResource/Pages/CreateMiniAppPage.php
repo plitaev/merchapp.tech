a1febb6bce8cce4e0d369d945a4dbc56f8bb1b8b
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-03 17:55:25 +0700

    MiniAppUpdate

--- app/Filament/Resources/MiniAppPageResource/Pages/CreateMiniAppPage.php
+++ app/Filament/Resources/MiniAppPageResource/Pages/CreateMiniAppPage.php
@@ -3,10 +3,17 @@
 namespace App\Filament\Resources\MiniAppPageResource\Pages;
 
 use App\Filament\Resources\MiniAppPageResource;
+use App\Models\Core\MiniAppPage;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateMiniAppPage extends CreateRecord
 {
     protected static string $resource = MiniAppPageResource::class;
+
+    public function mount(): void
+    {
+        $data['url'] = md5(time());
+        $this->form->fill($data);
+    }
 }
