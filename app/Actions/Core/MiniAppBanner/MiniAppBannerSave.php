cbf81a24291b9970e6c24c8f95bd63bfa528618f
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-03 14:04:53 +0700

    EditBannerFix

--- app/Actions/Core/MiniAppBanner/MiniAppBannerSave.php
+++ app/Actions/Core/MiniAppBanner/MiniAppBannerSave.php
@@ -7,18 +7,18 @@ use App\Models\Core\MiniAppBannerLinkPage;
 class MiniAppBannerSave
 {
     public function handle(array $data) {
-        /*
-        $posres = MiniAppBannerLinkPage::select('mini_app_banner_id', 'pos')->where('mini_app_page_id', $data['mini_app_page_id'])->where('pos', '>=', $data['pos'])->orderBy('pos')->get();
-        $pos = $data['pos'];
-        foreach ($posres as $posdata) {
-            $pos = $pos + 1;
-            MiniAppBanner::where('id', $posdata->id)->update(['pos' => $pos]);
-        }
-        */
 
         if ($data["id"] > 0) {
             \App\Models\Core\MiniAppBanner::where('id', $data["id"])->update($data);
         } else {
+
+            $posres = MiniAppBannerLinkPage::select('mini_app_banner_id', 'pos')->where('mini_app_page_id', $data['mini_app_page_id'])->where('pos', '>=', $data['pos'])->orderBy('pos')->get();
+            $pos = $data['pos'];
+            foreach ($posres as $posdata) {
+                $pos = $pos + 1;
+                MiniAppBanner::where('id', $posdata->id)->update(['pos' => $pos]);
+            }
+
             $databanner = $data;
 
             unset($databanner['mini_app_page_id']);
