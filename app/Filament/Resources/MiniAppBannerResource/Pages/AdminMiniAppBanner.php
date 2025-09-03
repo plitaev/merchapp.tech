9a07b06620a9e167b640d15603981f4076ecbb81
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-03 13:44:32 +0700

    EditBannerFix

--- app/Filament/Resources/MiniAppBannerResource/Pages/AdminMiniAppBanner.php
+++ app/Filament/Resources/MiniAppBannerResource/Pages/AdminMiniAppBanner.php
@@ -95,6 +95,8 @@ class AdminMiniAppBanner extends Page implements HasForms, HasTable
                 ->options(MiniAppPage::all()->pluck('name', 'id'))
                 ->searchable()
                 ->columns(['sm' => 2, 'xl' => 2, '2xl' => 2]);
+        } else {
+            $A[] = Hidden::make('mini_app_page_id');
         }
 
         $A[] = Forms\Components\Select::make('banner_class_id')
