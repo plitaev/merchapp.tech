f4f505fbdbf7974108028d1e7eb704b2becb395a
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-08 13:47:46 +0700

    TelegramSendMessageSchedule

--- app/Models/Core/TelegramSendMessageSchedule.php
+++ app/Models/Core/TelegramSendMessageSchedule.php
@@ -9,7 +9,6 @@ class TelegramSendMessageSchedule extends Model
     protected $fillable = [
         'sending_id',
         'bot_user_id',
-        'chat_id',
         'run_status',
         'send_status',
         'message_id'
