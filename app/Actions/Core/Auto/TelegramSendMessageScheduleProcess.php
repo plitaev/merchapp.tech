60c7bff71f06a6630ca0b2a9c31175d35f094ff5
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-08 15:46:28 +0700

    SendMessageScheduler

--- app/Actions/Core/Auto/TelegramSendMessageScheduleProcess.php
+++ app/Actions/Core/Auto/TelegramSendMessageScheduleProcess.php
@@ -20,6 +20,7 @@ class TelegramSendMessageScheduleProcess
             ->get();
 
         foreach ($res as $data) {
+            return $data->bot_message->bot_message_appointment;
             return $botSendMessage->handle($data->bot_user, $data->bot_message->bot_message_appointment);
         }
 
