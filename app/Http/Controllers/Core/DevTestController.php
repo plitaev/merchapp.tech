41494bbabd64c419af5541be7eb04c967d38f36c
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-09 14:46:15 +0700

    Gurfinkel

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -21,17 +21,41 @@ use App\Models\Core\TelegramChatJoinRequestLog;
 class DevTestController extends Controller
 {
     public function devtest() {
+        $telegram = new Api('8307593800:AAF5THy4VstNdji3U4oF01zRBfKuA74QC9E');
+
         $res = BotUser::whereNotNull('email')->get();
+
+        $members = [];
+        $others = [];
 
         foreach ($res as $data) {
 
+            $status = $telegram->getChatMember(['chat_id' => -1001288663452, 'user_id' => $data->telegram_chat_id]);
+            $status = $status->status;
+
+            if ($status == 'member') {
+                $members[] = $data->id;
+            } else {
+                $others[] = $data->id;
+            }
+
+            /*
             TelegramSendMessageSchedule::create(
                 [
                     'sending_id' => 2,
                     'bot_user_id' => $data->id
                 ]
             );
+            */
+        }
 
+        foreach ($others as $bot_user_id) {
+            TelegramSendMessageSchedule::create(
+                [
+                    'sending_id' => 5,
+                    'bot_user_id' => $bot_user_id
+                ]
+            );
         }
 
 
