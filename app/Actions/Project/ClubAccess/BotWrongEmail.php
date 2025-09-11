0fbd7f05416d50b4441b1bbc9df3199aa775f9db
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-11 19:44:41 +0700

    listener

--- app/Actions/Project/ClubAccess/BotWrongEmail.php
+++ app/Actions/Project/ClubAccess/BotWrongEmail.php
@@ -11,7 +11,7 @@ class BotWrongEmail
         $botSendMessage = new BotSendMessage();
         $botUserSetListener = new BotUserSetListener();
 
-        if ($bot_user->sys_wrong_email_first_status == 1) {
+        if ($bot_user->sys_email_pay_not_found_first_status == 1) {
             $botSendMessage->handle($bot_user, 'SYS_EMAIL_PAY_NOT_FOUND_SECOND');
         } else {
             $botSendMessage->handle($bot_user, 'SYS_EMAIL_PAY_NOT_FOUND_FIRST');
