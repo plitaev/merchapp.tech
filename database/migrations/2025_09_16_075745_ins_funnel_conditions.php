960f1d13ea262591b44bc22eef6d191d5ea8c74b
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-16 12:02:39 +0700

    Funnels

--- database/migrations/2025_09_16_075745_ins_funnel_conditions.php
+++ database/migrations/2025_09_16_075745_ins_funnel_conditions.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use App\Models\Core\Funnel;
 use App\Models\Core\FunnelCondition;
 
 return new class extends Migration
@@ -14,12 +13,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Funnel::create(['name' => 'Уведомление за 3 дня до списания рекуррента', 'alias' => 'notification_recurrent_on_3_days']);
-        Funnel::create(['name' => 'Уведомление за 1 день до списания рекуррента', 'alias' => 'notification_recurrent_on_1_days']);
-        Funnel::create(['name' => 'Уведомление за 3 дня без рекуррента', 'alias' => 'notification_recurent_off_3_days']);
-        Funnel::create(['name' => 'Уведомление за 1 дня без рекуррента', 'alias' => 'notification_recurrent_off_1_days']);
-        Funnel::create(['name' => 'Рекуррент успешно списан', 'alias' => 'notification_recurrent_success_payment']);
-        Funnel::create(['name' => 'Рекуррент не удалось списать', 'alias' => 'recurrent_fail']);
+        FunnelCondition::create(['name' => 'Уведомление за 3 дня до списания рекуррента', 'alias' => 'notification_recurrent_on_3_days']);
+        FunnelCondition::create(['name' => 'Уведомление за 1 день до списания рекуррента', 'alias' => 'notification_recurrent_on_1_days']);
+        FunnelCondition::create(['name' => 'Уведомление за 3 дня без рекуррента', 'alias' => 'notification_recurent_off_3_days']);
+        FunnelCondition::create(['name' => 'Уведомление за 1 дня без рекуррента', 'alias' => 'notification_recurrent_off_1_days']);
+        FunnelCondition::create(['name' => 'Рекуррент успешно списан', 'alias' => 'notification_recurrent_success_payment']);
+        FunnelCondition::create(['name' => 'Рекуррент не удалось списать', 'alias' => 'recurrent_fail']);
     }
 
     /**
