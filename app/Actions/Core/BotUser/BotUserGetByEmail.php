8826ae093e1264cbac7b82702be52ed75efc2f60
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-16 19:13:39 +0700

    getcourse_event_webhooks

--- app/Actions/Core/BotUser/BotUserGetByEmail.php
+++ app/Actions/Core/BotUser/BotUserGetByEmail.php
@@ -6,8 +6,7 @@ use App\Models\Core\BotUser;
 class BotUserGetByEmail
 {
     public function handle(int $bot_id, string $email) {
-        return BotUser::select('telegram_chat_id', 'first_name', 'last_name', 'hand_name', 'username', 'email', 'listen_email_status')
-            ->where('bot_id', $bot_id)
+        return BotUser::where('bot_id', $bot_id)
             ->where('email', $email)
             ->first();
     }
