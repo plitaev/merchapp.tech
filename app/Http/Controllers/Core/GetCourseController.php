159c4b606f720871da3993e2abdc85c7e2b1531e
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-16 19:09:47 +0700

    getcourse_event_webhooks

--- app/Http/Controllers/Core/GetCourseController.php
+++ app/Http/Controllers/Core/GetCourseController.php
@@ -35,8 +35,7 @@ class GetCourseController extends Controller
                 ->first();
 
             if ($bot_message) {
-                return $bot_message->bot_message_appointment->alias;
-                $botSendMessage->handle($bot_user, $bot_message->bot_message_appointment->alias);
+                return $botSendMessage->handle($bot_user, $bot_message->bot_message_appointment->alias);
             }
 
         }
