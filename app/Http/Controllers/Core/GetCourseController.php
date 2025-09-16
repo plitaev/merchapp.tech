221cdcd611f5007d46bba8863735f2a3dbf58176
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-16 15:34:00 +0700

    getcourse_event_webhooks

--- app/Http/Controllers/Core/GetCourseController.php
+++ app/Http/Controllers/Core/GetCourseController.php
@@ -1,11 +1,7 @@
 <?php
-
 namespace App\Http\Controllers\Core;
-
-use App\Http\Controllers\Controller;
-
-use App\Actions\Core\GetCourseEventWebhook\GetCourseEventWebhookCreate;
 use App\Actions\Core\GetCourseWebhook\GetCourseWebhookCreate;
+use App\Http\Controllers\Controller;
 
 class GetCourseController extends Controller
 {
@@ -13,10 +9,4 @@ class GetCourseController extends Controller
         $getCourseWebhookCreate = new GetCourseWebhookCreate();
         $getCourseWebhookCreate->handle($product_id, $getcourse_user_id, $getcourse_user_name, $email, $is_recurrent, $recurrent_status);
     }
-
-    public function getcourse_event_webhooks(int $getcourse_id, string $name, string $email, string $event) {
-        $getCourseEventWebhookCreate = new GetCourseEventWebhookCreate();
-        $getCourseEventWebhookCreate->handle($getcourse_id, $name, $email, $event);
-    }
-
 }
