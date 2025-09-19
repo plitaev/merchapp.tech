73ee2bfead4363fed4a9eed245c7e9be652d8a9d
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-19 11:49:35 +0700

    RecurrentScheduler

---
+++ database/migrations/2025_09_19_073459_bot_user_recurrent_schedules.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bot_user_recurrent_schedules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('bot_user_id');
+            $table->unsignedBigInteger('prevous_pay_id');
+            $table->unsignedBigInteger('new_pay_id')->nullable();
+            $table->datetime('recurrent_datetime');
+            $table->boolean('run_status');
+            $table->json('pay_system_responce', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
