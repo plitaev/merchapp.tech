6797136d31400e7e4e90464ec8c1524746cad125
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-19 16:51:05 +0700

    ReccurentScheduler

--- app/Actions/Core/Auto/BotUserRecurrentSchedulerProcess.php
+++ app/Actions/Core/Auto/BotUserRecurrentSchedulerProcess.php
@@ -10,8 +10,9 @@ class BotUserRecurrentSchedulerProcess
 {
     public function handle() {
         $res = BotUserRecurrentSchedule::with('prevous_pay:id,pay_system_payment_method_id')
-            ->with('bot:id,yookassa_shop_id,yookassa_shop_secret,price')
-            ->with('bot_user')
+            ->with('bot')
+            ->with('bot_user:id,telegram_chat_id')
+            ->select('bot_user_recurrent_schedules.id', 'prevous_pay_id', 'bot_user_id')
             ->where('recurrent_datetime', '<=', date('Y-m-d H:i:s', time()))
             ->where('run_status', 0)
             ->get();
