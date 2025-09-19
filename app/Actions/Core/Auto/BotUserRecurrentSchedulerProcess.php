87d87350b90f0c8a1b71eb69558f4a7ebca350c3
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-19 16:35:49 +0700

    ReccurentScheduler

--- app/Actions/Core/Auto/BotUserRecurrentSchedulerProcess.php
+++ app/Actions/Core/Auto/BotUserRecurrentSchedulerProcess.php
@@ -13,7 +13,10 @@ class BotUserRecurrentSchedulerProcess
             ->with('bot:id,yookassa_shop_id,yookassa_shop_secret,price')
             ->with('bot_user:id,telegram_chat_id')
             ->where('recurrent_datetime', '<=', date('Y-m-d H:i:s', time()))
-            ->where('run_status', 0)->get();
+            ->where('run_status', 0)
+            ->get();
+
+        return $res;
 
         foreach ($res as $data) {
             $client = new Client();
