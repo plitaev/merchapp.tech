60c5c9eb3a08d3cd19aaf64d5e59f2e45d7dc0fa
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-22 17:06:20 +0700

    Funnels

--- app/Actions/Core/Auto/BotSetFunnels.php
+++ app/Actions/Core/Auto/BotSetFunnels.php
@@ -15,10 +15,8 @@ class BotSetFunnels
             ->select('id', 'funnel_condition_id', 'funnel_condition_trigger_id', 'funnel_days', 'funnel_hours', 'funnel_minutes')
             ->whereNotNull('funnel_condition_id')
             ->whereNotNull('funnel_condition_trigger_id')
-            ->where('id', 15)
+            ->where('id', 18)
             ->get();
-
-        return $res;
 
         foreach ($res as $data) {
             return $funnelUserBan->handle($data);
