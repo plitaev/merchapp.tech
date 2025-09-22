91c371c4a874efb918bf1a4b0b3fb45e2e9ea8c1
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-22 16:25:59 +0700

    Funnels

--- app/Actions/Core/Auto/BotSetFunnels.php
+++ app/Actions/Core/Auto/BotSetFunnels.php
@@ -7,7 +7,9 @@ use App\Models\Core\BotMessage;
 class BotSetFunnels
 {
     public function handle() {
-        $res = BotMessage::with('funnel_condition')->with('funnel_condition_trigger')->whereNotNull('funnel_condition_id')->whereNotNull('funnel_condition_trigger_id')->get();
+        $res = BotMessage::with('funnel_condition:id,alias')->with('funnel_condition_trigger:id,alias')
+            ->select('id', 'funnel_condition_id', 'funnel_condition_trigger_id')
+            ->whereNotNull('funnel_condition_id')->whereNotNull('funnel_condition_trigger_id')->get();
         return $res;
     }
 }
