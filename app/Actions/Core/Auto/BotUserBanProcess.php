004fa9019f8bd60e34718bb57026121972996e77
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-23 21:11:47 +0700

    StopBan

--- app/Actions/Core/Auto/BotUserBanProcess.php
+++ app/Actions/Core/Auto/BotUserBanProcess.php
@@ -32,7 +32,7 @@ class BotUserBanProcess
 
             if (isset($supergroups[$ban->bot->id])) {
                 foreach ($supergroups[$ban->bot->id] as $supergroup) {
-
+                    /*
                     try {
                         $status = $telegram->banChatMember(['chat_id' => $supergroup, 'user_id' => $ban->bot_user->telegram_chat_id]);
                         TelegramBanScheduleLogs::create(['bot_user_id' => $ban->bot_user->id, 'chat_id' => $supergroup, 'user_id' =>$ban->bot_user->telegram_chat_id, 'status' => $status]);
@@ -42,6 +42,7 @@ class BotUserBanProcess
                     } catch (\Exception $exception) {
                         TelegramBanScheduleErrorLogs::create(['bot_user_id' => $ban->bot_user->id, 'chat_id' => $supergroup, 'user_id' =>$ban->bot_user->telegram_chat_id, 'text' => $exception]);
                     }
+                    */
 
                 }
             }
