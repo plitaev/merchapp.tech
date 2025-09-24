4dd9f39d1e6c8f550c9a64d800cdf497545eb49c
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-24 18:04:04 +0700

    BotSupergroupAdmin

---
+++ app/Models/Core/SupergroupDeleteParameter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Core;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SupergroupDeleteParameter extends Model
+{
+    protected $fillable = [
+        'name',
+        'alias'
+    ];
+}
