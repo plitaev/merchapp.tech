5399b2b19cf25fbf623a0d0520181c911eae9093
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-25 15:15:44 +0700

    BanScheduler

--- app/Actions/Core/Telegram/TelegramBanRun.php
+++ app/Actions/Core/Telegram/TelegramBanRun.php
@@ -3,6 +3,7 @@
 namespace App\Actions\Core\Telegram;
 
 use App\Models\Core\BotUser;
+use App\Models\Core\BotUserUnbanSchedule;
 use App\Models\Core\TelegramBanScheduleErrorLogs;
 use App\Models\Core\TelegramBanScheduleLogs;
 
@@ -14,6 +15,7 @@ class TelegramBanRun
             TelegramBanScheduleLogs::create(['bot_user_id' => $ban->bot_user->id, 'chat_id' => $supergroup->telegram_id, 'user_id' =>$ban->bot_user->telegram_chat_id, 'status' => $status]);
 
             BotUser::where('id', $ban->bot_user_id)->update(['ban' => 1, 'unban' => 0]);
+            BotUserUnbanSchedule::where('bot_user_id', $ban->bot_user_id)->where('run_status', 0)->update(['run_status' => 3]);
 
         } catch (\Exception $exception) {
             TelegramBanScheduleErrorLogs::create(['bot_user_id' => $ban->bot_user->id, 'chat_id' => $supergroup->telegram_id, 'user_id' =>$ban->bot_user->telegram_chat_id, 'text' => $exception]);
