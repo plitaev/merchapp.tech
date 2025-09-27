ab8fa1a27269ef68e5a581f828feadad7193216c
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-09-27 09:52:50 +0700

    Платежные системы

--- app/Filament/Resources/BotResource/Pages/BotPaySystemAdmin.php
+++ app/Filament/Resources/BotResource/Pages/BotPaySystemAdmin.php
@@ -38,63 +38,6 @@ class BotPaySystemAdmin extends Page implements HasForms
 
     public string $name;
 
-    public array $hours = [
-        '0' => '0',
-        '1' => '1',
-        '2' => '2',
-        '3' => '3',
-        '4' => '4',
-        '5' => '5',
-        '6' => '6',
-        '7' => '7',
-        '8' => '8',
-        '9' => '9',
-        '10' => '10',
-        '11' => '11',
-        '12' => '12',
-        '13' => '13',
-        '14' => '14',
-        '15' => '15',
-        '16' => '16',
-        '17' => '17',
-        '18' => '18',
-        '19' => '19',
-        '20' => '20',
-        '21' => '21',
-        '22' => '22',
-        '23' => '23',
-        '24' => '24',
-        '25' => '25',
-        '26' => '26',
-        '27' => '27',
-        '28' => '28',
-        '29' => '29',
-        '30' => '30',
-        '31' => '31',
-        '32' => '32',
-        '33' => '33',
-        '34' => '34',
-        '35' => '35',
-        '36' => '36',
-        '37' => '37',
-        '38' => '38',
-        '39' => '39',
-        '40' => '40',
-        '41' => '41',
-        '42' => '42',
-        '43' => '43',
-        '44' => '44',
-        '45' => '45',
-        '46' => '46'
-    ];
-
-    public array $minutes = [
-        '0' => '0',
-        '15' => '15',
-        '30' => '30',
-        '45' => '45'
-    ];
-
     public function getRecord(): ?Model
     {
         return Bot::class;
@@ -140,8 +83,8 @@ class BotPaySystemAdmin extends Page implements HasForms
     {
         return $form
             ->schema([
-                Section::make('Основные параметры')
-                    ->description('Укажите основные настройки и заполните все поля')
+                Section::make('Юкасса')
+                    ->description('Настройки платежной системы Юкасса')
                     ->columns([
                         'sm' => 2,
                         'md' => 2,
@@ -151,10 +94,10 @@ class BotPaySystemAdmin extends Page implements HasForms
                     ])
                     ->schema([
                         Forms\Components\TextInput::make('yookassa_shop_id')
-                            ->label('Юкасса ShopID')
+                            ->label('ID магазина (ShopID)')
                             ->maxLength(255),
                         Forms\Components\TextInput::make('yookassa_shop_secret')
-                            ->label('Юкасса ShopSecret')
+                            ->label('Секретный ключ API (ShopSecret)')
                             ->maxLength(255),
                     ]),
                 Actions::make([
