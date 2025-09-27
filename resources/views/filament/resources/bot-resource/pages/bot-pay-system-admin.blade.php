c53ab67d25fbe92b91456b367192b7c5b8290a9b
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-09-27 09:15:58 +0700

    Платежные системы

--- resources/views/filament/resources/bot-resource/pages/bot-pay-system-admin.blade.php
+++ resources/views/filament/resources/bot-resource/pages/bot-pay-system-admin.blade.php
@@ -1,6 +1,6 @@
 <x-filament-panels::page>
 
-    @include('filament.resources.bot-resource.pages.navigation-bot', ['category' => "pay-systems", 'bot_id' => $this->record])
+    @include('filament.resources.bot-resource.pages.navigation-bot', ['category' => "pay-system-admin", 'bot_id' => $this->record])
     {{$this->form}}
-    
+
 </x-filament-panels::page>
