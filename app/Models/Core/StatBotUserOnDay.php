d3fa674c687d5fc851448c85a7dbfea581b3d728
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-09-28 17:10:19 +0700

    AutoController(скрипт для stat_bot_user_on_day)

---
+++ app/Models/Core/StatBotUserOnDay.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Core;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StatBotUserOnDay extends Model
+{
+    use HasFactory;
+
+    protected $table='stat_bot_user_on_day';
+
+    protected $fillable = [
+        'bot_id',
+        'bot_user_count'
+    ];
+}
