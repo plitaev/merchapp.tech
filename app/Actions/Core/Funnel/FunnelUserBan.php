37cb3728e3a2a36b9552f064e496cd6d0ece6d20
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-29 19:36:28 +0700

    Funnels

--- app/Actions/Core/Funnel/FunnelUserBan.php
+++ app/Actions/Core/Funnel/FunnelUserBan.php
@@ -11,19 +11,21 @@ class FunnelUserBan
         if ($data->funnel_condition->alias == "user_ban") {
 
             if ($data->funnel_condition_trigger->alias == "now") {
-                $datetime_start = date('Y-m-d', time())." 00:00:00";
-                $datetime_end = date('Y-m-d', time())." 23:59:59";
+                $date = date('Y-m-d', time());
+                $datetime = $date."".$data->bot->ban_time;
             }
 
             if ($data->funnel_condition_trigger->alias == "after") {
-                $next_date = Carbon::now();
+                $prevous_date = Carbon::now();
 
-                if ($data->funnel_days && $data->funnel_days > 0) $next_date->subDays($data->funnel_days);
-                if ($data->funnel_hours && $data->funnel_hours > 0) $next_date->subHours($data->funnel_hours);
-                if ($data->funnel_minutes && $data->funnel_minutes > 0) $next_date->subMinutes($data->funnel_hours);
+                if ($data->funnel_days && $data->funnel_days > 0) $prevous_date->subDays($data->funnel_days);
+                if ($data->funnel_hours && $data->funnel_hours > 0) $prevous_date->subHours($data->funnel_hours);
+                if ($data->funnel_minutes && $data->funnel_minutes > 0) $prevous_date->subMinutes($data->funnel_hours);
 
-                $datetime_start = $next_date." 00:00:00";
-                $datetime_end = $next_date." 23:59:59";
+                $date = $prevous_date->format('Y-m-d');
+                $datetime = $prevous_date->format('Y-m-d H:i:s');
+
+                return $date." | ".$datetime;
             }
 
         }
