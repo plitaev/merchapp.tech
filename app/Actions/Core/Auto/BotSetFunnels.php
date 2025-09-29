1fab21ccc1a6b78441c49d66fd19dbc3dd30dc2a
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-29 19:32:04 +0700

    Funnels

--- app/Actions/Core/Auto/BotSetFunnels.php
+++ app/Actions/Core/Auto/BotSetFunnels.php
@@ -11,8 +11,8 @@ class BotSetFunnels
     public function handle() {
         $funnelUserBan = new FunnelUserBan();
 
-        $res = BotMessage::with('funnel_condition:id,alias')->with('funnel_condition_trigger:id,alias')
-            ->select('id', 'funnel_condition_id', 'funnel_condition_trigger_id', 'funnel_days', 'funnel_hours', 'funnel_minutes')
+        $res = BotMessage::with('funnel_condition:id,alias')->with('funnel_condition_trigger:id,alias')->with('bot:id,ban_time')
+            ->select('id', 'funnel_condition_id', 'funnel_condition_trigger_id', 'funnel_days', 'funnel_hours', 'funnel_minutes', 'bot_id')
             ->whereNotNull('funnel_condition_id')
             ->whereNotNull('funnel_condition_trigger_id')
             ->get();
