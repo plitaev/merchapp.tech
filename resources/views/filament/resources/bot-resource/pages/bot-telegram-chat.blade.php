c40aab04fdb4823e4f8d16f2a1c0ada34d872f86
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-09-30 21:04:46 +0700

    AutoController(скрипт для stat_bot_user_on_day)

--- resources/views/filament/resources/bot-resource/pages/bot-telegram-chat.blade.php
+++ resources/views/filament/resources/bot-resource/pages/bot-telegram-chat.blade.php
@@ -1,3 +1,14 @@
 <x-filament-panels::page>
     {{$this->form}}
+    <div class="fi-header flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
+        <div class="flex shrink-0 items-center gap-3">
+            <x-filament::modal id="add-page-modal">
+                <x-slot name="trigger">
+                    <x-filament::button>Добавить сообщение</x-filament::button>
+                </x-slot>
+
+                <form wire:submit.prevent="submit">{{$this->form_user_link_message}}</form>
+            </x-filament::modal>
+        </div>
+    </div>
 </x-filament-panels::page>
