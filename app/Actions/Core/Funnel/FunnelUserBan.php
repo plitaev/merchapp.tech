d52b1b1258b58819b76def73abc9b43461630bb3
Evgenii Plita <evgeniiplita@gmail.com>
2025-09-30 13:46:46 +0700

    Funnels

--- app/Actions/Core/Funnel/FunnelUserBan.php
+++ app/Actions/Core/Funnel/FunnelUserBan.php
@@ -42,7 +42,7 @@ class FunnelUserBan
                 $datetime = $next_date->format('Y-m-d H:i:s');
             }
 
-            return $date;
+            return $data->funnel_condition_trigger->alias." || ".$date;
             $bot_users = BotUser::where('bot_id', $data->bot->id)->where('date_end', $date)->get();
             return $bot_users;
 
