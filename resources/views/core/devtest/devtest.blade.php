f92261a590e5052f697921d400398c4efdb90b2c
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-03 18:11:02 +0700

    devtest

--- resources/views/core/devtest/devtest.blade.php
+++ resources/views/core/devtest/devtest.blade.php
@@ -1,10 +1,10 @@
 <table>
     @foreach ($webhooks as $webhook)
         <tr>
-            <td>{{$webhooks->getcourse_id}}</td>
-            <td>{{$webhooks->name}}</td>
-            <td>{{$webhooks->email}}</td>
-            <td>{{date('d.m.Y', strtotime($webhooks->created_at))}}</td>
+            <td>{{$webhook->getcourse_id}}</td>
+            <td>{{$webhook->name}}</td>
+            <td>{{$webhook->email}}</td>
+            <td>{{date('d.m.Y', strtotime($webhook->created_at))}}</td>
         </tr>
     @endforeach
 </table>
