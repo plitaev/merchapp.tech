2cae00c870d89116e0fe65dc97e1fc2f0e25c69f
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-04 13:59:31 +0700

    BotSendingAdmin

--- app/Actions/Core/BotSendingAdmin/BotSendingAdminDeleteRecord.php
+++ app/Actions/Core/BotSendingAdmin/BotSendingAdminDeleteRecord.php
@@ -12,7 +12,7 @@ class BotSendingAdminDeleteRecord
     public function handle($record, $action) {
         $check = TelegramSendMessageSchedule::where('sending_id', $record->id)->where('run_status', '>', 0)->count();
         if ($check) {
-            TelegramSendMessageSchedule::where('sending_id', $record->id)->update(['run_status' => 3]);
+            TelegramSendMessageSchedule::where('sending_id', $record->id)->where('run_status', 0)->update(['run_status' => 3]);
 
             Notification::make()
                 ->title('Рассылка уже доставлена пользователям, удаление невозможно. Неотправленные сообщения будут отменены.')
