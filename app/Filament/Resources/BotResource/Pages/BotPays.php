b5b38b01685921f93c5cd98a657e74ab30ef5a4e
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-05 10:42:09 +0700

    PaySortByDate

--- app/Filament/Resources/BotResource/Pages/BotPays.php
+++ app/Filament/Resources/BotResource/Pages/BotPays.php
@@ -12,6 +12,7 @@ use Filament\Tables;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder
 
 class BotPays extends Page implements HasTable
 {
