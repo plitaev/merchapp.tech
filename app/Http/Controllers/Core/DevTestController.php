6cce864382472855753076e7377089d920d94b42
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-05 21:28:07 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -24,14 +24,20 @@ use App\Models\Core\GetcourseWebhook;
 class DevTestController extends Controller
 {
     public function devtest() {
+        $result = [];
+
         $telegram = new Api('7427797340:AAEZd2WfiGalZ7EvAdRv2yCNkgTDwM7nVhY');
 
         $res = BotUser::where('date_end', '<=', '2025-10-04')->where('ban', 0)->get();
 
         foreach ($res as $data) {
             $status = $telegram->getChatMember(['chat_id' => -1002225281436, 'user_id' => $data->telegram_chat_id]);
-            return $status;
+            if ($status->status == 'member') {
+                $result[] = $data->telegram_chat_id;
+            }
 
         }
+
+        return $result;
     }
 }
