2f688a3aefc1c69f0ddb90912e9b18a680c62bd5
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-06 14:35:26 +0700

    BanFix

---
+++ database/migrations/2025_10_06_103404_update_bot_user_ban_schedules.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bot_user_ban_schedules', function (Blueprint $table) {
+            $table->removeIndex('bot_user_id');
+            $table->removeIndex('ban_datetime');
+
+            $table->unique(['bot_user_id', 'ban_datetime']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
