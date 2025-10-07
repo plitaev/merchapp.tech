4797cc7b5f00bd78abcf4021292fe5b531611abe
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-07 21:38:07 +0700

    UpgradeToFilamentV4

--- app/Filament/Resources/BotResource/Pages/BotNavigationMenu.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Filament\Resources\BotResource\Pages;
-
-class BotNavigationMenu
-{
-    public array $bot_navigation_menu = [1 => 'Основные', 2 => 'Сообщения', 3 => 'Подписчики', 4 => 'Супергруппы', 5 => 'Платежи', 6 => 'Платежи в ожидании'];
-}
