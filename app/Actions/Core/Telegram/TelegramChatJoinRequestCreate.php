fe7584a02d7d2a47241799aad960fbec850ac226
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-07 10:45:44 +0700

    decline_chat_join_request

--- app/Actions/Core/Telegram/TelegramChatJoinRequestCreate.php
+++ app/Actions/Core/Telegram/TelegramChatJoinRequestCreate.php
@@ -4,15 +4,17 @@ namespace App\Actions\Core\Telegram;
 use Telegram\Bot\Api;
 
 use App\Models\Core\Bot;
+use App\Models\Core\TelegramSupergroup;
 use App\Models\Core\TelegramChatJoinRequestLog;
 
 class TelegramChatJoinRequestCreate
 {
     public function handle(int $bot_id, $json, int $status) {
         $bot = Bot::select('telegram_token')->where('id', $bot_id)->first();
+        $supergroup = TelegramSupergroup::where('telegram_id', $json['chat_join_request']['chat']['id'])->first();
         $telegram = new Api($bot->telegram_token);
 
-        if ($status === 1) {
+        if ($status === 1 && $supergroup && $supergroup->decline_chat_join_request == 0) {
             $result = $telegram->approveChatJoinRequest(['chat_id' => $json['chat_join_request']['chat']['id'], 'user_id' => $json['chat_join_request']['user_chat_id']]);
         } else {
             $result = $telegram->declineChatJoinRequest(['chat_id' => $json['chat_join_request']['chat']['id'], 'user_id' => $json['chat_join_request']['user_chat_id']]);
