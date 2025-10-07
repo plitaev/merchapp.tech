d3679f1685bfa4719bfcce7292aae4b43b591834
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-07 22:03:02 +0700

    UpgradeToFilamentV4

--- app/Models/Core/TelegramSendMessageSchedule.php
+++ app/Models/Core/TelegramSendMessageSchedule.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 use App\Models\Core\BotMessage;
 use App\Models\Core\BotUser;
+use App\Models\Core\RunStatus;
 use App\Models\Core\Sending;
 
 class TelegramSendMessageSchedule extends Model
@@ -26,6 +27,11 @@ class TelegramSendMessageSchedule extends Model
     public function sending(): BelongsTo
     {
         return $this->belongsTo(Sending::class);
+    }
+
+    public function run_status_name(): BelongsTo
+    {
+        return $this->belongsTo(RunStatus::class);
     }
 
     public function bot_message(): HasOneThrough {
