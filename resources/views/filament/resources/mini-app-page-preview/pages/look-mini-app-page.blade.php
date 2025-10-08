5a5f3935e9a014af528849c871df50b4dc83d1b6
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-08 13:38:58 +0700

    UpgradeToFilamentV4

--- resources/views/filament/resources/mini-app-page-preview/pages/look-mini-app-page.blade.php
+++ resources/views/filament/resources/mini-app-page-preview/pages/look-mini-app-page.blade.php
@@ -19,5 +19,21 @@
         </div>
     </div>
 
+    <div class="fi-header fi-header-has-breadcrumbs">
+
+        <div class="fi-header-actions-ctn">
+            <div class="fi-ac fi-align-start">
+                <a href="/admin/mini-app-pages/{{$this->mini_app_page_id}}/admin" class="fi-color fi-color-primary fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
+                    Назад
+                </a>
+            </div>
+        </div>
+
+        <div>
+            <h1 class="fi-header-heading">Предпросмотр</h1>
+        </div>
+
+    </div>
+
     <iframe src="{{env("APP_URL")}}/{{$this->mini_app_page_url}}" class="appframe"></iframe>
 </x-filament::page>
