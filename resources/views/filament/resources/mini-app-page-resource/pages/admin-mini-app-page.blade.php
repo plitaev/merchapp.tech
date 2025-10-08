fe9748c196573c05b91cd18239ce8e6cc50981a8
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-08 13:35:53 +0700

    UpgradeToFilamentV4

--- resources/views/filament/resources/mini-app-page-resource/pages/admin-mini-app-page.blade.php
+++ resources/views/filament/resources/mini-app-page-resource/pages/admin-mini-app-page.blade.php
@@ -20,5 +20,22 @@
             </div>
     </div>
 
+    <div class="fi-header fi-header-has-breadcrumbs">
+        <div>
+            <h1 class="fi-header-heading">Баннеры</h1>
+        </div>
+
+        <div class="fi-header-actions-ctn">
+            <div class="fi-ac fi-align-start">
+                <a href="/admin/mini-app-banners/{{$record}}/0/admin" class="fi-color fi-color-primary fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
+                    Создать баннер
+                </a>
+                <a href="/admin/mini-app-pages/{{$record}}/preview" class="fi-color fi-color-primary fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
+                    Предварительный просмотр
+                </a>
+            </div>
+        </div>
+    </div>
+
     {{$this->table}}
 </x-filament::page>
