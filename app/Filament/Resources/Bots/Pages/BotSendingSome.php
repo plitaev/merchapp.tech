2dcbfb37f617464439b8300098a64476fdbfaa56
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-10-09 07:39:53 +0700

    отправить нескольким пользователям

--- app/Filament/Resources/Bots/Pages/BotSendingSome.php
+++ app/Filament/Resources/Bots/Pages/BotSendingSome.php
@@ -154,7 +154,7 @@ class BotSendingSome extends Page implements HasForms, HasTable, HasInfolists
                             if($email_mass) {
                                 foreach ($email_mass as $email) {
 
-                                    $bot_user_id = BotUser::where('email', $email)->fiirst();
+                                    $bot_user_id = BotUser::where('email', $email)->first();
 return $bot_user_id->id;
                                     if($bot_user_id) {
 
