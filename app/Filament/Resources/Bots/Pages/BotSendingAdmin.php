dd05fb19db4ce22139d965b2967cf12d8bbc9ff9
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-09 19:52:14 +0700

    YookassaJoinToAdmin

--- app/Filament/Resources/Bots/Pages/BotSendingAdmin.php
+++ app/Filament/Resources/Bots/Pages/BotSendingAdmin.php
@@ -291,7 +291,6 @@ class BotSendingAdmin extends Page implements HasForms, HasTable, HasInfolists
                                 return ['key' => $bot_user->id, 'value' => (isset($bot_user->first_name) && $bot_user->first_name!='none'?$bot_user->first_name:'')." ".(isset($bot_user->last_name) && $bot_user->last_name!='none'?$bot_user->last_name:'')." ".(isset($bot_user->username) && $bot_user->username!='none'?"(".$bot_user->username.")":'')];
                             })->pluck('value', 'key')->toArray())
                             ->searchable()
-
                     ]),
                 Actions::make([
                     Action::make('Сохранить')
