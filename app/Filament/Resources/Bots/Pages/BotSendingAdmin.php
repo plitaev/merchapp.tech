3ed9fc587a399e901c2ace12ddd1b28474d27c8b
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-10-09 20:23:43 +0700

    отправить нескольким пользователям

--- app/Filament/Resources/Bots/Pages/BotSendingAdmin.php
+++ app/Filament/Resources/Bots/Pages/BotSendingAdmin.php
@@ -290,8 +290,8 @@ class BotSendingAdmin extends Page implements HasForms, HasTable, HasInfolists
                             ->options(BotUser::where('bot_id', $this->bot_id)->get()->map(function ($bot_user) {
                                 return ['key' => $bot_user->id, 'value' => (isset($bot_user->first_name) && $bot_user->first_name!='none'?$bot_user->first_name:'')." ".(isset($bot_user->last_name) && $bot_user->last_name!='none'?$bot_user->last_name:'')." ".(isset($bot_user->username) && $bot_user->username!='none'?"(".$bot_user->username.")":'')];
                             })->pluck('value', 'key')->toArray())
-                           // ->relationship(name: 'bot_user_id', titleAttribute: 'name')
-                            //->searchable(['name', 'email'.'_'.'username']),
+                            ->relationship(name: 'bot_user_id', titleAttribute: 'name')
+                            ->searchable(['name', 'email'.'_'.'username']),
                     ]),
                 Actions::make([
                     Action::make('Сохранить')
