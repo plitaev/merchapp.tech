1fb4a63269392dd715189347123d581c65a463fa
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-10-09 20:02:35 +0700

    отправить нескольким пользователям

--- app/Filament/Resources/Bots/Pages/BotSendingAdmin.php
+++ app/Filament/Resources/Bots/Pages/BotSendingAdmin.php
@@ -288,9 +288,9 @@ class BotSendingAdmin extends Page implements HasForms, HasTable, HasInfolists
                                 'required' => 'Обязательно выберите пользователя',
                             ])
                             ->options(BotUser::where('bot_id', $this->bot_id)->get()->map(function ($bot_user) {
-                                return ['key' => $bot_user->id, 'value' => (isset($bot_user->first_name) && $bot_user->first_name!='none'?$bot_user->first_name:'')." ".(isset($bot_user->last_name) && $bot_user->last_name!='none'?$bot_user->last_name:'')." ".(isset($bot_user->username) && $bot_user->username!='none'?"(".$bot_user->username.")":'')];
+                                return ['key' => $bot_user->id, 'value' => (isset($bot_user->first_name) && $bot_user->first_name!='none'?$bot_user->first_name:'')." ".(isset($bot_user->last_name) && $bot_user->last_name!='none'?$bot_user->last_name:'')." ".(isset($bot_user->username) && $bot_user->username!='none'?"(".$bot_user->username.")":''), 'search' => $bot_user->email.'_'.$bot_user->username];
                             })->pluck('value', 'key')->toArray())
-                            ->searchable()
+                            ->searchable(['search']),
                     ]),
                 Actions::make([
                     Action::make('Сохранить')
