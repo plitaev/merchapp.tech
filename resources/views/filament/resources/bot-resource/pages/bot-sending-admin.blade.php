d0fa7e37520aaf5fd8543241be5e9427bd9f37c3
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-10-09 08:25:53 +0700

    отправить нескольким пользователям

--- resources/views/filament/resources/bot-resource/pages/bot-sending-admin.blade.php
+++ resources/views/filament/resources/bot-resource/pages/bot-sending-admin.blade.php
@@ -12,16 +12,15 @@
 
                     <form wire:submit.prevent="submit">{{$this->form_bot_user}}</form>
                 </x-filament::modal>
-            </div>
 
-            <div class="fi-header-actions-ctn">
-                <div class="fi-ac fi-align-start">
-                    <a href="/admin/bots/{{$this->bot_id}}/{{$this->id}}/sending-some" class="fi-color fi-color-primary fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
-                        Добавить несколько пользователей
-                    </a>
+                <div class="fi-header-actions-ctn">
+                    <div class="fi-ac fi-align-start">
+                        <a href="/admin/bots/{{$this->bot_id}}/{{$this->id}}/sending-some" class="fi-color fi-color-primary fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
+                            Добавить несколько пользователей
+                        </a>
+                    </div>
                 </div>
             </div>
-
         </div>
 
         {{$this->table}}
