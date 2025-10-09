cb099e750bf1fa67e19623da8c91661e0e949465
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-09 19:47:07 +0700

    YookassaJoinToAdmin

--- app/Filament/Resources/Bots/Pages/BotSendingAdmin.php
+++ app/Filament/Resources/Bots/Pages/BotSendingAdmin.php
@@ -281,6 +281,16 @@ class BotSendingAdmin extends Page implements HasForms, HasTable, HasInfolists
                     ->description('')
                     ->schema([
                         Hidden::make('sending_id'),
+                        Select::make('bot_user_id')
+                            ->label('Пользователь')
+                            ->required()
+                            ->validationMessages([
+                                'required' => 'Обязательно выберите пользователя',
+                            ])
+                            ->options(BotUser::where('bot_id', $this->bot_id)->get()->map(function ($bot_user) {
+                                return ['key' => $bot_user->id, 'value' => (isset($bot_user->first_name) && $bot_user->first_name!='none'?$bot_user->first_name:'')." ".(isset($bot_user->last_name) && $bot_user->last_name!='none'?$bot_user->last_name:'')." ".(isset($bot_user->username) && $bot_user->username!='none'?"(".$bot_user->username.")":'')];
+                            })->pluck('value', 'key')->toArray())
+                            ->searchable()
 
                     ]),
                 Actions::make([
