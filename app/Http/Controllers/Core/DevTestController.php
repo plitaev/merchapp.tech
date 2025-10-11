8911a4d46bc47f37bb2ca1888e400d6fd586e8e7
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-12 00:34:12 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -48,6 +48,7 @@ class DevTestController extends Controller
             ->where('bot_user_id', $bot_user->id)
             ->where('gift', 0)
             ->where('status', 1)
+            ->whereNot('id', 33090)
             ->get();
 
 
@@ -59,6 +60,7 @@ class DevTestController extends Controller
             ->where('gift_bot_user_id', $bot_user->id)
             ->where('gift', 1)
             ->where('status', 1)
+            ->whereNot('id', 33090)
             ->get();
 
         $alldays = [];
