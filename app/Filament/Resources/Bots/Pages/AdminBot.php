1a320835cb4e2dda8b28416a867134932701e4ab
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-10-11 19:52:49 +0700

    Перенос функционала кнопки Статус в основные настройки

--- app/Filament/Resources/Bots/Pages/AdminBot.php
+++ app/Filament/Resources/Bots/Pages/AdminBot.php
@@ -142,7 +142,7 @@ class AdminBot extends Page implements HasForms
         return $this->name;
     }
 
-    public function form(Schema $schema): Schema
+    public function form(Schema $schema, $telegramWebhookInfo): Schema
     {
         return $schema
             ->components([
@@ -192,7 +192,7 @@ class AdminBot extends Page implements HasForms
                         '2xl' => 2,
                     ])
                     ->schema([
-                        Text::make($this->telegramWebhookInfo)
+                        Text::make($telegramWebhookInfo)
                     ]),
                 Actions::make([
                     Action::make('Сохранить')
