fa37453265b20aef0fc3336e7c36ea24312b1090
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-11 23:31:22 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -34,7 +34,7 @@ class DevTestController extends Controller
     public function devtest() {
         return Carbon::parse('2025-08-12')->addDays(30);
 
-        $diff_days=Carbon::parse('2025-08-12')->startOfDay()->diffInDays('2025-09-05');
+        $diff_days=Carbon::parse('2025-08-12')->startOfDay()->diffInDays('2025-09-05')->format('Y-m-d');
         return $diff_days;
     }
 }
