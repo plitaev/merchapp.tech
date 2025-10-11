70a5df054be39f34890e8fc9bf06899b098bff5a
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-11 10:34:52 +0700

    GoogleController

--- app/Http/Controllers/Core/GoogleController.php
+++ app/Http/Controllers/Core/GoogleController.php
@@ -15,13 +15,15 @@ class GoogleController
 
         $res = BotUser::where('date_end', $date_end)->get();
         foreach ($res as $data) {
-            $data[] = [
+            $A = [
                 (isset($data->email)?$data->email:''),
                 ($data->first_name != 'none'?$data->first_name:''),
                 ($data->last_name != 'none'?$data->last_name:''),
                 date('d.m.Y', strtotime($date_end)),
                 ($data->username != 'none'?$data->username:''),
             ];
+
+            $data[] = $A;
         }
 
         Sheets::spreadsheet(config('google.post_spreadsheet_id'))->sheet($sheet_name)->append($data);
