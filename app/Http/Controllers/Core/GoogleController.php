e5b01a5be62a1d9a85da7a2574d800be16b395f9
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-11 10:32:36 +0700

    GoogleController

--- app/Http/Controllers/Core/GoogleController.php
+++ app/Http/Controllers/Core/GoogleController.php
@@ -1,10 +1,26 @@
 <?php
-
 namespace App\Http\Controllers\Core;
+use Revolution\Google\Sheets\Facades\Sheets;
 
 class GoogleController
 {
     public function send_banneds() {
-        return 'ok';
+        $date_end = '2025-10-10';
+        $sheet_name = 'Отписавшиеся пользователи (удалены из клуба)';
+
+        $data = [];
+
+        $res = BotUsers::where('date_end', $date_end)->get();
+        foreach ($res as $data) {
+            $data[] = [
+                (isset($data->email)?$data->email:''),
+                ($data->first_name != 'none'?$data->first_name:''),
+                ($data->last_name != 'none'?$data->last_name:''),
+                date('d.m.Y', strtotime($date_end)),
+                ($data->username != 'none'?$data->username:''),
+            ];
+        }
+
+        Sheets::spreadsheet(config('google.post_spreadsheet_id'))->sheet($sheet_name)->append($data);
     }
 }
