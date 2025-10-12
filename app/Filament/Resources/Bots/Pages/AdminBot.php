0a4817f3e5706237b51dad074b4a685c9c2ef103
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-10-12 19:25:35 +0700

    Перенос функционала кнопки Статус в основные настройки

--- app/Filament/Resources/Bots/Pages/AdminBot.php
+++ app/Filament/Resources/Bots/Pages/AdminBot.php
@@ -227,7 +227,8 @@ class AdminBot extends Page implements HasForms
                             $telegramWebhookInfo_temp = new TelegramWebhookInfo();
 
                             $telegramWebhookInfo= $telegramWebhookInfo_temp->handle($telegram_token, $telegram_webhook);
-                            $jsonString = json_encode($telegramWebhookInfo);
+                            $jsonString = json_encode($telegramWebhookInfo, true);
+                            echo $jsonString['components'];
                             return 'h'.$jsonString["components"];
 
                             Notification::make()
