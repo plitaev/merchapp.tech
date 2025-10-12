8ac2f918a802df715e7e777574dabdc96be9af5e
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-10-12 21:55:52 +0700

    Перенос функционала кнопки Статус в основные настройки

--- routes/core/telegram.php
+++ routes/core/telegram.php
@@ -7,6 +7,6 @@ Route::controller(TelegramController::class)->group(function() {
     Route::post('/telegram/get_webhook_info/{token}/{webhook}', 'get_webhook_info');
     Route::post('/telegram/set_webhook/{id}/{token}/{webhook}', 'set_webhook');
     Route::post('/telegram/delete_webhook/{token}/{webhook}', 'delete_webhook');
-    Route::get('/telegram/get_webhook_info/{token}/{webhook}', 'get_webhook_info');
+    //Route::get('/telegram/get_webhook_info/{token}/{webhook}', 'get_webhook_info');
 
 });
