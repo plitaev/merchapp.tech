4f0f07f61da1e7374a74f840ec98367e32443832
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-14 21:18:04 +0700

    BotBranches

--- app/Filament/Resources/Bots/Pages/BotBranches.php
+++ app/Filament/Resources/Bots/Pages/BotBranches.php
@@ -78,12 +78,12 @@ class BotBranches extends Page implements HasTable
                 //
             ])
             ->recordActions([
-                EditAction::make()->url(fn($record) => "/admin/bots/".$this->bot_id."/".$record->id."/branches"),
+                EditAction::make()->url(fn($record) => "/admin/bots/".$this->bot_id."/".$record->id."/bot-branch-admin"),
                 DeleteAction::make()
                     ->before(function (DeleteAction $action, Sending $record) {
                         $botSendingAdminDeleteRecord = new BotSendingAdminDeleteRecord();
                         $botSendingAdminDeleteRecord->handle($record, $action);
                     })
-            ])->recordUrl(fn($record) => "/admin/bots/".$this->bot_id."/".$record->id."/sending-admin");
+            ])->recordUrl(fn($record) => "/admin/bots/".$this->bot_id."/".$record->id."/bot-branch-admin");
     }
 }
