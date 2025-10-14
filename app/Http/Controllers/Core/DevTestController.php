a9ca1fc8ac541dbc26079410942211444b24baa8
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-14 14:23:31 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -32,75 +32,7 @@ use Carbon\Carbon;
 class DevTestController extends Controller
 {
     public function devtest() {
-
-        /*
-        return Carbon::parse('2025-09-05')->addDays(30)->format('Y-m-d');
-        $diff_days=Carbon::parse('2025-08-12')->startOfDay()->diffInDays('2025-09-05');
-        return $diff_days;
-        */
-
-        $bot_user = BotUser::find(2435);
-
-        $alldays1 = Pay::with('bot')
-            ->select('id', 'product_id')
-            ->whereHas('bot', function ($query) use ($bot_user) {
-                $query->where('bot_id', $bot_user->bot_id);
-            })
-            ->where('bot_user_id', $bot_user->id)
-            ->where('gift', 0)
-            ->where('status', 1)
-            ->get();
-
-
-        $alldays2 = Pay::with('bot')
-            ->select('id', 'product_id')
-            ->whereHas('bot', function ($query) use ($bot_user) {
-                $query->where('bot_id', $bot_user->bot_id);
-            })
-            ->where('gift_bot_user_id', $bot_user->id)
-            ->where('gift', 1)
-            ->where('status', 1)
-            ->get();
-
-        $alldays = [];
-        foreach ($alldays1 as $allday1) $alldays[] = $allday1->id;
-        foreach ($alldays2 as $allday2) $alldays[] = $allday2->id;
-
-        $alldays = Pay::select('days', 'created_at', 'updated_at')->whereIn('id', $alldays)->orderBy('created_at')->get();
-
-        $Adates_start=[];
-        $Adates_end=[];
-
-        foreach ($alldays as $allday) {
-
-            $Adates_start[]=$allday->created_at;
-            $Adates_end[]=$allday->created_at->addDays($allday->days);
-        }
-
-        $days_to_add=0;
-        $Adiffs_in_days = [];
-
-        foreach ($Adates_end as $k=>$date) {
-            $next_pos=$k+1;
-            if (isset($Adates_start[$next_pos])) {
-                if ($Adates_start[$next_pos] < $date) {
-                    $diff_days=$Adates_start[$next_pos]->startOfDay()->diffInDays($date);
-                    $Adiffs_in_days[]=$diff_days;
-                    if ($diff_days>0) {
-                        $days_to_add=$days_to_add+$diff_days;
-                        $Adates_end[$next_pos]=$Adates_end[$next_pos]->startOfDay()->addDay($diff_days);
-                    }
-                }
-            }
-        }
-
-        //return $Adiffs_in_days;
-
-        foreach ($Adates_end as $date) {
-            $date_end=$date;
-        }
-
-        return $date_end->format('Y-m-d');
-
+        $emails = BotUser::select('email')->whereNotNull('email')->pluck('email')->toArray();
+        return GetCourseWebhook::where('created_at', '>=', '2025-10-04 00:00:00')->whereNotIn('email', $emails)->get();
     }
 }
