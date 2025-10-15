b3845536fc307933124da44ffafb7fd3df6a1518
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-10-15 21:05:52 +0700

    кол-во сообщений

--- app/Filament/Resources/Bots/Pages/BotChatAdmin.php
+++ app/Filament/Resources/Bots/Pages/BotChatAdmin.php
@@ -188,10 +188,10 @@ class BotChatAdmin extends Page implements HasForms, HasTable, HasInfolists
             ])->statePath('data');
     }
 
-    public static function table(Table $table): Table
+    public function table(Table $table): Table
     {
         return $table
-            ->query(BotMessage::select('id as count')->where('bot_id',$bot_id))
+            ->query(BotMessage::select('id as count')->where('bot_id', $this->bot_id))
             ->columns([
                 TextColumn::make('name')
                     ->label('Сообщения от бота (кол-во записей сообщений в БД)?')
