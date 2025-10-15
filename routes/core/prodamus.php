bcec949477c2683f0b45e5dfe3a9e937e562820b
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-15 21:22:23 +0700

    BotBranches

--- routes/core/prodamus.php
+++ routes/core/prodamus.php
@@ -3,5 +3,5 @@ use App\Http\Controllers\Core\ProdamusController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(ProdamusController::class)->group(function() {
-    Route::post('/shop/callback_prodamus', 'callback_prodamus');
+    Route::post('/prodamus/callback', 'callback_prodamus');
 });
