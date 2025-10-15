ea3ecfe31f11a70abba8a4f9d139196ea8534815
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-15 21:28:17 +0700

    BotBranches

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
             '/telegram/*',
             '/pdf/*',
             '/yookassa/*',
+            '/prodamus/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
