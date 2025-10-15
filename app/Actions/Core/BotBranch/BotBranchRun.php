ec14fa91ea30cc3e34100563aa08daed976e9caf
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-15 16:35:12 +0700

    BotBranches

--- app/Actions/Core/BotBranch/BotBranchRun.php
+++ app/Actions/Core/BotBranch/BotBranchRun.php
@@ -36,7 +36,7 @@ class BotBranchRun
             $member = 0;
             $member_end = $bot_user->date_end." ".$bot_user->bot->ban_time;
 
-            if (Carbon::now() > Carbon::parse($member_end)) {
+            if (Carbon::now() < Carbon::parse($member_end)) {
                 return 'yes';
                 $member = 1;
             } else {
