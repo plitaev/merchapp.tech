404f5615dac22da7cc3fcf9eccb3fd38b00ca223
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-10-15 20:12:41 +0700

    кол-во сообщений

--- app/Filament/Resources/Bots/Pages/BotChatAdmin.php
+++ app/Filament/Resources/Bots/Pages/BotChatAdmin.php
@@ -187,7 +187,7 @@ class BotChatAdmin extends Page implements HasForms,HasTable, HasInfolists
             ->query(BotMessage::select('id as count')->where('bot_id',$this->bot_id))
             ->columns([
                 TextColumn::make('name')
-                    ->label('Сообщения от бота (кол-во записей сообщений в БД)')
+                    ->label('Сообщения от бота (кол-во записей сообщений в БД)?')
             ])
 
             ->filters([
