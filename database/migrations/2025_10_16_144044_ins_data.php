81069939db22f4438fd6828fba83436556420b3c
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-16 18:43:33 +0700

    BotBranches

---
+++ database/migrations/2025_10_16_144044_ins_data.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\Models\Core\BotMessageAppointment;
+use App\Models\Core\BotMessageButtonCallback;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        BotMessageAppointment::create(['name' => 'Оплата всех тарифов полностью', 'alias' => 'SYS_PAY_IN_BOT_ALL_TARIFFS']);
+        BotMessageButtonCallback::create(['name' => 'Оплата всех тарифов полностью', 'alias' => 'GoToFullTariffs']);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
