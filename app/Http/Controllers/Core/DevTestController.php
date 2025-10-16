3b7265ef5d807f7b6675c782fc5d932c4f7e1140
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-16 17:07:08 +0700

    BotBranches

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -34,7 +34,7 @@ class DevTestController extends Controller
 {
     public function devtest() {
 
-        $bot_user = BotUser::find(7874);
+        $bot_user = BotUser::find(10254);
         $botSendMessage = new BotSendMessage();
         $botSendMessage->handle($bot_user, 'MAGICLIFE_TEST_ACCESS_BUY_1');
 
