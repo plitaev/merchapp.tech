7187eecdf750bc6951a22e60560c52deae714f49
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-16 14:00:36 +0700

    BotBranches

--- database/migrations/2025_10_16_095459_ins_bot_message_button_types.php
+++ database/migrations/2025_10_16_095459_ins_bot_message_button_types.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use App\Models\Core\BotMessageButton;
+use App\Models\Core\BotMessageButtonType;
 
 return new class extends Migration
 {
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        BotMessageButton::create(['name' => 'Тариф']);
+        BotMessageButtonType::create(['name' => 'Тариф']);
     }
 
     /**
