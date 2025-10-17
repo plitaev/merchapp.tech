98fbe3aa620987f32ba3389a88fcc26992d0ea83
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-17 11:20:47 +0700

    BotBranches

--- app/Actions/Project/ClubAccess/BotGoToClub.php
+++ app/Actions/Project/ClubAccess/BotGoToClub.php
@@ -9,11 +9,11 @@ class BotGoToClub
         $botRequestAndConfirmEmail = new BotRequestAndConfirmEmail();
         $botUserSetListener = new BotUserSetListener();
 
-        $botUserSetListener->handle('sys_go_to_pay', 1, $bot_user->id);
-        $botRequestAndConfirmEmail->handle($bot_user);
-
         if (isset($webhook['callback_query']['message']['message_id'])) {
             $telegram->answerCallbackQuery(['callback_query_id' => $webhook['callback_query']['id']]);
         }
+
+        $botUserSetListener->handle('sys_go_to_pay', 1, $bot_user->id);
+        $botRequestAndConfirmEmail->handle($bot_user);
     }
 }
