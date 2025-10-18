99339e28abee4105e9fdfd01b7be5303e6081d6b
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-19 00:02:52 +0700

    AdminBot

--- resources/views/filament/resources/bot-resource/pages/admin-bot.blade.php
+++ resources/views/filament/resources/bot-resource/pages/admin-bot.blade.php
@@ -1,19 +1,6 @@
 <x-filament::page>
     @include('filament.resources.bot-resource.pages.navigation-bot', ['category' => "edit", 'bot_id' => $this->record])
-    {{$this->form}}
-</x-filament::page>
 
-<script type="text/javascript">
-    function getWebhookInfo() {
-        fetch('/telegram/get_webhook_info/{{$telegram_token}}/{{$telegram_webhook}}', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            data: {'token': '{{$telegram_token}}', 'webhook': '{{$telegram_webhook}}'}
-        }).then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        }).then(data => document.getElementById('result').innerHTML = JSON.stringify(data)).catch(error => console.error('Error:', error));
-    }
-</script>
+    {{$this->form}}
+
+</x-filament::page>
