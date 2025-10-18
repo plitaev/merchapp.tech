fa2ead5feaa94fe322f4cbb7817466378eb0ef1a
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-10-18 20:56:55 +0700

    test webhook

--- resources/views/filament/resources/bot-resource/pages/admin-bot.blade.php
+++ resources/views/filament/resources/bot-resource/pages/admin-bot.blade.php
@@ -4,18 +4,16 @@
 </x-filament::page>
 
 <script type="text/javascript">
-    $(document).ready(function() {
-        function getWebhookInfo() {
-            fetch('/telegram/get_webhook_info/{{$telegram_token}}/{{$telegram_webhook}}', {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                data: {'token': '{{$telegram_token}}', 'webhook': '{{$telegram_webhook}}'}
-            }).then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            }).then(data => document.getElementById('result').innerHTML = JSON.stringify(data)).catch(error => console.error('Error:', error));
-        }
-    });
+    function getWebhookInfo() {
+        fetch('/telegram/get_webhook_info/{{$telegram_token}}/{{$telegram_webhook}}', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            data: {'token': '{{$telegram_token}}', 'webhook': '{{$telegram_webhook}}'}
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        }).then(data => document.getElementById('result').innerHTML = JSON.stringify(data)).catch(error => console.error('Error:', error));
+    }
 </script>
