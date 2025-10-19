02aa3deef19b486b503ec1025062977ca6d3a87c
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-19 19:16:12 +0700

    BotBranches

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -34,7 +34,7 @@ class DevTestController extends Controller
 {
     public function devtest() {
 
-        $olds = Pay::select('bot_user_id')->where('status', 1)->whereIn('product_id', [1, 2, 3])->where('created_at', '<=', '2025-10-17 10:00:00')->get();
+        $olds = Pay::select('bot_user_id')->where('status', 1)->whereIn('product_id', [1, 2, 3])->where('created_at', '<=', '2025-10-17 10:00:00')->pluck('bot_user_id')->toArray();
         $pays = Pay::select('bot_user_id')->whereNotIn('bot_user_id', $olds)->where('status', 1)->where('product_id', 27)->where('created_at', '>=', '2025-10-17 10:00:00')->pluck('bot_user_id')->toArray();
         return $pays;
 
