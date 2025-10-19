61443cdba70c0a7e7f860f056f8f6a732aaaeb38
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-19 23:55:08 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -54,8 +54,8 @@ class DevTestController extends Controller
 
         $pays = Pay::select('bot_user_id')->where('status', 1)->where('product_id', 27)->pluck('bot_user_id')->toArray();
 
-        // === Не оплатившие из первого дня - Рассылка на их третий день
-
+        // === Не оплатившие из 17.10 - Рассылка на их третий день
+        /*
         $datetime_start = '2025-10-17 00:00:00';
         $datetime_end = '2025-10-17 23:59:59';
 
@@ -68,9 +68,24 @@ class DevTestController extends Controller
             ->toArray();
 
         return $bot_users;
+        */
+        // === Не оплатившие из 18.10 - Рассылка на их второй день
 
-        // === Купившие из первого дня за 150 и не купившие полный - Рассылка на их третий день
+        $datetime_start = '2025-10-18 00:00:00';
+        $datetime_end = '2025-10-18 23:59:59';
 
+        $bot_users = BotUser::select('id')
+            ->where('bot_branch_id', 2)
+            ->whereNotIn('id', $pays)
+            ->where('updated_at', '>=', $datetime_start)
+            ->where('updated_at', '<=', $datetime_end)
+            ->pluck('id')
+            ->toArray();
+
+        return $bot_users;
+
+        // === Купившие из 17.10 за 150 и не купившие полный - Рассылка на их третий день
+        /*
         $pays = Pay::select('bot_user_id')->where('status', 1)->where('product_id', 27)->where('created_at', '>=', $datetime_start)->where('created_at', '<=', $datetime_end)->pluck('bot_user_id')->toArray();
         $pays_full = Pay::select('bot_user_id')->where('status', 1)->whereIn('product_id', [1, 2, 3])->where('created_at', '>=', $datetime_start)->pluck('bot_user_id')->toArray();
 
@@ -84,7 +99,7 @@ class DevTestController extends Controller
             ->toArray();
 
         return $bot_users;
-
+        */
 
     }
 }
