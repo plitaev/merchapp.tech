93aa7a20abf913a3daf89ac281634395b331c242
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-21 11:53:32 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -51,17 +51,17 @@ class DevTestController extends Controller
         //return BotUserUnbanSchedule::select('bot_user_id')->whereNotIn('bot_user_id', $bot_users)->pluck('bot_user_id')->toArray();
 
 
-        /*
+
         $olds = Pay::select('bot_user_id')->where('status', 1)->where('created_at', '<=', '2025-10-17 10:00:00')->pluck('bot_user_id')->toArray();
         $pays = Pay::select('bot_user_id')->whereNotIn('bot_user_id', $olds)->where('status', 1)->where('product_id', 27)->where('created_at', '>=', '2025-10-17 10:00:00')->groupBy('bot_user_id')->pluck('bot_user_id')->toArray();
         return $pays;
-        */
 
 
+        /*
         $bot_users = BotUser::select('id')->pluck('id')->toArray();
         $pays = Pay::select('bot_user_id')->whereIn('bot_user_id', $bot_users)->where('status', 1)->where('product_id', 27)->where('created_at', '>=', '2025-10-17 10:00:00')->pluck('bot_user_id')->toArray();
         return Pay::whereIn('bot_user_id', $pays)->where('status', 1)->whereIn('product_id', [1, 2, 3])->where('created_at', '>=', '2025-10-17 10:00:00')->get();
-
+        */
 
         //Выборки
 
