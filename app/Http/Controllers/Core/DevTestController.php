c21c4aa892f7c3fea40adc5f20339c0f9d47ee7a
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-22 19:27:56 +0700

    RecurrentFix

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -44,6 +44,7 @@ class DevTestController extends Controller
     public function devtest() {
 
         $bot_users = BotUser::select('id')->where('recurrent', 0)->pluck('id')->toArray();
+        return $bot_users;
         BotUserRecurrentSchedule::whereIn('bot_user_id', $bot_users)->where('run_status', 0)->update(['run_status' => 3]);
 
         //$role = Role::create(['name' => 'bots']);
