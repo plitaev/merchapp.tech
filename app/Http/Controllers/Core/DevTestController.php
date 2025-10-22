e8cb11a848fcd2e9a8635d3a4e8fca2e74ce87a5
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-22 11:32:22 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -47,15 +47,17 @@ class DevTestController extends Controller
         return $pays;
         */
 
-
+        /*
         $bot_users = BotUser::select('id')->pluck('id')->toArray();
         $pays = Pay::select('bot_user_id')->whereIn('bot_user_id', $bot_users)->where('status', 1)->where('product_id', 27)->where('created_at', '>=', '2025-10-17 10:00:00')->pluck('bot_user_id')->toArray();
         return Pay::whereIn('bot_user_id', $pays)->where('status', 1)->whereIn('product_id', [1, 2, 3])->where('created_at', '>=', '2025-10-17 10:00:00')->get();
-
+        */
 
         //Выборки
 
-        //$pays = Pay::select('bot_user_id')->where('status', 1)->where('product_id', 27)->pluck('bot_user_id')->toArray();
+        $bot_users = BotUser::select('id')->pluck('id')->toArray();
+        $pays = Pay::select('bot_user_id')->where('status', 1)->where('product_id', 27)->pluck('bot_user_id')->toArray();
+        return $pays;
 
         // === Не оплатившие из 17.10 - Рассылка на их третий день
         /*
