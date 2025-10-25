7c0f5a3f33b9b7dd80121a7ec51a360907518a1a
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-25 21:45:50 +0700

    devtest

--- resources/views/core/mini-app/mini-app-banner-page.blade.php
+++ resources/views/core/mini-app/mini-app-banner-page.blade.php
@@ -37,58 +37,7 @@
 
             {{------}}
 
-            @foreach ($banners_big as $banner_big)
-                <div class="mt-3 ml-3 mr-3">
-                    <div class="relative flex flex-col justify-between w-full rounded-xl bg-cover shadow-xl ring-1 ring-gray-900/10">
-                        <img src="{{env('APP_URL').'/content/'.$banner_big->miniapp_banner->image}}?updated_at={{base64_encode($banner_big->updated_at)}}" class="z-1 rounded-xl"/>
-                        <a href="{{$banner_big->miniapp_banner->button_url}}" aria-describedby="tier-hobby" class="inset-x-0 bottom-0 absolute z-2 m-2 block rounded-md px-3.5 py-2 text-center text-sm/6 font-semibold shadow-sm hover:bg-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-400" style="background-color: {{$banner_big->button_bg_color}}; color: {{$banner_big->button_text_color}}">{{$banner_big->miniapp_banner->button_text}}</a>
-                    </div>
-                </div>
-            @endforeach
 
-            {{------}}
-
-            @php
-                $bscount = 0;
-            @endphp
-
-            @foreach ($banners_medium as $banner_medium)
-
-
-            @endforeach
-
-            {{------}}
-
-            @php
-                $bscount = 0;
-            @endphp
-
-            @foreach ($banners_small as $banner_small)
-                @php
-                    $bscount = $bscount + 1;
-                @endphp
-
-                @if ($bscount==1)
-                    <div class="mx-auto grid max-w-md grid-cols-2 mt-3">
-                        @endif
-
-                        <div class="flex flex-col justify-between ml-1.5 mr-3 p-1.5 rounded-xl bg-white shadow-xl ring-1 ring-gray-900/10">
-                            <div>
-                                <img src="{{env('APP_URL').'/content/'.$banner_small->miniapp_banner->image}}?updated_at={{base64_encode($banner_small->miniapp_banner->updated_at)}}" class="rounded-xl"/>
-                            </div>
-
-                            <a href="{{$banner_small->miniapp_banner->button_url}}" aria-describedby="tier-team" class="mt-2 block rounded-md px-3.5 py-2 text-center text-sm/6 font-semibold shadow-sm hover:bg-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-400" style="background-color: {{$banner_small->miniapp_banner->button_bg_color}}; color: {{$banner_small->miniapp_banner->button_text_color}}">{{$banner_small->miniapp_banner->button_text}}</a>
-                        </div>
-
-                        @if ($bscount==2)
-                    </div>
-
-                    @php
-                        $bscount = 0;
-                    @endphp
-                @endif
-
-            @endforeach
 
             {{------}}
 
