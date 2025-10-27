283ecf4bd92f1f29a329caeeeb08406beaa1b25c
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-27 10:56:11 +0700

    GoogleSheets

--- routes/core/google.php
+++ routes/core/google.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::controller(GoogleController::class)->group(function() {
     Route::get('/google/send_banneds', 'send_banneds');
     Route::get('/google/send_recurrent_fail', 'send_recurrent_fail');
+    Route::get('/google/send_non_active', 'send_non_active');
 });
