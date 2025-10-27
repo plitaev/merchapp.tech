68314e321f56f2882297ef5b3d411e9034e2ffd7
Evgenii Plita <evgeniiplita@gmail.com>
2025-10-27 17:43:30 +0700

    devtest

--- resources/views/core/devtest/devtest.blade.php
+++ resources/views/core/devtest/devtest.blade.php
@@ -1,10 +1,10 @@
 <table>
     @foreach ($pays as $pay)
         <tr>
-            <td>{{$pay->first_name}}</td>
-            <td>{{$pay->last_name}}</td>
-            <td>{{$pay->email}}</td>
-            <td>{{$pay->pay_price}}</td>
+            <td>{{$pay->bot_user->first_name}}</td>
+            <td>{{$pay->bot_user->last_name}}</td>
+            <td>{{$pay->bot_user->email}}</td>
+            <td>{{$pay->price}}</td>
             <td>{{date('d.m.Y', strtotime($pay->created_at))}}</td>
         </tr>
     @endforeach
