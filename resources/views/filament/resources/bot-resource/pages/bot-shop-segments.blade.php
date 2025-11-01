b540c3e4c1e89377c0cbfce2e16af53859ea3c7b
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-11-01 13:42:09 +0700

    8

--- resources/views/filament/resources/bot-resource/pages/bot-shop-segments.blade.php
+++ resources/views/filament/resources/bot-resource/pages/bot-shop-segments.blade.php
@@ -1,9 +1,5 @@
 <x-filament-panels::page>
-    <div class="fi-header fi-header-has-breadcrumbs">
-        <div>
-            <h1 class="fi-header-heading"></h1>
-        </div>
-    </div>
+
     {{$this->form}}
 
 </x-filament-panels::page>
