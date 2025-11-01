c3560233f02f7c9be914c9914267b163f0da8930
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-01 23:09:48 +0700

    Prodamus

--- app/Http/Controllers/Core/ProdamusController.php
+++ app/Http/Controllers/Core/ProdamusController.php
@@ -12,7 +12,7 @@ class ProdamusController
         //$paySystemCallbackCreate->handle($source, 'prodamus');
         PaySystemCallback::create(['pay_system_id' => 2, 'callback' => $source]);
 
-        http_response_code(200);
+        //http_response_code(200);
         return 'success';
     }
 }
