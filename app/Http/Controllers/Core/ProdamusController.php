ca3978bf1968a59074f86a095d0b10fc59e01417
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-01 22:57:55 +0700

    Prodamus

--- app/Http/Controllers/Core/ProdamusController.php
+++ app/Http/Controllers/Core/ProdamusController.php
@@ -1,27 +1,14 @@
 <?php
 namespace App\Http\Controllers\Core;
 
-use App\Actions\Core\Pay\PayMakeSuccessful;
 use App\Actions\Core\PaySystemCallback\PaySystemCallbackCreate;
 
 class ProdamusController
 {
     public function callback() {
-        $payMakeSuccessful = new PayMakeSuccessful();
         $paySystemCallbackCreate = new PaySystemCallbackCreate();
-
         $source = file_get_contents('php://input');
-        $Adata=json_decode($source,true);
-
         $paySystemCallbackCreate->handle($source, 'prodamus');
-
-        if ($Adata['payment_status']=='success') {
-            $binding_id = (isset($Adata['binding_id'])?$Adata['binding_id']:NULL);
-
-            if ($Adata['order_num'] == '') return 'success';
-
-            $payMakeSuccessful->handle($source, $Adata['order_num'], $Adata['order_id'], $binding_id, $Adata['commission_sum']);
-        }
 
         http_response_code(200);
         return 'success';
