2d2642dfba6a976792fe4ae66319cb1fb741a3b0
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-01 23:06:58 +0700

    Prodamus

--- app/Http/Controllers/Core/ProdamusController.php
+++ app/Http/Controllers/Core/ProdamusController.php
@@ -2,13 +2,15 @@
 namespace App\Http\Controllers\Core;
 
 use App\Actions\Core\PaySystemCallback\PaySystemCallbackCreate;
+use App\Models\Core\PaySystemCallback;
 
 class ProdamusController
 {
     public function callback() {
         $paySystemCallbackCreate = new PaySystemCallbackCreate();
         $source = file_get_contents('php://input');
-        $paySystemCallbackCreate->handle($source, 'prodamus');
+        //$paySystemCallbackCreate->handle($source, 'prodamus');
+        PaySystemCallback::create(['pay_system_id' => 2, 'callback' => $source]);
 
         http_response_code(200);
         return 'success';
