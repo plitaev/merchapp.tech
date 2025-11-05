c0b25a4789251a93c1a63e0da82c1cda19224b3b
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-05 13:26:55 +0700

    ReferralProgram

--- app/Actions/Core/ReferralProgram/ReferralProgramRunForReferral.php
+++ app/Actions/Core/ReferralProgram/ReferralProgramRunForReferral.php
@@ -11,6 +11,7 @@ class ReferralProgramRunForReferral
     public function handle($bot_user, $branch_data) {
         $botSendMessage = new BotSendMessage();
 
+        //== Проверяем, проходил ли юзер по реф.ссылке другого пользователя
         $check_in_other_rp = BotBranchReferralProgram::whereNot('referrer_bot_user_id', $branch_data[2])
             ->where('bot_branch_id', $branch_data[1])
             ->where('referral_bot_user_id', $bot_user->id)
@@ -18,6 +19,12 @@ class ReferralProgramRunForReferral
 
         if ($check_in_other_rp > 0) {
             $botSendMessage->handle($bot_user, 'SYS_RP_REFERRAL_JOIN_LINK_WHEN_ALREADY_JOINED');
+            die();
+        }
+
+        //== Если реферрер прошел сам по своей ссылке
+        if ($bot_user->id == $branch_data[2]) {
+            $botSendMessage->handle($bot_user, 'SYS_RP_REFERRER_GO_TO_HIS_LINK');
             die();
         }
 
