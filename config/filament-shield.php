c1abc0447df30490fe2fc17468d7da58893385af
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-11-06 00:49:37 +0700

    1

--- config/filament-shield.php
+++ config/filament-shield.php
@@ -69,14 +69,6 @@ return [
         'intercept_gate' => 'before',
     ],
 
-
-    'read_only' => [
-        'enabled' => true,
-        'name' => 'read_only',
-        'define_via_gate' => false,
-        'intercept_gate' => 'before',
-    ],
-
     /*
     |--------------------------------------------------------------------------
     | Panel User
