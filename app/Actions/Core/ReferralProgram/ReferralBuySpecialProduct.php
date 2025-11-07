a029b0aaf2304da330bc5445b08f736f34587400
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-07 17:47:48 +0700

    ReferralProgram

--- app/Actions/Core/ReferralProgram/ReferralBuySpecialProduct.php
+++ app/Actions/Core/ReferralProgram/ReferralBuySpecialProduct.php
@@ -2,13 +2,18 @@
 
 namespace App\Actions\Core\ReferralProgram;
 
+use App\Actions\Core\BotSendMessage\BotSendMessage;
+
 use App\Models\Core\BotBranch;
 use App\Models\Core\BotBranchReferralProgram;
+use App\Models\Core\BotUser;
 
 class ReferralBuySpecialProduct
 {
     public function handle($pay) {
         $datetime = date('Y-m-d H:i:s', time());
+
+        $botSendMessage = new BotSendMessage();
 
         $products = BotBranch::select('bot_branch_product_id')
             ->where('bot_branch_type', 3)
@@ -37,7 +42,18 @@ class ReferralBuySpecialProduct
 
                 if ($referral_record) {
                     BotBranchReferralProgram::where('id', $referral_record->id)->update(['referral_got_product_special' => 1]);
+
                     //и тут кидаем сообщение реферреру
+                    $referrals_count = BotBranchReferralProgram::query()
+                        ->where('bot_branch_id', $bot_branch->id)
+                        ->where('referrer_bot_user_id', $referral_record->referrer_bot_user_id)
+                        ->whereNotNull('referral_bot_user_id')
+                        ->where('referral_got_product_special', 1)
+                        ->count();
+
+                    $referrer_bot_user = BotUser::find($referral_record->referrer_bot_user_id);
+
+                    $botSendMessage->handle($referrer_bot_user, 'SYS_RP_REFERRER_NOTICE_WHEN_REFERAL_JOIN_'.$referrals_count);
                 }
 
             }
