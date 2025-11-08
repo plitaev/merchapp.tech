2c5585c2f93d72a537071f48755e871d5c8a8c3c
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-09 01:04:38 +0700

    ReferralProgram

--- app/Actions/Core/ReferralProgram/ReferralBuySpecialProduct.php
+++ app/Actions/Core/ReferralProgram/ReferralBuySpecialProduct.php
@@ -3,6 +3,7 @@
 namespace App\Actions\Core\ReferralProgram;
 
 use App\Actions\Core\BotSendMessage\BotSendMessage;
+use App\Actions\Core\Pay\PayCreateByEmail;
 
 use App\Models\Core\BotBranch;
 use App\Models\Core\BotBranchReferralProgram;
@@ -14,6 +15,7 @@ class ReferralBuySpecialProduct
         $datetime = date('Y-m-d H:i:s', time());
 
         $botSendMessage = new BotSendMessage();
+        $payCreateByEmail = new PayCreateByEmail();
 
         $products = BotBranch::select('bot_branch_product_id')
             ->where('bot_branch_type', 3)
@@ -60,13 +62,20 @@ class ReferralBuySpecialProduct
 
         } else {
 
-            $check_is_active_referral = BotBranchReferralProgram::select('id', 'referrer_bot_user_id')
+            $check_is_active_referral = BotBranchReferralProgram::select('id', 'bot_branch_id', 'referrer_bot_user_id')
                 ->where('referral_bot_user_id', $pay->bot_user_id)
                 ->where('referral_got_product_special', 1)
                 ->where('referral_got_product_full', 0)
                 ->first();
 
             if ($check_is_active_referral) {
+                $bot_branch = BotBranch::select('referal_program_product_id_for_referrer')
+                    ->find($check_is_active_referral->bot_branch_id);
+
+                if ($bot_branch) {
+                    $bot_user_referrer = BotUser::select('email')->find($check_is_active_referral->referrer_bot_user_id);
+                    $payCreateByEmail->handle($bot_user_referrer->email, $bot_branch->referal_program_product_id_for_referrer, 0, 0);
+                }
 
             }
 
