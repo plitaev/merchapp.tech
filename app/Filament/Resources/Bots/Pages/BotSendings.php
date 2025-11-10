e7519a89b85e3453113cf9c064754f1252d332e3
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-10 23:47:26 +0700

    fix

--- app/Filament/Resources/Bots/Pages/BotSendings.php
+++ app/Filament/Resources/Bots/Pages/BotSendings.php
@@ -90,7 +90,7 @@ class BotSendings extends Page implements HasTable
             ])
             ->recordActions([
                 EditAction::make()->url(fn($record) => "/admin/bots/".$this->bot_id."/".$record->id."/sending-admin")
-                    ->visible(fn() => auth()->user()->disabled('Update:Sending') && auth()->user()->hasRole('super_admin')),
+                    ->visible(fn() => auth()->user()->can('Update:Sending')),
                 DeleteAction::make()
                     ->before(function (DeleteAction $action, Sending $record) {
                         $botSendingAdminDeleteRecord = new BotSendingAdminDeleteRecord();
