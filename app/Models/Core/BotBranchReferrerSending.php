8aa8d3b812e78751aafa3eaee2bb75bb7fb9fd47
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-12 20:05:19 +0700

    ReferralProgram

---
+++ app/Models/Core/BotBranchReferrerSending.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Core;
+
+class BotBranchReferrerSending
+{
+    protected $fillable = [
+        'name',
+        'alias'
+    ];
+}
---
+++ database/migrations/2025_11_12_155714_create_bot_branch_referrer_sending.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bot_branch_referrer_sendings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('alias', 255);
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bot_branch_referrer_sending');
+    }
+};
