5d19ab276ee4998c46d7dfbf47291393a155087e
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-13 12:41:42 +0700

    BotMessageAdmin

--- app/Filament/Resources/Bots/Pages/BotMessageAdmin.php
+++ app/Filament/Resources/Bots/Pages/BotMessageAdmin.php
@@ -254,17 +254,14 @@ class BotMessageAdmin extends Page implements HasForms, HasTable, HasInfolists
                             ->disabled(auth()->user()->hasPermissionTo('Update:BotMessage')?false:true)
                             ->live(),
                         TextInput::make('funnel_days')
-                            ->required()
                             ->label('Дней')
                             ->disabled(auth()->user()->hasPermissionTo('Update:BotMessage')?false:true)
                             ->maxLength(255),
                         TextInput::make('funnel_hours')
-                            ->required()
                             ->label('Часов')
                             ->disabled(auth()->user()->hasPermissionTo('Update:BotMessage')?false:true)
                             ->maxLength(255),
                         TextInput::make('funnel_minutes')
-                            ->required()
                             ->label('Минут')
                             ->disabled(auth()->user()->hasPermissionTo('Update:BotMessage')?false:true)
                             ->maxLength(255)
