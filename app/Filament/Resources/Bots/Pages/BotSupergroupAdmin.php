153a542fea32e305879309ff44cfbfe83864c653
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-11-13 12:53:51 +0700

    1

--- app/Filament/Resources/Bots/Pages/BotSupergroupAdmin.php
+++ app/Filament/Resources/Bots/Pages/BotSupergroupAdmin.php
@@ -172,7 +172,8 @@ class BotSupergroupAdmin extends Page implements HasForms
                                     }
                                 }
                             })
-                            ->disabled(auth()->user()->hasPermissionTo('Update:TelegramSupergroup')?false:true)->maxLength(255)
+                            ->disabled(auth()->user()->hasPermissionTo('Update:TelegramSupergroup')?false:true)
+                            ->maxLength(255)
                             ->disabled(function (Get $get) {
                                 if (is_callable($get)) {
                                     return $get('supergroup_delete_parameter_id') == 2 || $get('supergroup_delete_parameter_id') == 3;
