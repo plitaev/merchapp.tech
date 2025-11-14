d1bc34c7e2b85d895a003afe43568c5e27d4272a
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-14 18:05:58 +0700

    GoogleController

--- app/Http/Controllers/Core/GoogleController.php
+++ app/Http/Controllers/Core/GoogleController.php
@@ -35,7 +35,6 @@ class GoogleController
 
     public function send_recurrent_fail() {
         $date = date('Y-m-d', time());
-        $date = '2025-11-13';
         $datetime_start = $date.' 00:00:00';
         $datetime_end = $date.' 23:59:59';
 
