b54d602441a9752757c152e94679811d2bc5a02c
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-17 13:41:08 +0700

    fix

--- resources/views/filament/resources/bot-resource/pages/navigation-bot.blade.php
+++ resources/views/filament/resources/bot-resource/pages/navigation-bot.blade.php
@@ -1,20 +1,20 @@
 @php
     $categories = [];
-    if (Auth::user()->hasPermissionTo('Update:Bot')) $categories['edit'] = 'Основные';
-    if (Auth::user()->hasPermissionTo('Update:PaySystem')) $categories['pay-system-admin'] = 'Платежные системы';
-    if (Auth::user()->hasPermissionTo('Update:BotMessage')) $categories['messages'] = 'Сообщения';
-    if (Auth::user()->hasPermissionTo('Update:BotUser')) $categories['chats'] = 'Подписчики';
-    if (Auth::user()->hasPermissionTo('Update:Pay')) $categories['pays'] = 'Платежи';
-    if (Auth::user()->hasPermissionTo('Update:PayGuest')) $categories['pay-guests'] = 'В ожидании';
-    if (Auth::user()->hasPermissionTo('Update:TelegramSupergroup'))$categories['supergroups'] = 'Супергруппы';
-     if (Auth::user()->hasPermissionTo('Update:Product'))$categories['products'] = 'Тарифы';
-     if (Auth::user()->hasPermissionTo('Update:Bot'))$categories['getcourse-settings'] = 'Настройки GetCourse';
-     if (Auth::user()->hasPermissionTo('Update:Bot'))$categories['getcourse-webhooks'] = 'Вебхуки GetCourse';
-     if (Auth::user()->hasPermissionTo('Update:BotUserBanSchedule'))$categories['telegram-ban-schedules'] = 'Баны';
-     if (Auth::user()->hasPermissionTo('Update:TelegramUnbanSchedule'))$categories['telegram-unban-schedules'] = 'Разбаны';
-     if (Auth::user()->hasPermissionTo('Update:Funnel'))$categories['funnels'] = 'Воронки';
-     if (Auth::user()->hasPermissionTo('Update:Sending'))$categories['sendings'] = 'Рассылки';
-     if (Auth::user()->hasPermissionTo('Update:BotBranch'))$categories['branches'] = 'Акции';
+    if (Auth::user()->hasPermissionTo('View:Bot')) $categories['edit'] = 'Основные';
+    if (Auth::user()->hasPermissionTo('View:PaySystem')) $categories['pay-system-admin'] = 'Платежные системы';
+    if (Auth::user()->hasPermissionTo('View:BotMessage')) $categories['messages'] = 'Сообщения';
+    if (Auth::user()->hasPermissionTo('View:BotUser')) $categories['chats'] = 'Подписчики';
+    if (Auth::user()->hasPermissionTo('View:Pay')) $categories['pays'] = 'Платежи';
+    if (Auth::user()->hasPermissionTo('View:PayGuest')) $categories['pay-guests'] = 'В ожидании';
+    if (Auth::user()->hasPermissionTo('View:TelegramSupergroup'))$categories['supergroups'] = 'Супергруппы';
+     if (Auth::user()->hasPermissionTo('View:Product'))$categories['products'] = 'Тарифы';
+     if (Auth::user()->hasPermissionTo('View:Bot'))$categories['getcourse-settings'] = 'Настройки GetCourse';
+     if (Auth::user()->hasPermissionTo('View:Bot'))$categories['getcourse-webhooks'] = 'Вебхуки GetCourse';
+     if (Auth::user()->hasPermissionTo('View:BotUserBanSchedule'))$categories['telegram-ban-schedules'] = 'Баны';
+     if (Auth::user()->hasPermissionTo('View:TelegramUnbanSchedule'))$categories['telegram-unban-schedules'] = 'Разбаны';
+     if (Auth::user()->hasPermissionTo('View:Funnel'))$categories['funnels'] = 'Воронки';
+     if (Auth::user()->hasPermissionTo('View:Sending'))$categories['sendings'] = 'Рассылки';
+     if (Auth::user()->hasPermissionTo('View:BotBranch'))$categories['branches'] = 'Акции';
 
 
 @endphp
