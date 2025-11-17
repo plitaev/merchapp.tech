f85c0b170c5caf5aa8024adbe5258c7fcfc814ae
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-11-17 21:54:56 +0700

    скрытие вкладок для неадминов

--- app/Filament/Resources/Bots/Pages/BotTelegramBanSchedules.php
+++ app/Filament/Resources/Bots/Pages/BotTelegramBanSchedules.php
@@ -154,7 +154,7 @@ class BotTelegramBanSchedules extends Page implements HasTable, HasForms
                             ->options(BotUser::where('bot_id', $this->bot_id)->get()->map(function ($bot_user) {
                                 return ['key' => $bot_user->id, 'value' => (isset($bot_user->first_name) && $bot_user->first_name != 'none' ? $bot_user->first_name : '') . " " . (isset($bot_user->last_name) && $bot_user->last_name != 'none' ? $bot_user->last_name : '') . " " . (isset($bot_user->username) && $bot_user->username != 'none' ? "(" . $bot_user->username . ")" : '')];
                             })->pluck('value', 'key')->toArray())
-                            ->disabled(fn() => ['readonly' => auth()->user()->can('Update:BotUserBanSchedule')?true:false]),
+                            ->disabled(auth()->user()->can('Update:BotUserBanSchedule')),
 
                     ]),
                 Actions::make([
