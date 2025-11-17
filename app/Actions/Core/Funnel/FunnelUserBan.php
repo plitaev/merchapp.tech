f7d52b088b0c546de5297866bf41cdd80b61c0c9
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-17 13:33:35 +0700

    Funnels

--- app/Actions/Core/Funnel/FunnelUserBan.php
+++ app/Actions/Core/Funnel/FunnelUserBan.php
@@ -3,6 +3,8 @@ namespace App\Actions\Core\Funnel;
 
 use Carbon\Carbon;
 
+use App\Actions\Core\Funnel\FunnelGetDateTime;
+
 use App\Models\Core\BotUser;
 use App\Models\Core\Sending;
 use App\Models\Core\TelegramSendMessageSchedule;
@@ -13,47 +15,13 @@ class FunnelUserBan
 
         if ($data->funnel_condition->alias == "user_ban") {
 
-            if ($data->funnel_condition_trigger->alias == "now") {
-                $date = date('Y-m-d', time());
-                $time = $data->bot->ban_time;
-                $datetime = $date." ".$data->bot->ban_time;
-            }
+            $funnelGetDateTimeNow = new FunnelGetDateTime();
 
-            if ($data->funnel_condition_trigger->alias == "after") {
-                $prevous_date = Carbon::now();
+            $funnel_date_time = $funnelGetDateTimeNow->handle($data);
 
-                if ($data->funnel_days && $data->funnel_days > 0) $prevous_date->subDays($data->funnel_days);
-
-                $date = $prevous_date->format('Y-m-d');
-                $time = $prevous_date->format('H:i:s');
-
-                $datetime = $date." ".$data->bot->ban_time;
-                $datetime = Carbon::parse($datetime);
-
-                if ($data->funnel_hours && $data->funnel_hours > 0) $datetime->addHours($data->funnel_hours);
-                if ($data->funnel_minutes && $data->funnel_minutes > 0) $datetime->addMinutes($data->funnel_hours);
-
-                $time = $datetime->format('H:i:s');
-                $datetime = $datetime->format('Y-m-d H:i:s');
-            }
-
-            if ($data->funnel_condition_trigger->alias == "before") {
-                $next_date = Carbon::now();
-
-                if ($data->funnel_days && $data->funnel_days > 0) $next_date->addDays($data->funnel_days);
-
-                $date = $next_date->format('Y-m-d');
-                $time = $next_date->format('H:i:s');
-
-                $datetime = $date." ".$data->bot->ban_time;
-                $datetime = Carbon::parse($datetime);
-
-                if ($data->funnel_hours && $data->funnel_hours > 0) $datetime->subHours($data->funnel_hours);
-                if ($data->funnel_minutes && $data->funnel_minutes > 0) $datetime->subMinutes($data->funnel_hours);
-
-                $time = $datetime->format('H:i:s');
-                $datetime = $datetime->format('Y-m-d H:i:s');
-            }
+            $date = $funnel_date_time['date'];
+            $time = $funnel_date_time['time'];
+            $datetime = $funnel_date_time['datetime'];
 
             if (date('H:i:s') >= $time) {
 
