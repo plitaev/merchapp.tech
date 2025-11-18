9b18a76260e18bc06c1e3f053228be620329fd4f
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-18 17:55:13 +0700

    ReferralFunnel

--- app/Actions/Core/Funnel/FunnelGetDateTime.php
+++ app/Actions/Core/Funnel/FunnelGetDateTime.php
@@ -2,67 +2,41 @@
 
 namespace App\Actions\Core\Funnel;
 
-use Carbon\Carbon;
-
 class FunnelGetDateTime
 {
     public function handle($data) {
         $A = [];
 
         if ($data->funnel_condition_trigger->alias == "now") {
+            $now = time();
 
-            $date = date('Y-m-d', time());
-            $time = $data->bot->ban_time;
-            $datetime = $date." ".$data->bot->ban_time;
-
-            $A['date'] = $date;
-            $A['time'] = $time;
-            $A['datetime'] = $datetime;
-
+            $A['date'] = date('Y-m-d', $now);
+            $A['time'] = date('H:i:s', $now);
+            $A['datetime'] = date('Y-m-d H:i:s', $now);
         }
 
         if ($data->funnel_condition_trigger->alias == "after") {
             $prevous_date = Carbon::now();
 
             if ($data->funnel_days && $data->funnel_days > 0) $prevous_date->subDays($data->funnel_days);
+            if ($data->funnel_hours && $data->funnel_hours > 0) $prevous_date->subHours($data->funnel_hours);
+            if ($data->funnel_minutes && $data->funnel_minutes > 0) $prevous_date->subMinutes($data->funnel_hours);
 
-            $date = $prevous_date->format('Y-m-d');
-
-            $datetime = $date." ".$data->bot->ban_time;
-            $datetime = Carbon::parse($datetime);
-
-            if ($data->funnel_hours && $data->funnel_hours > 0) $datetime->addHours($data->funnel_hours);
-            if ($data->funnel_minutes && $data->funnel_minutes > 0) $datetime->addMinutes($data->funnel_hours);
-
-            $time = $datetime->format('H:i:s');
-            $datetime = $datetime->format('Y-m-d H:i:s');
-
-            $A['date'] = $date;
-            $A['time'] = $time;
-            $A['datetime'] = $datetime;
-
+            $A['date'] = $prevous_date->format('Y-m-d');
+            $A['time'] = $prevous_date->format('H:i:s');
+            $A['datetime'] = $prevous_date->format('Y-m-d H:i:s');
         }
 
         if ($data->funnel_condition_trigger->alias == "before") {
             $next_date = Carbon::now();
 
             if ($data->funnel_days && $data->funnel_days > 0) $next_date->addDays($data->funnel_days);
+            if ($data->funnel_hours && $data->funnel_hours > 0) $next_date->addHours($data->funnel_hours);
+            if ($data->funnel_minutes && $data->funnel_minutes > 0) $next_date->addMinutes($data->funnel_hours);
 
-            $date = $next_date->format('Y-m-d');
-
-            $datetime = $date." ".$data->bot->ban_time;
-            $datetime = Carbon::parse($datetime);
-
-            if ($data->funnel_hours && $data->funnel_hours > 0) $datetime->subHours($data->funnel_hours);
-            if ($data->funnel_minutes && $data->funnel_minutes > 0) $datetime->subMinutes($data->funnel_hours);
-
-            $time = $datetime->format('H:i:s');
-            $datetime = $datetime->format('Y-m-d H:i:s');
-
-            $A['date'] = $date;
-            $A['time'] = $time;
-            $A['datetime'] = $datetime;
-
+            $A['date'] = $next_date->format('Y-m-d');
+            $A['time'] = $next_date->format('H:i:s');
+            $A['datetime'] = $next_date->format('Y-m-d H:i:s');
         }
 
         return $A;
