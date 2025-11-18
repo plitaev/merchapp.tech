63c79da7f6e30ffb7908560805dc48dc6ca706ea
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-18 15:35:39 +0700

    ReferralFunnel

---
+++ database/migrations/2025_11_18_113338_ins_funnel_conditions.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\Models\Core\FunnelCondition;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        FunnelCondition::create(['name' => 'Не купил продукт акции или реф.программы', 'alias' => 'not_buy_branch_product']);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
