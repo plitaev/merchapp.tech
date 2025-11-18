05db6fa1525958e574808270ace824bc08c23171
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-18 10:58:42 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -98,7 +98,7 @@ class DevTestController extends Controller
                     $diff_days=$Adates_start[$next_pos]->startOfDay()->diffInDays($date);
                     if ($diff_days>0) {
                         $days_to_add=$days_to_add+$diff_days;
-                        $Adates_end[$next_pos]=$Adates_end[$next_pos]->addDay($diff_days);
+                        $Adates_end[$next_pos]=$Adates_end[$next_pos]->addDay($diff_days)->format('d.m.Y');
                     }
                 }
             }
