0344681d8dfbfb61a682d6c660b62fe213e42294
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-18 11:50:50 +0700

    provider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -132,7 +132,7 @@ class AdminPanelProvider extends PanelProvider
                 DispatchServingFilamentEvent::class,
             ])
             ->plugins([
-                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make()->hasPermissionTo("Update::Bots")
+                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make()
             ])
             ->authMiddleware([
                 Authenticate::class,
