36f70a057ef45cb177f427e020560c92061086b2
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-11-19 16:40:08 +0700

    скрытие вкладок для неадминов

--- resources/views/filament/resources/mini-app-page-resource/pages/admin-mini-app-page.blade.php
+++ resources/views/filament/resources/mini-app-page-resource/pages/admin-mini-app-page.blade.php
@@ -3,33 +3,34 @@
     @if (Auth::user()->hasPermissionTo('View:MiniAppPage'))
 
 
-    <style>
-        .fi-ta-search-field{min-width: 300px}
-    </style>
+        <style>
+            .fi-ta-search-field{min-width: 300px}
+        </style>
 
-    <form wire:submit.prevent="submit">{{$this->form}}</form>
+        <form wire:submit.prevent="submit">{{$this->form}}</form>
 
-    <div class="fi-header fi-header-has-breadcrumbs">
-        <div>
-            <h1 class="fi-header-heading">Баннеры</h1>
-        </div>
+        <div class="fi-header fi-header-has-breadcrumbs">
+            <div>
+                <h1 class="fi-header-heading">Баннеры</h1>
+            </div>
 
-        <div class="fi-header-actions-ctn">
-            <div class="fi-ac fi-align-start">
-                @if (auth()->user()->can('Create:MiniAppBanner'))
+            <div class="fi-header-actions-ctn">
+                <div class="fi-ac fi-align-start">
+                    @if (auth()->user()->can('Create:MiniAppBanner'))
 
-                <a href="/admin/mini-app-banners/{{$record}}/0/admin" class="fi-color fi-color-primary fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
-                    Создать баннер
-                </a>
-                @endif
-                <a href="/admin/mini-app-pages/{{$record}}/preview" class="fi-color fi-color-primary fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
-                    Предварительный просмотр
-                </a>
+                        <a href="/admin/mini-app-banners/{{$record}}/0/admin" class="fi-color fi-color-primary fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
+                            Создать баннер
+                        </a>
+
+                        <a href="/admin/mini-app-pages/{{$record}}/preview" class="fi-color fi-color-primary fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
+                            Предварительный просмотр
+                        </a>
+                    @endif
+                </div>
             </div>
         </div>
-    </div>
 
-    {{$this->table}}
+        {{$this->table}}
 
     @endif
 </x-filament::page>
