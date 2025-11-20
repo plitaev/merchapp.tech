f9d55008202f9f4568bfaf62049fd00acd9f7a8a
Evgenii Plita <evgeniiplita@gmail.com>
2025-11-20 19:57:58 +0700

    BotSetFunnels

--- app/Actions/Core/Funnel/FunnelReferrer.php
+++ app/Actions/Core/Funnel/FunnelReferrer.php
@@ -25,6 +25,8 @@ class FunnelReferrer
             if (date('H:i:s') >= $time) {
                 $schedules = $getUsersAlreadyInSendingToday->handle($data);
 
+                return $schedules;
+
                 $referrers_with_referrals = BotBranchReferralProgram::select('referrer_bot_user_id')
                     ->where('bot_branch_id', $data->bot_branch_id)
                     ->whereNotNull('referral_bot_user_id')
