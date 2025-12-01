54667207e3834aaf82e3a56d367d06644f9a0d79
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-01 15:19:45 +0700

    PriceUpdate

---
+++ database/migrations/2025_12_01_111549_update_bot_users.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bot_users', function (Blueprint $table) {
+            $table->unsignedBigInteger('last_product_id')->after('recurrent')->nullable();
+            $table->integer('last_product_price')->after('last_product_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
