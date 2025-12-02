5d1cba36212a890725f275d82fde8c1214eaf8b2
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-02 11:51:46 +0700

    BotUserPriceFix

--- app/Actions/Core/BotUserPrice/BotUserPriceSet.php
+++ app/Actions/Core/BotUserPrice/BotUserPriceSet.php
@@ -9,9 +9,14 @@ class BotUserPriceSet
 {
     public function handle($bot_user) {
         $products = Product::all();
+        $prices = BotUserPrice::select('product_id')->where('bot_user_id', $bot_user->id)->groupBy('product_id')->pluck('product_id')->toArray();
 
         foreach ($products as $product) {
-            BotUserPrice::create(['bot_user_id' => $bot_user->id, 'product_id' => $product->id, 'price' => $product->price]);
+
+            if (!in_array($product->id, $prices)) {
+                BotUserPrice::create(['bot_user_id' => $bot_user->id, 'product_id' => $product->id, 'price' => $product->price]);
+            }
+
         }
 
     }
