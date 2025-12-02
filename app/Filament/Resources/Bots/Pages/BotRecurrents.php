9c899b2453b72822c88b0b35c64cb50f384c7e03
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-02 14:21:25 +0700

    BotRecurrents

--- app/Filament/Resources/Bots/Pages/BotRecurrents.php
+++ app/Filament/Resources/Bots/Pages/BotRecurrents.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Bots\Pages;
 
+use App\Models\Core\BotUser;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Actions\EditAction;
 use Filament\Actions\DeleteAction;
@@ -37,12 +38,27 @@ class BotRecurrents extends Page implements HasTable
     public int $bot_id;
     public string $bot_name;
 
+    public array $recurrents;
+
     public function mount(int $bot_id): void
     {
         $this->bot_id = $bot_id;
         $bot = Bot::select('name')->find($bot_id);
 
         $this->bot_name = $bot->name;
+
+        $recurrents_all = BotUser::select('date_end')->where('date_end', '>=', date('Y-m-d', time()))->where('recurrent', 1)->get();
+        $Adates = [];
+        foreach ($recurrents_all as $recurrent_all) {
+            $Adates[$recurrent_all->date_end][] = 1;
+        }
+
+        $recurrent_dates = BotUser::select('date_end')->where('date_end', '>=', date('Y-m-d', time()))->where('recurrent', 1)->orderBy('date_end')->pluck('date_end')->toArray();
+        foreach ($recurrent_dates as $recurrent_date) {
+            $recurrents[] = ['date' => date('d.m.Y', strtotime($recurrent_date->date_end)), 'count' => (isset($Adates[$recurrent_date])?count($Adates[$recurrent_date]):0)];
+        }
+
+
 
         if (!Auth::user()->hasPermissionTo('View:Pay')) {
             redirect('/admin/bots/access');
@@ -62,39 +78,10 @@ class BotRecurrents extends Page implements HasTable
     public function table(Table $table): Table
     {
         return $table
-            ->query(Product::with('product_type')->where('bot_id', $this->bot_id))
-            ->persistSearchInSession()
+            ->records(fn (): array => $this->recurrents)
             ->columns([
-                TextColumn::make('name')
-                    ->label('Название')
-                    ->searchable(),
-                TextColumn::make('product_type.name')
-                    ->label('Тип'),
-                TextColumn::make('price')
-                    ->label('Стоимость'),
-                TextColumn::make('days')
-                    ->label('Дни')
-            ])
-            ->filters([
-                //
-            ])
-            ->recordActions([
-                ViewAction::make()->url(fn($record) => "/admin/bots/".$this->bot_id."/".$record->id."/product-admin")
-                    ->visible(!auth()->user()->can('Create:Pay')),
-                EditAction::make()->url(fn($record) => "/admin/bots/".$this->bot_id."/".$record->id."/product-admin")
-                    ->visible(auth()->user()->can('Create:Pay')),
-
-                DeleteAction::make()
-                    ->visible(auth()->user()->can('Delete:Pay')),
-
-            ])
-            ->recordUrl(fn($record) => "/admin/bots/".$this->bot_id."/".$record->id."/product-admin")
-            ->toolbarActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make()
-                        ->visible(auth()->user()->can('Delete:Pay')),
-
-                ]),
+                TextColumn::make('date'),
+                TextColumn::make('count')
             ]);
     }
 }
