40b17c2ea48578374f6b70da23d69a75c495f5e3
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-03 18:58:15 +0700

    BotRecurrents

--- app/Filament/Resources/Bots/Pages/BotRecurrents.php
+++ app/Filament/Resources/Bots/Pages/BotRecurrents.php
@@ -77,7 +77,7 @@ class BotRecurrents extends Page implements HasForms, HasTable
 
         $recurrents_by_months = "";
         foreach ($Amys as $my) {
-            $recurrents_by_months .= "<p style='display: block; margin-bottom: 10px; font-weight:bold'>".(isset($Amys[$my])?count($Amys[$my]):0)."</p>";
+            $recurrents_by_months .= "<p style='display: block; margin-bottom: 10px; font-weight:bold'>".$my." - ".(isset($Amys[$my])?count($Amys[$my]):0)."</p>";
         }
 
         $this->recurrents_by_months = $recurrents_by_months;
