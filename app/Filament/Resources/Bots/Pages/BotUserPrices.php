a5ef350a15eff2f6e066935881426da4ab7f234e
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-12-04 07:35:08 +0700

    скрытие вкладок для неадминов

--- app/Filament/Resources/Bots/Pages/BotUserPrices.php
+++ app/Filament/Resources/Bots/Pages/BotUserPrices.php
@@ -13,7 +13,7 @@ use Filament\Actions\Action;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Actions\EditAction;
 use Filament\Actions\DeleteAction;
-use App\Models\Core\BotMessage;
+use App\Models\Core\BotUserPrice;
 use App\Models\Core\TelegramSendMessageSchedule;
 use Illuminate\Support\HtmlString;
 
@@ -21,6 +21,9 @@ use Illuminate\Support\HtmlString;
 use App\Filament\Resources\Bots\BotResource;
 
 use Filament\Forms;
+
+use Filament\Infolists\Contracts\HasInfolists;
+
 use Filament\Forms\Components;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\DatePicker;
@@ -46,17 +49,18 @@ use App\Models\Core\Product;
 use App\Models\Core\BotUser;
 use Illuminate\Support\Facades\Auth;
 
-class BotUserPrices extends Page implements HasTable, HasForms
+class BotUserPrices extends Page implements HasTable, HasForms, HasInfolists
 {
     use InteractsWithForms;
   
     use InteractsWithTable;
 
+    use InteractsWithInfolists;
 
 
     protected static string $resource = BotResource::class;
 
-    protected string $view = 'filament.resources.bot-resource.pages.bot-users-prices';
+    protected string $view = 'filament.resources.bot-resource.pages.bot-user-prices';
 
     public int $id;
 
@@ -72,15 +76,15 @@ class BotUserPrices extends Page implements HasTable, HasForms
 
     protected static ?string $model = BotUserPrice::class;
 
-    public static ?string $label = "Индивидуальные  цены";
-    public static ?string $navigationLabel = "Индивидуальная  цена";
-    public static ?string $title = "Индивидуальная  цена";
+    public static ?string $label = "Индивидуальная цена";
+    public static ?string $navigationLabel = "Индивидуальная цена";
+    public static ?string $title = "Индивидуальная цена";
     public ?array $data = [];
 
-    public function getRecord(): ?Model
-    {
-        return BotUserPrice::class;
-    }
+//    public function getRecord(): ?Model
+//    {
+//        return BotUserPrice::class;
+//    }
 
     public function getHeading(): string
     {
@@ -106,13 +110,13 @@ class BotUserPrices extends Page implements HasTable, HasForms
         $this->bot_user_id = $bot_user_id;
 
         if($id > 0) {
-            $botUserPrices = BotUserPrices::find($this->id);
+            $botUserPrices = BotUserPrice::find($this->id);
         }else {
-            $botUserPrices = BotUserPrices::get();
+            $botUserPrices = BotUserPrice::get();
         }
-        if (!Auth::user()->hasPermissionTo('View:BotUserBanSchedule')) {
-            redirect('/admin/bots/access');
-        }
+//        if (!Auth::user()->hasPermissionTo('View:BotUserBanSchedule')) {
+//            redirect('/admin/bots/access');
+//        }
     }
 
     public function getTitle(): string
@@ -134,10 +138,10 @@ class BotUserPrices extends Page implements HasTable, HasForms
                     ->label('Цена'),
             ])
             ->recordActions([
-                EditAction::make()->url(fn($record) => "/admin/bots/".$this->bot_user_id."/".$record->id."/bot-users-prices")
-                    ->visible(auth()->user()->can('Update:TelegramSupergroup')),
+                EditAction::make()->url(fn($record) => "/admin/bots/".$this->bot_id."/".$this->bot_user_id."/".$record->id."/users-prices"),
+                    //->visible(auth()->user()->can('Update:TelegramSupergroup')),
                 DeleteAction::make()
-                    ->visible(auth()->user()->can('Delete:TelegramSupergroup')),
+                    //->visible(auth()->user()->can('Delete:TelegramSupergroup')),
 
             ])
 
