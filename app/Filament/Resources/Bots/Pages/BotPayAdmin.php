0a213f4ed3ba988ca56c582ac5ecd84513a0b9f4
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-05 16:05:57 +0700

    devtest

--- app/Filament/Resources/Bots/Pages/BotPayAdmin.php
+++ app/Filament/Resources/Bots/Pages/BotPayAdmin.php
@@ -196,7 +196,6 @@ class BotPayAdmin extends Page implements HasForms
 
                             if ($this->id>0) {
 
-                                $data['email'] = trim($data['email']);
                                 unset($data['email']);
                                 unset($data['bot_id']);
 
