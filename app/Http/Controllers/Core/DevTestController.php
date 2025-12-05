3ccdd13149ae8fcb51303c471181b5b97d7af040
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-05 14:55:45 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -46,6 +46,6 @@ use Illuminate\Support\Facades\Auth;
 class DevTestController extends Controller
 {
     public function devtest() {
-        return Carbon::parse("2025-12-01 00:00:00", "Europe/Moscow")->addDays(30)->startOfDay();
+        return Carbon::parse("2025-12-01")->startOfDay()->addDays(30)->startOfDay();
     }
 }
