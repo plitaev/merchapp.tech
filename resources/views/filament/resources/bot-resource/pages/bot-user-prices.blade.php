39ab80b08f3c5bab1a6675beefc7368fd4386939
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-06 16:19:50 +0700

    BotUserPriceAdmin

--- resources/views/filament/resources/bot-resource/pages/bot-user-prices.blade.php
+++ resources/views/filament/resources/bot-resource/pages/bot-user-prices.blade.php
@@ -5,13 +5,21 @@
     </style>
 
     @if (auth()->user()->can('Create:Pay'))
-        <div class="fi-header-actions-ctn">
-            <div class="fi-ac fi-align-start">
-                <a href="/admin/bots//0/pay-admin" class="fi-color fi-color-primary fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
-                    Добавить индивидуальную цену
-                </a>
+        <div class="fi-header fi-header-has-breadcrumbs">
+            <div>
+                <h1 class="fi-header-heading"></h1>
             </div>
+            @if (auth()->user()->can('Create:Pay'))
+                <div class="fi-header-actions-ctn">
+                    <div class="fi-ac fi-align-start">
+                        <a href="/admin/bots//0/pay-admin" class="fi-color fi-color-primary fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
+                            Добавить индивидуальную цену
+                        </a>
+                    </div>
+                </div>
+            @endif
         </div>
+
     @endif
 
     {{$this->table}}
