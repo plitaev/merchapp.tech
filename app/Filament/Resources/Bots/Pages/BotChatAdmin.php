21b5bd4e0546cc84464220e4e9f5f83f929ed7c8
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-06 14:57:09 +0700

    BotUserPriceAdmin

--- app/Filament/Resources/Bots/Pages/BotChatAdmin.php
+++ app/Filament/Resources/Bots/Pages/BotChatAdmin.php
@@ -139,7 +139,7 @@ class BotChatAdmin extends Page implements HasForms, HasInfolists
         $bot_user_prices_product_ids = [];
 
         foreach ($bot_user_prices as $bot_user_price) {
-            $this->bot_user_prices_individual .= "<p style='margin-bottom: 10px; font-weight:bold'>".$bot_user_price->product->name . ' - ' . $bot_user_price->price."</p>";
+            $this->bot_user_prices_individual .= "<p style='margin-bottom: 10px'>".$bot_user_price->product->name . ' - ' . $bot_user_price->price."</p>";
             $bot_user_prices_product_ids[] = $bot_user_price->product_id;
         }
 
@@ -147,14 +147,14 @@ class BotChatAdmin extends Page implements HasForms, HasInfolists
 
             $products = Product::select('id', 'name', 'price')->whereNotIn('id', $bot_user_prices_product_ids)->get();
             foreach ($products as $product) {
-                $this->bot_user_prices_standard .= "<p style='margin-bottom: 10px; font-weight:bold'>".$product->name . ' - ' . $product->price."</p>";
+                $this->bot_user_prices_standard .= "<p style='margin-bottom: 10px'>".$product->name . ' - ' . $product->price."</p>";
             }
 
         } else {
 
             $products = Product::select('id', 'name', 'price')->get();
             foreach ($products as $product) {
-                $this->bot_user_prices_standard .= "<p style='margin-bottom: 10px; font-weight:bold'>".$product->name . ' - ' . $product->price."</p>";
+                $this->bot_user_prices_standard .= "<p style='margin-bottom: 10px'>".$product->name . ' - ' . $product->price."</p>";
             }
 
         }
