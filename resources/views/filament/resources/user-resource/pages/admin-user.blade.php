e5158eeab8c38e502242a1f5049d96c0d07eab5e
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2025-12-07 23:42:31 +0700

    крад для назначения ролей и инд прав пользователю

--- resources/views/filament/resources/user-resource/pages/admin-user.blade.php
+++ resources/views/filament/resources/user-resource/pages/admin-user.blade.php
@@ -3,20 +3,16 @@
         .fi-ta-search-field{min-width: 300px}
     </style>
 
-    @if (auth()->user()->hasPermissionTo('View:User'))
-
-        <form wire:submit.prevent="submit">{{$this->form}}</form>
-
+    <form wire:submit.prevent="submit">{{$this->form}}</form>
 
         {{$this->table}}
 
-        <div class="fi-header flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
-                <a href="/admin/users/{{$this->id}}/model-has-permission" style="--c-400:var(--primary-400);--c-500:var(--primary-500);--c-600:var(--primary-600);" class="fi-color fi-color-success  fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
-                    <span class="fi-btn-label">Назначить индивидуальные права</span>
-                </a>
-        </div>
-
-
+    @if (auth()->user()->hasPermissionTo('View:User'))
+    <div class="fi-header flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
+            <a href="/admin/users/{{$this->id}}/model-has-permission" style="--c-400:var(--primary-400);--c-500:var(--primary-500);--c-600:var(--primary-600);" class="fi-color fi-color-success  fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
+                <span class="fi-btn-label">Назначить индивидуальные права</span>
+            </a>
+    </div>
 
     @endif
 </x-filament-panels::page>
