61e207b79a4d5d45b786a6b311deb596660d693a
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-08 13:21:25 +0700

    Delete:UserPermissionFix

--- database/migrations/2025_12_08_091542_ins_permissions.php
+++ database/migrations/2025_12_08_091542_ins_permissions.php
@@ -15,7 +15,7 @@ return new class extends Migration
     public function up(): void
     {
         //$new = Permission::create(['name' => 'Delete:User', 'guard_name' => 'web']);
-        DB::table('role_has_permissions')->insertOrIgnore(['role_id' => 1, 'permission_id' => $new->id]);
+        DB::table('role_has_permissions')->insertOrIgnore(['role_id' => 1, 'permission_id' => 296]);
     }
 
     /**
