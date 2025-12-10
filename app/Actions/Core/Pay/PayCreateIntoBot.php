a4bd3a72b0941748e6ae72d18b9f691b035eb61f
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-10 10:58:54 +0700

    RecurrentDateEndFix

--- app/Actions/Core/Pay/PayCreateIntoBot.php
+++ app/Actions/Core/Pay/PayCreateIntoBot.php
@@ -2,12 +2,9 @@
 
 namespace App\Actions\Core\Pay;
 
-use App\Actions\Core\DateEnd\DateEnd;
-
 class PayCreateIntoBot
 {
     public function handle($bot_user, $product, $additional_data) {
-        $dateEnd = new DateEnd();
 
         $price = (isset($additional_data['price'])?$additional_data['price']:$product->price);
         $recurrent = (isset($additional_data['recurrent'])?$additional_data['recurrent']:0);
@@ -24,8 +21,7 @@ class PayCreateIntoBot
             'pay_system_id' => $additional_data['pay_system_id']
         ]);
 
-        $dateEnd->handle($bot_user, 'Y-m-d');
-
         return $new;
     }
+
 }
