3866f0bad4b6f1a38dc4204938f22eca41a13b20
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-11 18:53:46 +0700

    BotRecurrentFailNotification

--- app/Actions/Core/Prodamus/ProdamusMakeRecurrent.php
+++ app/Actions/Core/Prodamus/ProdamusMakeRecurrent.php
@@ -8,6 +8,7 @@ use App\Actions\Core\Pay\PayCreateIntoBot;
 use App\Actions\Core\Pay\PayGetAdditionalData;
 use App\Actions\Core\Pay\PayMakeSuccessful;
 
+use App\Models\Core\BotUser;
 use App\Models\Core\BotUserBanSchedule;
 use App\Models\Core\Product;
 
