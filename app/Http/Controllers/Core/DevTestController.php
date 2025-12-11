8bfb24bce29969955fdb6b93a41f88032af47095
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-11 21:23:11 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -12,28 +12,5 @@ use Telegram\Bot\Api;
 class DevTestController extends Controller
 {
     public function devtest() {
-        $botSendMessage = new BotSendMessage();
-
-        $A = [
-            1692,
-            1733,
-            2856,
-            3516,
-            4696,
-            4932,
-            8121,
-            14200,
-            17810,
-            18481,
-            19771,
-            19830,
-            19997
-        ];
-
-        $bot_users = BotUser::whereIn('id', $A)->get();
-        foreach ($bot_users as $bot_user) {
-            $botSendMessage->handle($bot_user, 'BOT_PAYMENT_RECURRENT_FAIL');
-        }
-
     }
 }
