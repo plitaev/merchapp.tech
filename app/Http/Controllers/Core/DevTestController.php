232a5ae98467fe30c38ef1578b2710e76c9095ae
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-13 13:25:58 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -1,16 +1,24 @@
 <?php
 namespace App\Http\Controllers\Core;
 
-use App\Actions\Core\BotSendMessage\BotSendMessage;
 use App\Http\Controllers\Controller;
-use App\Models\Core\BotUser;
-use App\Models\Core\BotUserSupergroupStatus;
+
+use App\Models\Core\BotUserPrice;
+use App\Models\Core\Product;
 use Illuminate\Support\Facades\DB;
 use Telegram\Bot\Api;
 
+use App\Models\Core\Pay;
 
 class DevTestController extends Controller
 {
     public function devtest() {
+        $product_from = 5;
+        $product_to = 2;
+
+        Pay::where('product_id', $product_from)->update(['product_id' => $product_to]);
+        BotUserPrice::where('product_id', $product_from)->delete();
+        Product::destroy($product_from);
+
     }
 }
