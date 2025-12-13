a72668d9b1cdcc1cf911fafed68e48bb4ac50652
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-13 13:30:42 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -13,8 +13,8 @@ use App\Models\Core\Pay;
 class DevTestController extends Controller
 {
     public function devtest() {
-        $product_from = 6;
-        $product_to = 4;
+        $product_from = 7;
+        $product_to = 28;
 
         Pay::where('product_id', $product_from)->update(['product_id' => $product_to]);
         BotUserPrice::where('product_id', $product_from)->delete();
