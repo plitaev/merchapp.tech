71f0cb39d77a2173d6e00fd4750a89e192078536
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-16 20:22:30 +0700

    PluginsSupport

--- app/Http/Controllers/Project/ClubAccessController.php
+++ app/Http/Controllers/Project/ClubAccessController.php
@@ -258,6 +258,7 @@ class ClubAccessController extends Controller
 
                 if (file_exists(storage_path().'/merchapp/club_access.php')) {
                     require_once storage_path().'/merchapp/club_access.php';
+                    return 'plugin connected';
                 }
 
             } else {
