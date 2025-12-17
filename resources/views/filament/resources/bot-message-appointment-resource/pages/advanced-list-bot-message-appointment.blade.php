25384f4d30014462c61f6f152668fa803126d1f0
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-17 16:05:16 +0700

    ебучие баги насти

--- resources/views/filament/resources/bot-message-appointment-resource/pages/advanced-list-bot-message-appointment.blade.php
+++ resources/views/filament/resources/bot-message-appointment-resource/pages/advanced-list-bot-message-appointment.blade.php
@@ -33,7 +33,7 @@
 
         <div class="fi-header-actions-ctn">
             <div class="fi-ac fi-align-start">
-                <a href="/admin/bot-message-appointment/0/admin" class="fi-color fi-color-primary fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
+                <a href="/admin/bot-message-appointments/0/admin" class="fi-color fi-color-primary fi-bg-color-400 hover:fi-bg-color-300 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-900 hover:fi-text-color-800 dark:fi-text-color-950 dark:hover:fi-text-color-950 fi-btn fi-size-md  fi-ac-btn-action">
                     Создать
                 </a>
             </div>
