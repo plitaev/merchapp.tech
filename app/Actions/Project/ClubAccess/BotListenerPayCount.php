84444a46889b79bc1d1b407e632561280dae0c48
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-17 18:22:14 +0700

    UserPayCount

--- app/Actions/Project/ClubAccess/BotListenerPayCount.php
+++ app/Actions/Project/ClubAccess/BotListenerPayCount.php
@@ -3,16 +3,19 @@
 namespace App\Actions\Project\ClubAccess;
 
 use App\Actions\Core\BotSendMessage\BotSendMessage;
+use App\Actions\Core\BotUser\BotUserSetListener;
 
 class BotListenerPayCount
 {
     public function handle($webhook, $bot_user) {
         $botSendMessage = new BotSendMessage();
+        $botUserSetListener = new BotUserSetListener();
 
         if (isset($webhook['message']['text'])) {
 
             if (is_int($webhook['message']['text'])) {
                 $botSendMessage->handle($bot_user, 'SYS_USER_ENTERED_PAY_COUNT');
+                $botUserSetListener->handle('listen_pay_count', 0, $bot_user->id);
             } else {
                 $botSendMessage->handle($bot_user, 'SYS_USER_ENTERED_PAY_COUNT_NON_INT');
             }
