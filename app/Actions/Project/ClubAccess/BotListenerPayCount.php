e9e9e0cb9dbcb3b1e7b1f82053a793cf07dc51e3
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-18 12:04:43 +0700

    Robokassa

--- app/Actions/Project/ClubAccess/BotListenerPayCount.php
+++ app/Actions/Project/ClubAccess/BotListenerPayCount.php
@@ -11,19 +11,21 @@ class BotListenerPayCount
         $botSendMessage = new BotSendMessage();
         $botUserSetListener = new BotUserSetListener();
 
-        if ($bot_user->listen_pay_count_status == 0) die();
+        if ($bot_user->listen_pay_count_status == 1) {
 
-        if (isset($webhook['message']['text'])) {
+            if (isset($webhook['message']['text'])) {
 
-            if (is_numeric($webhook['message']['text'])) {
-                $botSendMessage->handle($bot_user, 'SYS_USER_ENTERED_PAY_COUNT');
-                $botUserSetListener->handle('listen_pay_count', 0, $bot_user->id);
+                if (is_numeric($webhook['message']['text'])) {
+                    $botSendMessage->handle($bot_user, 'SYS_USER_ENTERED_PAY_COUNT');
+                    $botUserSetListener->handle('listen_pay_count', 0, $bot_user->id);
+                } else {
+                    $botSendMessage->handle($bot_user, 'SYS_USER_ENTERED_PAY_COUNT_NON_INT');
+                }
+
             } else {
                 $botSendMessage->handle($bot_user, 'SYS_USER_ENTERED_PAY_COUNT_NON_INT');
             }
 
-        } else {
-            $botSendMessage->handle($bot_user, 'SYS_USER_ENTERED_PAY_COUNT_NON_INT');
         }
 
     }
