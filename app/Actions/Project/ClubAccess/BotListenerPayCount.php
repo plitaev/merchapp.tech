7a495725b4b61985702d2a1ec3e79e30a3fb1883
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-18 22:36:23 +0700

    Robokassa

--- app/Actions/Project/ClubAccess/BotListenerPayCount.php
+++ app/Actions/Project/ClubAccess/BotListenerPayCount.php
@@ -4,6 +4,7 @@ namespace App\Actions\Project\ClubAccess;
 
 use App\Actions\Core\BotSendMessage\BotSendMessage;
 use App\Actions\Core\BotUser\BotUserSetListener;
+use App\Models\Core\BotUser;
 
 class BotListenerPayCount
 {
@@ -16,8 +17,11 @@ class BotListenerPayCount
             if (isset($webhook['message']['text'])) {
 
                 if (is_numeric($webhook['message']['text'])) {
+                    BotUser::where('id', $bot_user->id)->update(['pay_count' => $webhook['message']['text']]);
                     $botSendMessage->handle($bot_user, 'SYS_USER_ENTERED_PAY_COUNT');
                     $botUserSetListener->handle('listen_pay_count', 0, $bot_user->id);
+
+                    $bot_user = BotUser::find($bot_user->id);
                 } else {
                     $botSendMessage->handle($bot_user, 'SYS_USER_ENTERED_PAY_COUNT_NON_INT');
                 }
