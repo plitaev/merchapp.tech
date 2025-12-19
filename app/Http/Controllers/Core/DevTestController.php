eaca6f2955723172ea8326dccfe42218323969c7
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-19 11:46:04 +0700

    Robokassa

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -16,22 +16,22 @@ class DevTestController extends Controller
     public function devtest() {
         $source = "out_summ=50.000000&OutSum=50.000000&inv_id=79&InvId=79&crc=0003046F45C02FE8749691DEDF154314&SignatureValue=0003046F45C02FE8749691DEDF154314&PaymentMethod=BankCard&IncSum=50.000000&IncCurrLabel=BankCardPSR&EMail=evgeniiplita@gmail.com&Fee=1.960000";
 
-        $result = [];
-
-        $result_string = '';
+        $result_k = [];
+        $result_v = [];
 
         $A = explode('&', $source);
         foreach ($A as $value) {
             $AA = explode('=', $value);
             foreach ($AA as $k => $v) {
-                $result_string.=$k.' - '.$v.' | ';
-                $result[] = [$k => $v];
+                if ($k == 0) $result_k[] = $v;
+                if ($k == 1) $result_v[] = $v;
             }
         }
 
+        $result = array_combine($result_k, $result_v);
         $result = json_encode($result, JSON_UNESCAPED_UNICODE);
 
-        return $result_string;
+        return $result;
 
     }
 }
