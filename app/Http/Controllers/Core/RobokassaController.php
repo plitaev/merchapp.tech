752814c7567b553f6f2c34c01c371af17716abeb
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-19 11:36:58 +0700

    Robokassa

--- app/Http/Controllers/Core/RobokassaController.php
+++ app/Http/Controllers/Core/RobokassaController.php
@@ -8,12 +8,22 @@ class RobokassaController
 {
     public function callback() {
         $source = file_get_contents('php://input');
+
+        $result = [];
+
+        $A = explode('&', $source);
+        foreach ($A as $value) {
+            $AA = explode('=', $value);
+            foreach ($AA as $k => $v) {
+                $result[$k] = $v;
+            }
+        }
+
+        $result = json_encode($result);
         PaySystemCallback::create(['pay_system_id' => 3, 'callback' => $source]);
 
-        $source = json_encode($source);
+        $result = json_decode($result);
         PaySystemCallback::create(['pay_system_id' => 3, 'callback' => $source]);
 
-        $source = json_encode($source);
-        PaySystemCallback::create(['pay_system_id' => 3, 'callback' => json_decode(json_encode($_POST))]);
     }
 }
