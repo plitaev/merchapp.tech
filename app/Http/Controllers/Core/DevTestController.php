41c5a418e88d437871802ea7167ba378a2f375ff
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-19 11:40:54 +0700

    Robokassa

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -26,7 +26,7 @@ class DevTestController extends Controller
             }
         }
 
-        $result = json_encode($result);
+        $result = json_decode(json_encode($result));
 
         return $result;
 
