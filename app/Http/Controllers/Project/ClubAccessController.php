d9fe9edbc2ddeda24d858f9caca4fe6eba6ae6e9
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-19 14:15:45 +0700

    Robokassa

--- app/Http/Controllers/Project/ClubAccessController.php
+++ app/Http/Controllers/Project/ClubAccessController.php
@@ -41,8 +41,6 @@ class ClubAccessController extends Controller
 {
     public function club_access(int $bot_id) {
 
-        die();
-
         //== Инициализируем основные классы
         $botBranchRun = new BotBranchRun();
         $botBranchEndOnRestart = new BotBranchEndOnRestart();
