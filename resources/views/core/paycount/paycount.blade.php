ce6beb1423e217a5a06535a935f71e737e24ce60
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-20 18:29:08 +0700

    Paycount

--- resources/views/core/paycount/paycount.blade.php
+++ resources/views/core/paycount/paycount.blade.php
@@ -4,6 +4,7 @@
 </head>
 <body>
 <form action="/paycount/load_post" method="POST" enctype="multipart/form-data">
+    @csrf
     <textarea style="margin: 0 auto; width: 980px; height: 600px"></textarea>
     <div style="margin-top: 25px"><input type="submit" value="Отправить"/></div>
 </form>
