208148cab590f194c7198a3775e6c468a5295653
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-20 18:34:56 +0700

    Paycount

--- app/Http/Controllers/Core/PayCountController.php
+++ app/Http/Controllers/Core/PayCountController.php
@@ -10,9 +10,14 @@ class PayCountController
         return view('core.paycount.paycount');
     }
 
-    public function load_post(Request $request)
-    {
-        return $request;
+    public function load_post(Request $request) {
+        $not_founds = [];
+
+        $A = explode('\r\n', $request->data);
+        foreach ($A as $v) {
+            return $v;
+        }
+
     }
 
 }
