c74b7cd1363dd8503e6425f83bba9705a55b7e55
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-20 18:28:16 +0700

    Paycount

--- resources/views/core/paycount/paycount.blade.php
+++ resources/views/core/paycount/paycount.blade.php
@@ -3,7 +3,7 @@
 <head>
 </head>
 <body>
-<form action="/paycount/load_post" method="POST">
+<form action="/paycount/load_post" method="POST" enctype="multipart/form-data">
     <textarea style="margin: 0 auto; width: 980px; height: 600px"></textarea>
     <div style="margin-top: 25px"><input type="submit" value="Отправить"/></div>
 </form>
