aa709dbd895ac52fecd9f95bcca17b155eb308e7
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-21 21:05:12 +0700

    phone

--- database/migrations/2025_12_20_175209_update_bot_users.php
+++ database/migrations/2025_12_20_175209_update_bot_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('bot_users', function (Blueprint $table) {
-            $table->string('phone', 15)->after('email')->nullable();
+            $table->text('phone')->after('email')->nullable();
         });
     }
 
