c004c4c1c73f39665b416b418a9f8a5dde29e6cc
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-23 18:06:58 +0700

    devtest

--- resources/views/core/paycount/change_number.blade.php
+++ resources/views/core/paycount/change_number.blade.php
@@ -6,8 +6,20 @@
 <form action="/paycount/change_number_post" method="POST" enctype="multipart/form-data">
     @csrf
 
-    <input type="text" style="margin: 0 auto; width: 250px" id="number" name="number">
-    <input type="text" style="margin: 0 auto; width: 500px" id="email" name="email">
+    <table>
+        <tr style="font-weight: bold">
+            <td>Number</td>
+            <td>Email</td>
+        </tr>
+        <tr>
+            <td>
+                <input type="text" style="margin: 0 auto; width: 250px" id="number" name="number">
+            </td>
+            <td>
+                <input type="text" style="margin: 0 auto; width: 500px" id="email" name="email">
+            </td>
+        </tr>
+    </table>
 
     <div style="margin-top: 25px"><input type="submit" value="Отправить"/></div>
 </form>
