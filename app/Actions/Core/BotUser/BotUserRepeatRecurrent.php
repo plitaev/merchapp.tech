5f215d22e54b5867eb2805e766490fb7b506d0c7
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-26 17:50:35 +0700

    BotUserRepeatRecurrent

---
+++ app/Actions/Core/BotUser/BotUserRepeatRecurrent.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Actions\Core\BotUser;
+
+class BotUserRepeatRecurrent
+{
+    public function handle(int $bot_user_id) {
+
+    }
+}
--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -15,6 +15,15 @@ use App\Models\Core\Pay;
 class DevTestController extends Controller
 {
     public function devtest() {
+        $pays = Pay::with('bot_user')
+            ->where('status', 1)
+            ->where('created_at', '>=', '2025-11-25 00:00:00')
+            ->where('created_at', '<=', '2025-12-25 23:59:59')
+            ->orderByDesc('created_at')
+            ->get();
+
+        return view('core.devtest.devtest', ['pays' => $pays]);
+
         return GetcourseWebhookTicket::orderByDesc('created_at')->get();
     }
 
