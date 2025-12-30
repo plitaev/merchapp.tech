b5425ac88ff34220419969ba6d41f26621e1a5c5
Evgenii Plita <evgeniiplita@gmail.com>
2025-12-30 20:23:33 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -16,6 +16,19 @@ class DevTestController extends Controller
 {
     public function devtest() {
 
+        $recurrent = 1;
+
+        $recurrents_all = BotUser::select('date_end')->where('date_end', '>=', date('Y-m', time())."-01")->where('recurrent', $recurrent)->get();
+        foreach ($recurrents_all as $recurrent_all) {
+            $Amys_users[date("m.Y", strtotime($recurrent_all->date_end))][] = 1;
+        }
+
+        $recurrent_dates = BotUser::select('date_end')->where('date_end', '>=', date('Y-m', time())."-01")->where('recurrent', $recurrent)->groupBy('date_end')->orderBy('date_end')->pluck('date_end')->toArray();
+        foreach ($recurrent_dates as $recurrent_date) {
+            $Amys[] = date("m.Y", strtotime($recurrent_date));
+        }
+
+        return $Amys;
     }
 
     public function paycounts() {
