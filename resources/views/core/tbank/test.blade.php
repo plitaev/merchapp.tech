1a47ba6642931cb9e2b94749d03ef0b779889b65
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-02 11:32:41 +0700

    TBank

---
+++ resources/views/core/tbank/test.blade.php
@@ -0,0 +1,13 @@
+<form name="TinkoffPayForm" id="TinkoffPayForm" onsubmit="pay(this); return false;" style="display:none">
+    <input class="tinkoffPayRow" type="hidden" id="terminalkey" name="terminalkey" value="{{env("TINKOFF_TERMINAY_KEY")}}">
+    <input class="tinkoffPayRow" type="hidden" name="frame" value="false">
+    <input class="tinkoffPayRow" type="hidden" name="language" value="ru">
+    <input class="tinkoffPayRow" type="text" value="1000" name="amount" required id="amount">
+    <input class="tinkoffPayRow" type="text" value="1" name="order" id="order">
+    <input class="tinkoffPayRow" type="text" value="Тестовый продукт" name="description" id="description">
+    <input class="tinkoffPayRow" type="text" value="Плита Евгений" name="name" id="name">
+    <input class="tinkoffPayRow" type="text" value="evgeniiplita@gmail.com" name="email" id="email">
+    <input class="tinkoffPayRow" type="hidden" id="receipt" name="receipt" value='{"Email":"evgeniiplita@gmail.com","EmailCompany":"evgeniiplita@gmail.com","Taxation":"usn_income","Items":[{"Name":"Тестовый продукт","Price":1000,"Quantity": 1.00,
+"Amount":100000,"PaymentMethod":"full_payment","PaymentObject":"service","Tax":"none"}]}'>
+    <input class="tinkoffPayRow" type="submit" value="Оплатить">
+</form>
