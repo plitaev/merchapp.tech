7887e016b91e84ed4c4f3b6d7b5eb98ca6a4a401
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-02 11:47:17 +0700

    TBank

--- resources/views/core/tbank/test.blade.php
+++ resources/views/core/tbank/test.blade.php
@@ -6,39 +6,39 @@
 <body>
 <form name="TinkoffPayForm" id="TinkoffPayForm" onsubmit="pay(this); return false;">
     <div style="margin: 25px">
-        <div>Ключ тестового терминала</div>
+        <div style="margin-bottom: 10px">Ключ тестового терминала</div>
         <input class="tinkoffPayRow" type="text" id="terminalkey" name="terminalkey" value="">
     </div>
     <div style="margin: 25px">
-        <div>Открывать во всплывающем окне</div>
+        <div style="margin-bottom: 10px">Открывать во всплывающем окне</div>
         <input class="tinkoffPayRow" type="text" name="frame" value="false">
     </div>
     <div style="margin: 25px">
-        <div>Язык</div>
+        <div style="margin-bottom: 10px">Язык</div>
         <input class="tinkoffPayRow" type="text" name="language" value="ru">
     </div>
     <div style="margin: 25px">
-        <div>Сумма</div>
+        <div style="margin-bottom: 10px">Сумма</div>
         <input class="tinkoffPayRow" type="text" value="1000" name="amount" required id="amount">
     </div>
     <div style="margin: 25px">
-        <div>ID заказа на стороне merchApp</div>
+        <div style="margin-bottom: 10px">ID заказа на стороне merchApp</div>
         <input class="tinkoffPayRow" type="text" value="1" name="order" id="order">
     </div>
     <div style="margin: 25px">
-        <div>Описание</div>
+        <div style="margin-bottom: 10px">Описание</div>
         <input class="tinkoffPayRow" type="text" value="Тестовый продукт" name="description" id="description">
     </div>
     <div style="margin: 25px">
-        <div>Фамилия и имя клиента</div>
+        <div style="margin-bottom: 10px">Фамилия и имя клиента</div>
         <input class="tinkoffPayRow" type="text" value="Плита Евгений" name="name" id="name">
     </div>
     <div style="margin: 25px">
-        <div>Email клиента</div>
+        <div style="margin-bottom: 10px">Email клиента</div>
         <input class="tinkoffPayRow" type="text" value="evgeniiplita@gmail.com" name="email" id="email">
     </div>
     <div style="margin: 25px">
-        <div>JSON чека (Проверьте совпадение цены, почты и ФИО клиента)</div>
+        <div style="margin-bottom: 10px">JSON чека (Проверьте совпадение цены, почты и ФИО клиента)</div>
         <textarea class="tinkoffPayRow" type="hidden" id="receipt" name="receipt" style="width: 500px; height: 500px">{"Email":"evgeniiplita@gmail.com","EmailCompany":"evgeniiplita@gmail.com","Taxation":"usn_income","Items":[{"Name":"Тестовый продукт","Price":1000,"Quantity": 1.00,"Amount":100000,"PaymentMethod":"full_payment","PaymentObject":"service","Tax":"none"}]}</textarea>
     </div>
     <input class="tinkoffPayRow" type="submit" value="Оплатить">
