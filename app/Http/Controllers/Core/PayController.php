a04878642b00d36628de0911d2fe797cedd8bbe6
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-02 13:04:47 +0700

    TBank

--- app/Http/Controllers/Core/PayController.php
+++ app/Http/Controllers/Core/PayController.php
@@ -62,8 +62,14 @@ class PayController
             $curl=curl_init();
             curl_setopt($curl,CURLOPT_URL,"https://securepay.tinkoff.ru/v2/Init");
             curl_setopt($curl,CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl,CURLOPT_POSTFIELDS,$json);
-            curl_setopt($curl,CURLOPT_HTTPHEADER,['Content-Type: application/json']);
+            curl_setopt($curl,CURLOPT_ENCODING, '');
+            curl_setopt($curl,CURLOPT_MAXREDIRS, 10);
+            curl_setopt($curl,CURLOPT_TIMEOUT, 0);
+            curl_setopt($curl,CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($curl,CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+            curl_setopt($curl,CURLOPT_CUSTOMREQUEST, 'POST');
+            curl_setopt($curl,CURLOPT_POSTFIELDS, $json);
+            curl_setopt($curl,CURLOPT_HTTPHEADER,['Content-Type: application/json', 'Accept: application/json']);
             $result = curl_exec($curl);
             curl_close($curl);
 
