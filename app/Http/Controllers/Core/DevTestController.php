224b0d853f491aeee506e80c46d2913ddabc6a31
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-03 12:32:41 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -16,18 +16,18 @@ use App\Models\Core\Pay;
 class DevTestController extends Controller
 {
     public function devtest() {
-        /*
-        $res = BotUser::where('bot_id', 2)->where('id', '>', 6)->get();
+
+        $res = BotUser::where('bot_id', 3)->where('ban', 0)->whereNull('date_end')->get();
         foreach ($res as $data) {
             BotUserBanSchedule::create(
                 [
                     'bot_user_id' => $data->id,
                     'run_status' => 0,
-                    'ban_datetime' => '2026-01-02 12:00:00'
+                    'ban_datetime' => '2026-01-03 07:28:00'
                 ]
             );
         }
-        */
+
     }
 
     public function paycounts() {
