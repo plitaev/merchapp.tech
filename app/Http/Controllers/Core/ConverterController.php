dfc65214b1133df301e054fd452fc75a973f6813
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-03 10:51:48 +0700

    converter

--- app/Http/Controllers/Core/ConverterController.php
+++ app/Http/Controllers/Core/ConverterController.php
@@ -156,7 +156,12 @@ class ConverterController extends Controller
 
     public function load_users_loverse(int $tunnel_id_from, int $bot_id_to) {
         $in_new = BotUser::select('telegram_chat_id')->where('bot_id', $bot_id_to)->pluck('telegram_chat_id')->toArray();
-        $new_users_ids = DB::table('loverse.bot_user_tunnel')->whereNotIn('chat_id', $in_new)->where('tunnel_id', $tunnel_id_from)->get();
+        $new_users_ids = DB::table('loverse.bot_user_tunnel')
+            ->select('telegram_chat_id')
+            ->whereNotIn('telegram_chat_id', $in_new)
+            ->where('tunnel_id', $tunnel_id_from)
+            ->pluck('telegram_chat_id')
+            ->toArray();
 
         $res = DB::table('loverse.telegram_chats')->whereIn('chat_id', $new_users_ids)->get();
         foreach ($res as $data) {
