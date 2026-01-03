8039368370d186952d6e49f4f81b1f5adf1e4195
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-03 10:35:42 +0700

    devtest

--- app/Http/Controllers/Core/TbankController.php
+++ app/Http/Controllers/Core/TbankController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\Core;
 
 use App\Actions\Core\Pay\PayMakeSuccessful;
 use App\Actions\Core\PaySystemCallback\PaySystemCallbackCreate;
+use App\Models\Core\Pay;
 
 class TbankController
 {
@@ -18,8 +19,13 @@ class TbankController
         if ($requestBody['Status']=='CONFIRMED') {
 
             if (isset($requestBody['OrderId']) && isset($requestBody['Token'])) {
-                $payment_method_id = (isset($requestBody['RebillId'])?$requestBody['RebillId']:NULL);
-                $payMakeSuccessful->handle($source, $requestBody['OrderId'], $requestBody['Token'], $payment_method_id, NULL);
+                $check_pay = Pay::where('id', $requestBody['OrderId'])->where('status', 1)->first();
+
+                if ($check_pay) {
+                    $payment_method_id = (isset($requestBody['RebillId'])?$requestBody['RebillId']:NULL);
+                    $payMakeSuccessful->handle($source, $requestBody['OrderId'], $requestBody['Token'], $payment_method_id, NULL);
+                }
+
             }
 
         }
