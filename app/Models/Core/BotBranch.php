821c7fc817dc3a756a9b211d85ef87494dbd9188
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2026-01-05 08:05:35 +0700

    добавление  Завершить акцию с отправкой сообщения

--- app/Models/Core/BotBranch.php
+++ app/Models/Core/BotBranch.php
@@ -2,11 +2,17 @@
 
 namespace App\Models\Core;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class BotBranch extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
+        'id',
         'bot_id',
         'bot_branch_type',
         'bot_branch_product_id',
@@ -27,4 +33,9 @@ class BotBranch extends Model
         'referal_program_max_referrals_count',
         'referal_program_product_id_for_referrer'
     ];
+
+    public function bot_message(): BelongsTo
+    {
+        return $this->belongsTo(BotMessage::class);
+    }
 }
--- resources/views/filament/resources/bot-resource/pages/bot-branch-admin.blade.php
+++ resources/views/filament/resources/bot-resource/pages/bot-branch-admin.blade.php
@@ -1,3 +1,4 @@
 <x-filament-panels::page>
     {{$this->form}}
+    <x-filament-actions::modals />
 </x-filament-panels::page>
