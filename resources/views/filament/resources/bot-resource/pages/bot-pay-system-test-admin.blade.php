b7168fa3d05b4ff66904a095a16f437ed78f6785
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-06 20:08:16 +0700

    BotBranchEndFix

--- resources/views/filament/resources/bot-resource/pages/bot-pay-system-test-admin.blade.php
+++
@@ -1,9 +0,0 @@
-
-<x-filament-panels::page>
-
-    <script src="https://securepay.tinkoff.ru/html/payForm/js/tinkoff_v2.js"></script>
-
-    <form wire:submit.prevent="submit" onsubmit="pay(this); return false;">{{$this->form}}</form>
-    
-
-</x-filament-panels::page>
