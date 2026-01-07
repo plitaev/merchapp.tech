72cd04593c4f70f0a71ccbc025a10545432c4867
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-07 18:13:50 +0700

    DateEndNewTest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -12,16 +12,17 @@ use Telegram\Bot\Api;
 
 use App\Models\Core\Pay;
 
+use App\Actions\Core\DateEnd\DateEndNew;
+
 class DevTestController extends Controller
 {
     public function devtest() {
+        $dateEndNew = new DateEndNew();
 
-        $users_td = Pay::select('bot_user_id')->where('product_id', 27)->where('status', 1)->where('created_at', '>=', '2026-01-03 10:00:00')->groupBy('bot_user_id')->pluck('bot_user_id')->toArray();
-        $fulls_td = Pay::select('bot_user_id')->whereNot('product_id', 27)->where('status', 1)->whereIn('bot_user_id', $users_td)->where('created_at', '>=', '2026-01-03 10:00:00')->whereNot('days', 1)->groupBy('bot_user_id')->get();
-        $olds_for_td = Pay::select('bot_user_id')->where('status', 1)->where('created_at', '<', '2026-01-03 10:00:00')->groupBy('bot_user_id')->pluck('bot_user_id')->toArray();
-        $users_td_without_olds = Pay::select('bot_user_id')->where('product_id', 27)->where('status', 1)->where('created_at', '>=', '2026-01-03 10:00:00')->whereNotIn('bot_user_id', $olds_for_td)->groupBy('bot_user_id')->pluck('bot_user_id')->toArray();
-
-        return count($users_td).' - '.count($fulls_td).' - '.count($users_td_without_olds);
-
+        $bot_users = BotUser::where('run_status', 0)->get();
+        foreach ($bot_users as $bot_user) {
+            $dateEndNew->handle($bot_user, 'Y-m-d');
+            BotUser::where('id', $bot_user->id)->update(['run_status' => 1]);
+        }
     }
 }
