a8703d452e2bc166943f9093723c5285cff10498
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-07 14:04:50 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -18,11 +18,11 @@ class DevTestController extends Controller
     public function devtest() {
 
         $users_td = Pay::select('bot_user_id')->where('product_id', 27)->where('status', 1)->where('created_at', '>=', '2026-01-03 10:00:00')->groupBy('bot_user_id')->pluck('bot_user_id')->toArray();
-        $fulls_td = Pay::select('bot_user_id')->whereNot('product_id', 27)->where('status', 1)->whereIn('bot_user_id', $users_td)->where('created_at', '>=', '2026-01-03 10:00:00')->groupBy('bot_user_id')->count();
+        $fulls_td = Pay::select('bot_user_id')->whereNot('product_id', 27)->where('status', 1)->whereIn('bot_user_id', $users_td)->where('created_at', '>=', '2026-01-03 10:00:00')->groupBy('bot_user_id')->get();
         $olds_for_td = Pay::select('bot_user_id')->where('status', 1)->where('created_at', '<', '2026-01-03 10:00:00')->groupBy('bot_user_id')->pluck('bot_user_id')->toArray();
         $users_td_without_olds = Pay::select('bot_user_id')->where('product_id', 27)->where('status', 1)->where('created_at', '>=', '2026-01-03 10:00:00')->whereNotIn('bot_user_id', $olds_for_td)->groupBy('bot_user_id')->pluck('bot_user_id')->toArray();
 
-        return count($users_td).' - '.$fulls_td.' - '.count($users_td_without_olds);
+        return count($users_td).' - '.count($fulls_td).' - '.count($users_td_without_olds);
 
     }
 
