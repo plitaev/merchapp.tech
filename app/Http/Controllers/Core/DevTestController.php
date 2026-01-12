b53c850b0fb2da03b30e9095b014324f674ff132
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-12 12:30:06 +0700

    KolchikiConverter

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -29,11 +29,12 @@ class DevTestController extends Controller
             BotUser::where('id', $bot_user->id)->update(['run_status' => 1]);
         }
         */
-
+        /*
         $bot_users = BotUser::where('date_end', '>=', date('Y-m-d', time()))->get();
         foreach ($bot_users as $bot_user) {
             BotUserPrice::create(['bot_user_id' => $bot_user->id, 'product_id' => 6, 'price' => 990]);
         }
+        */
 
     }
 
