0445ec7811d42b2440b2b653ea02534d4f8a9d71
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-16 17:54:08 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -21,7 +21,9 @@ class DevTestController extends Controller
 {
     public function devtest() {
         $fm = BotUser::select('telegram_chat_id')->where('bot_id', 2)->pluck('telegram_chat_id')->toArray();
-        return BotUser::select('telegram_chat_id')->where('bot_id', 25)->whereIn('telegram_chat_id', $fm)->pluck('telegram_chat_id')->toArray();
+        $elki = BotUser::select('bot_user_id')->where('bot_id', 25)->whereIn('telegram_chat_id', $fm)->pluck('bot_user_id')->toArray();
+        $elki_products_fm = Pay::whereIn('bot_user_id', $elki)->whereIn('product_id', [6, 9, 10, 22,23])->get();
+        return $elki_products_fm;
     }
 
     public function paycounts() {
