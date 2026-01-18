75cae8101a029f57b658b18e3fd709c8c6084ab5
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-18 18:08:27 +0700

    devtest

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -22,15 +22,14 @@ use App\Actions\Core\DateEnd\DateEnd;
 class DevTestController extends Controller
 {
     public function devtest() {
-        /*
+
         $dateEndNew = new DateEndNew();
 
-        $bot_users = BotUser::where('run_status', 0)->get();
+        $bot_users = BotUser::all();
         foreach ($bot_users as $bot_user) {
             $dateEndNew->handle($bot_user, 'Y-m-d');
-            BotUser::where('id', $bot_user->id)->update(['run_status' => 1]);
         }
-        */
+
         /*
         $result = [];
 
