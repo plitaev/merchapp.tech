7766e42c2c10d5f2a94fe06ae646fa476870dd60
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-19 21:10:44 +0700

    GetCourseUpdate

--- app/Actions/Core/BotUserPrice/BotUserPriceSet.php
+++ app/Actions/Core/BotUserPrice/BotUserPriceSet.php
@@ -7,14 +7,15 @@ use App\Models\Core\Product;
 
 class BotUserPriceSet
 {
-    public function handle($bot_user) {
+    public function handle($bot_user, $external_prices = []) {
         $products = Product::where('bot_id', $bot_user->bot_id)->get();
         $prices = BotUserPrice::select('product_id')->where('bot_user_id', $bot_user->id)->groupBy('product_id')->pluck('product_id')->toArray();
 
         foreach ($products as $product) {
 
             if (!in_array($product->id, $prices)) {
-                BotUserPrice::create(['bot_user_id' => $bot_user->id, 'product_id' => $product->id, 'price' => $product->price]);
+                $price = (isset($external_prices[$product->id])?$external_prices[$product->id]:$product->price);
+                BotUserPrice::create(['bot_user_id' => $bot_user->id, 'product_id' => $product->id, 'price' => $price]);
             }
 
         }
