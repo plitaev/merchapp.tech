5183d9dd6e95744c024246e30ab0dc3673a89d7f
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-20 13:44:12 +0700

    GetCourseUpdate

--- app/Http/Controllers/Core/GetCourseController.php
+++ app/Http/Controllers/Core/GetCourseController.php
@@ -19,10 +19,7 @@ class GetCourseController extends Controller
 {
     public function getcourse_webhook(int $product_id, int $getcourse_user_id, string $getcourse_user_name, string $email, int $is_recurrent, int $recurrent_status) {
         $getCourseWebhookCreate = new GetCourseWebhookCreate();
-        $payCreateByEmail = new PayCreateByEmail();
-
         $getCourseWebhookCreate->handle($product_id, $getcourse_user_id, $getcourse_user_name, $email, $is_recurrent, $recurrent_status);
-        $payCreateByEmail->handle($email, $product_id, $is_recurrent, $recurrent_status);
     }
 
     public function getcourse_cancel_webhook(int $getcourse_user_id, string $getcourse_user_name, string $email) {
