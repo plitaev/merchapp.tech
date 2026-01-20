d114fd12e45d40a11325475b87dc12478df4c993
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-20 13:04:06 +0700

    GetCourseUpdate

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -27,7 +27,7 @@ class DevTestController extends Controller
     public function devtest() {
         $paySystemCallbackCreate = new PaySystemCallbackCreate();
 
-        $res = GetcourseWebhook::where('created_at', '>', '2026-01-19 11:57:38')->get();
+        $res = GetcourseWebhook::all();
         foreach ($res as $data) {
             $A = [
                 'product_id' => $data->product_id,
