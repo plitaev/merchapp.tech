400883674d8083e96e7725e5ac71c547e397d402
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-20 13:41:02 +0700

    GetCourseUpdate

--- app/Http/Controllers/Core/GetCourseController.php
+++ app/Http/Controllers/Core/GetCourseController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 
 use App\Actions\Core\BotSendMessage\BotSendMessage;
 use App\Actions\Core\BotUser\BotUserGetByEmail;
+use App\Actions\Core\GetCourseWebhook\GetCourseWebhookCreate;
 use App\Actions\Core\GetCourseEventWebhook\GetCourseEventWebhookCreate;
 use App\Actions\Core\Pay\PayCreateByEmail;
 use App\Actions\Core\PaySystemCallback\PaySystemCallbackCreate;
@@ -18,9 +19,12 @@ class GetCourseController extends Controller
 {
     public function getcourse_webhook(int $product_id, int $getcourse_user_id, string $getcourse_user_name, string $email, int $is_recurrent, int $recurrent_status) {
 
+        $getCourseWebhookCreate = new GetCourseWebhookCreate();
         $paySystemCallbackCreate = new PaySystemCallbackCreate();
         $payCreateByEmail = new PayCreateByEmail();
+
         $payCreateByEmail->handle($email, $product_id, $is_recurrent, $recurrent_status);
+        $getCourseWebhookCreate->handle($product_id, $getcourse_user_id, $getcourse_user_name, $email, $is_recurrent, $recurrent_status);
 
         $A = [
             'product_id' => $product_id,
