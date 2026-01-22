710287171838e53482910545338a65d1d9b09f49
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-22 15:19:12 +0700

    VideoMiniApp

---
+++ database/migrations/2026_01_22_111620_ins_miniapp_class.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\Models\Core\MiniAppClass;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        MiniAppClass::create(['name' => 'Доступ к видео']);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
