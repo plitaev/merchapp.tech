1bdbe7abbda57ab3762cb0abe8a8a1de2df753ea
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-24 21:34:34 +0700

    VideoMiniApp

--- app/Filament/Resources/MiniAppVideos/Schemas/MiniAppVideoForm.php
+++ app/Filament/Resources/MiniAppVideos/Schemas/MiniAppVideoForm.php
@@ -35,25 +35,9 @@ class MiniAppVideoForm
                             ->disk('local')
                             ->directory('miniapp_banners')
                             ->disabled(auth()->user()->hasPermissionTo('Update:MiniAppPage')?false:true)
-                            ->visibility('public'),
-                        Actions::make([
-                            Action::make('Сохранить')
-                                ->action(function () {
-
-                                    Notification::make()
-                                        ->title('Данные успешно сохранены!')
-                                        ->success()
-                                        ->send();
-                                })
-                                ->visible(fn() => auth()->user()->can('Create:BotMessage')),
-
-                            Action::make('Cancel')
-                                ->action(function () {
-
-                                })
-                                ->label('Вернуться назад')
-                        ])
+                            ->visibility('public')
                     ]),
+
             ])->statePath('data');
     }
 }
