abee333e2232ea1e552ba50845f2958c0ebaf0b9
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-27 13:48:32 +0700

    VideoMiniApp

--- app/Filament/Resources/MiniAppVideos/Schemas/MiniAppVideoForm.php
+++ app/Filament/Resources/MiniAppVideos/Schemas/MiniAppVideoForm.php
@@ -35,25 +35,25 @@ class MiniAppVideoForm
                             ->disk('local')
                             ->directory('miniapp_banners')
                             ->disabled(auth()->user()->hasPermissionTo('Update:MiniAppPage')?false:true)
-                            ->visibility('public')
+                            ->visibility('public'),
+                        Actions::make([
+                            Action::make('Сохранить')
+                                ->action(function () {
+
+                                    Notification::make()
+                                        ->title('Данные успешно сохранены!')
+                                        ->success()
+                                        ->send();
+                                })
+                                ->visible(fn() => auth()->user()->can('Create:BotMessage')),
+
+                            Action::make('Cancel')
+                                ->action(function () {
+
+                                })
+                                ->label('Вернуться назад')
+                        ])
                     ]),
-                Actions::make([
-                    Action::make('Сохранить')
-                        ->action(function () {
-
-                            Notification::make()
-                                ->title('Данные успешно сохранены!')
-                                ->success()
-                                ->send();
-                        })
-                        ->visible(fn() => auth()->user()->can('Create:BotMessage')),
-
-                    Action::make('Cancel')
-                        ->action(function () {
-
-                        })
-                        ->label('Вернуться назад')
-                ])
             ])->statePath('data');
     }
 }
