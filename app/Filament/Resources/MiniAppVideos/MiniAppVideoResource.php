41ec6082cc94fab5da96cb79ae609c6631253f89
Evgenii Plita <evgeniiplita@gmail.com>
2026-01-27 20:19:53 +0700

    VideoApp

--- app/Filament/Resources/MiniAppVideos/MiniAppVideoResource.php
+++ app/Filament/Resources/MiniAppVideos/MiniAppVideoResource.php
@@ -11,6 +11,7 @@ use Filament\Notifications\Notification;
 
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Actions;
 use Filament\Schemas\Components\Section;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
