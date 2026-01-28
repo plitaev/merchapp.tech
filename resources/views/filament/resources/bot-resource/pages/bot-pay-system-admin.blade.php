49fce39ea1a7f681fceb611beda7370cea115ef3
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2026-01-28 15:24:51 +0700

    исправление бага навигации(готово только на вкладках для ботов)

--- resources/views/filament/resources/bot-resource/pages/bot-pay-system-admin.blade.php
+++ resources/views/filament/resources/bot-resource/pages/bot-pay-system-admin.blade.php
@@ -46,17 +46,7 @@ if($this->bot_id != 0) {
                             <path fill-rule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd"></path>
                         </svg>
 
-                        <a href="/admin/bots/{{$this->bot_id}}/messages" class="fi-breadcrumbs-item-label">Платежные системы</a>
-                    </li>
-                    <li class="fi-breadcrumbs-item">
-                        <svg class="fi-icon fi-size-md fi-breadcrumbs-item-separator fi-ltr" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                            <path fill-rule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd"></path>
-                        </svg>
-                        <svg class="fi-icon fi-size-md fi-breadcrumbs-item-separator fi-rtl" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                            <path fill-rule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd"></path>
-                        </svg>
-
-                        <a href="/admin/bots/{{$this->bot_id}}/{{$this->id}}/message-admin" class="fi-breadcrumbs-item-label">
+                        <a href="/admin/bots/{{$this->bot_id}}/pay-system-admin" class="fi-breadcrumbs-item-label">
                             @if($this->id == 0 ) Новая платежная система
                             @else Редактировать платежную систему
                             @endif
