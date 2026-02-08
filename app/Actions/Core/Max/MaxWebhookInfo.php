e9b9ff96176e5e43e8ffc88c6e2a8011913e480c
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2026-02-08 20:58:51 +0700

    max

--- app/Actions/Core/Max/MaxWebhookInfo.php
+++ app/Actions/Core/Max/MaxWebhookInfo.php
@@ -5,18 +5,12 @@ namespace App\Actions\Core\Max;
 class MaxWebhookInfo
 {
     public function handle(string $token, string $webhook) {
-        $webhook = str_replace('-', '/', $webhook);
+        $webhook=str_replace("-","/",$webhook);
 
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, "https://platform-api.max.ru/me/$token/getWebhookInfo?url=$webhook");
+        curl_setopt($curl, CURLOPT_URL, "https://api.max.ru/webhooks/".$webhook."/info");
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
         $result = curl_exec($curl);
-        if (curl_errno($curl)) {
-            error_log(curl_error($curl));
-            return null;
-        }
-
         curl_close($curl);
         return $result;
     }
