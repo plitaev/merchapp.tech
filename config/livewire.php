4299ff719b2a2ad1fb358caee0eee26384b19b9c
Evgenii Plita <evgeniiplita@gmail.com>
2026-02-11 16:28:27 +0700

    VideoAppUpdate

--- config/livewire.php
+++ config/livewire.php
@@ -2,6 +2,6 @@
 
 return [
     'temporary_file_upload' => [
-        'rules' => 'file|mimes:png,jpg,pdf,mp3,mp4|max:10485760',
+        'rules' => 'file|mimes:png,jpg,pdf,mp3,mp4,avi|max:10485760',
     ],
 ];
