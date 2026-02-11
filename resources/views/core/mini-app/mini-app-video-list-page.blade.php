14b5608d734c43a92fc53fdca17ae4f2c824a24b
Evgenii Plita <evgeniiplita@gmail.com>
2026-02-12 00:24:43 +0700

    VideoAppUpdate

--- resources/views/core/mini-app/mini-app-video-list-page.blade.php
+++ resources/views/core/mini-app/mini-app-video-list-page.blade.php
@@ -38,7 +38,7 @@
         };
     </script>
 
-    <div class="isolate overflow-y-scroll bg-indigo-50 h-[100vh]">
+    <div class="isolate overflow-y-scroll bg-white h-[100vh]">
         <div class="flow-root pb-24 sm:pb-32">
             <div id="username" class="mt-2 mb-2 ml-4 font-semibold text-xl"></div>
 
@@ -52,7 +52,7 @@
                         $bscount = $bscount + 1;
                     @endphp
 
-                    <a href="/miniapp/player/{{$video->id}}/MESSENGER_USER_ID" class="ref-to-player block my-2 rounded-md bg-indigo-200 px-3.5 py-2.5 text-md font-semibold text-indigo-600 border-1 border-indigo-600 shadow-xs hover:bg-indigo-100">
+                    <a href="/miniapp/player/{{$video->id}}/MESSENGER_USER_ID" class="ref-to-player block my-2 rounded-md bg-indigo-100 px-3.5 py-2.5 text-md font-semibold text-indigo-600 border-1 border-indigo-600 shadow-xs hover:bg-indigo-100">
                         {{$bscount}}. {{$video->name}}
                     </a>
 
