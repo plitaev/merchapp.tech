853dad081b5b06b14fe6cf127822f6fa6feae55b
Evgenii Plita <evgeniiplita@gmail.com>
2026-02-12 14:08:53 +0700

    VideoAppUpdate

--- resources/views/core/mini-app/mini-app-video-list-page.blade.php
+++ resources/views/core/mini-app/mini-app-video-list-page.blade.php
@@ -56,7 +56,7 @@
                         {{$bscount}}. {{$video->name}}
                     </a>
 
-                    <hr class="py-2.5 px-3.5 text-indigo-300 text-center">
+                    <hr class="block py-2.5 px-3.5 text-indigo-300 text-center">
 
                 @endforeach
             </div>
