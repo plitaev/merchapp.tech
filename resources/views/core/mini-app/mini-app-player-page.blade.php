bcc01ba86d5fc6cd01e085c65051c43371bee05f
Evgenii Plita <evgeniiplita@gmail.com>
2026-02-13 15:37:05 +0700

    VideoAppUpdate

--- resources/views/core/mini-app/mini-app-player-page.blade.php
+++ resources/views/core/mini-app/mini-app-player-page.blade.php
@@ -36,7 +36,7 @@
     <link rel='stylesheet' href="/css/vjs-quality-selector.css">
     <link rel='stylesheet' href="/css/videojs-seek-buttons.css">
 
-    <div class="m-2 bg-white">
+    <div class="p-8">
         <div class="flow-root pb-24 sm:pb-32">
             <div id="username" class="mt-2 mb-2 ml-4 font-semibold text-xl"></div>
 
