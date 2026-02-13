06517e5854f43f69b7482e7ddc985c045b88d5d9
Evgenii Plita <evgeniiplita@gmail.com>
2026-02-13 22:27:37 +0700

    VideoAppUpdate

--- resources/views/core/mini-app/mini-app-video-list-page.blade.php
+++ resources/views/core/mini-app/mini-app-video-list-page.blade.php
@@ -48,6 +48,11 @@
                     @endphp
 
                     <a href="/miniapp/player/{{$video->id}}/MESSENGER_USER_ID" class="ref-to-player px-3.5 block text-md font-semibold text-indigo-500">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#5a67d8" class="size-6">
+                            <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z" clip-rule="evenodd" />
+                        </svg>
+
                         {{$bscount}}. {{$video->name}}
                     </a>
 
