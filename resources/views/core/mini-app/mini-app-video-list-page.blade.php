c61e1fb602401f3a8f01a663ad1fdea1d13fbcfd
Evgenii Plita <evgeniiplita@gmail.com>
2026-02-13 23:07:04 +0700

    VideoAppUpdate

--- resources/views/core/mini-app/mini-app-video-list-page.blade.php
+++ resources/views/core/mini-app/mini-app-video-list-page.blade.php
@@ -49,11 +49,11 @@
 
                     <a href="/miniapp/player/{{$video->id}}/MESSENGER_USER_ID" class="ref-to-player flex justify-between align-center text-md font-semibold text-indigo-500">
 
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FB7185" class="w-[120px]">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FB7185" style="width: 25%">
                             <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z" clip-rule="evenodd" />
                         </svg>
 
-                        <span class="text-justify ml-5 w-3/4">{{$bscount}}. {{$video->name}}</span>
+                        <span class="text-justify ml-5 w-3/4" style="width: 75%">{{$bscount}}. {{$video->name}}</span>
                     </a>
 
                 @if ($bscount != count($videos))
