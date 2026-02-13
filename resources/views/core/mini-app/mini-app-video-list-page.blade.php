d234940ec63826cfd3ab915d34c39968a0f81fda
Evgenii Plita <evgeniiplita@gmail.com>
2026-02-13 22:37:31 +0700

    VideoAppUpdate

--- resources/views/core/mini-app/mini-app-video-list-page.blade.php
+++ resources/views/core/mini-app/mini-app-video-list-page.blade.php
@@ -53,7 +53,7 @@
                             <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z" clip-rule="evenodd" />
                         </svg>
 
-                        <span class="w-3/4 text-justify">{{$bscount}}. {{$video->name}}</span>
+                        <span class="w-3/4 text-justify ml-5">{{$bscount}}. {{$video->name}}</span>
                     </a>
 
                 @if ($bscount != count($videos))
