361198745ea0a5787b29d5db5e24d22a3e1f4556
Evgenii Plita <evgeniiplita@gmail.com>
2026-02-14 17:01:23 +0700

    VideoApp

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -29,10 +29,13 @@ class DevTestController extends Controller
     public function devtest() {
         $telegram = new Api('7427797340:AAEZd2WfiGalZ7EvAdRv2yCNkgTDwM7nVhY');
 
+        $chat_id = 247632034;
+
         $caption = '🆘 <b>КЛУБнички! Нам нужна ваша помощь!</b>%0A%0AПомогите протестировать приложение для клуба.%0A%0A❗️ Это ПРОБНАЯ версия. После теста оно будет удалено.%0A%0AПолная версия уже собирается, видео загружаются на серверы и через несколько дней доступ появится у всех участников Клуба%0A%0AЧто важно сейчас:%0A%0A1️⃣ Запустить приложение по этой ссылке:%0A<a href="https://t.me/ChudoClubVideoTestBot?startapp%0A%0A">https://t.me/ChudoClubVideoTestBot?startapp%0A%0A</a>2️⃣ Перейти в оба раздела и попробовать запустить видео%0A%0A3️⃣ Оставить отзыв о том, всё ли было понятно, как работает приложение и что работает или НЕ РАБОТАЕТ. %0AДля этого нажмите%0A👇👇👇👇%0A%0A<a href="https://t.me/+VTYfEQWOv8YyNGJi">ОСТАВИТЬ ОТЗЫВ</a>%0A%0AВаша помощь будет бесценна ❤️';
         $caption = urldecode($caption);
 
         $keyboard=["inline_keyboard"=>[
+            [["text" => "НАЧАТЬ ТЕСТИРОВАНИЕ", "url" => "https://t.me/ChudoClubVideoTestBot?startapp"]],
             [["text" => "ОСТАВИТЬ ОТЗЫВ", "url" => "https://t.me/+VTYfEQWOv8YyNGJi"]]
         ]];
         $keyboard=json_encode($keyboard,true);
@@ -61,12 +64,21 @@ class DevTestController extends Controller
 
 
         $A = [
-            'chat_id' => 247632034,
+            'chat_id' => $chat_id,
             'media' => json_encode($media),
-            'reply_parameters' => json_encode($keyboard)
         ];
 
         $telegram->sendMediaGroup($A);
+
+        //===
+
+        $A = [
+            'chat_id' => $chat_id,
+            'text' => urldecode('---------------------------------')
+        ];
+
+        $telegram->sendMessage($A);
+
     }
 
 }
