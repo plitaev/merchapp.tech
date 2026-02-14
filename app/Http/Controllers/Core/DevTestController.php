4a355e2c505cf9fa9d03223e5e4f6f1b36c3c9f8
Evgenii Plita <evgeniiplita@gmail.com>
2026-02-14 16:37:38 +0700

    VideoApp

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -30,6 +30,7 @@ class DevTestController extends Controller
         $telegram = new Api('7427797340:AAEZd2WfiGalZ7EvAdRv2yCNkgTDwM7nVhY');
 
         $caption = '🆘 <b>КЛУБнички! Нам нужна ваша помощь!</b>%0A%0AПомогите протестировать приложение для клуба.%0A%0A❗️ Это ПРОБНАЯ версия. После теста оно будет удалено.%0A%0AПолная версия уже собирается, видео загружаются на серверы и через несколько дней доступ появится у всех участников Клуба%0A%0AЧто важно сейчас:%0A%0A1️⃣ Запустить приложение по этой ссылке:%0A<a href="https://t.me/ChudoClubVideoTestBot?startapp%0A%0A">https://t.me/ChudoClubVideoTestBot?startapp%0A%0A</a>2️⃣ Перейти в оба раздела и попробовать запустить видео%0A%0A3️⃣ Оставить отзыв о том, всё ли было понятно, как работает приложение и что работает или НЕ РАБОТАЕТ. %0AДля этого нажмите%0A👇👇👇👇%0A%0A<a href="https://t.me/+VTYfEQWOv8YyNGJi">ОСТАВИТЬ ОТЗЫВ</a>%0A%0AВаша помощь будет бесценна ❤️';
+        $caption = urldecode($caption);
 
         $media = array(
             array(
