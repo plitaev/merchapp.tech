af3befd0a9e502558abad1cf369ff1e81b7e4e8f
Evgenii Plita <evgeniiplita@gmail.com>
2026-02-25 12:44:49 +0700

    BotMessageAdminFix

--- app/Http/Controllers/Core/DevTestController.php
+++ app/Http/Controllers/Core/DevTestController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers\Core;
 
 use App\Actions\Core\PaySystemCallback\PaySystemCallbackCreate;
+use App\Models\Core\BotMessage;
 use App\Models\Core\BotMessageButton;
 use App\Models\Core\GetcourseWebhook;
 use App\Models\Core\TelegramSendMessageSchedule;
@@ -28,34 +29,8 @@ use App\Models\Core\TelegramBanScheduleLogs;
 class DevTestController extends Controller
 {
     public function devtest() {
-
-        $bot_message_id = 7;
-        $id = 0;
-
-        $res = BotMessageButton::where('bot_message_id', $bot_message_id)->orderBy('pos')->get();
-        $last_pos = 0;
-
-        $k = [];
-        $v = [];
-
-        foreach ($res as $data) {
-            $k[] = $data->pos;
-            $v[] = $data->pos." - ".$data->name;
-            $last_pos = $data->pos;
-        }
-
-        if ($id == 0) {
-            $next_pos = $last_pos + 1;
-            $k[] = $next_pos;
-            $v[] = $next_pos.' - Новая кнопка';
-        } else {
-            $next_pos = 1;
-        }
-
-        $result = array_combine($k, $v);
-
-        return [$result, $next_pos];
-
+        $data = BotMessage::with('bot_message_type')->with('bot')->find(81)->toArray();
+        return $data;
     }
 
 }
