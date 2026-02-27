f0306c8de924e8eb34018d84e70db81915ea38d2
Evgenii Plita <evgeniiplita@gmail.com>
2026-02-27 16:08:18 +0700

    fix

--- app/Actions/Core/Prodamus/ProdamusMakeRecurrent.php
+++ app/Actions/Core/Prodamus/ProdamusMakeRecurrent.php
@@ -69,6 +69,8 @@ class ProdamusMakeRecurrent
         $responce = curl_exec($curl);
         curl_close($curl);
 
+        return $responce;
+
         $responce_array = json_decode($responce, true);
 
         if ($responce_array['success'] == true) {
