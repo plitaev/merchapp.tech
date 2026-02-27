02f943d6ee15f82aa6084144c31ee4d9548084f6
Evgenii Plita <evgeniiplita@gmail.com>
2026-02-27 20:35:26 +0700

    fix

---
+++ database/migrations/2026_02_27_163304_update_mini_app_pages.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('mini_app_pages', function (Blueprint $table) {
+            $table->boolean('back_button')->after('url')->default(0);
+            $table->text('back_button_url')->after('back_button')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
