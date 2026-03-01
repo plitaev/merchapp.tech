33165b7f573e7fd49a491f6498149deead843562
Evgenii Plita <evgeniiplita@gmail.com>
2026-03-01 20:33:29 +0700

    fix

--- resources/views/core/mini-app/mini-app-player-page.blade.php
+++ resources/views/core/mini-app/mini-app-player-page.blade.php
@@ -10,9 +10,10 @@
         window.onload = function() {
             let app = window.Telegram.WebApp;
             app.BackButton.show();
+            let id = app.initDataUnsafe.user.id;
 
             app.BackButton.onClick(function() {
-                window.location.href="{{env("APP_URL")}}/{{$back_page}}";
+                window.location.href="{{env("APP_URL")}}/{{$back_page}}?telegram_chat_id="+id;
             });
 
             let first_name = app.initDataUnsafe.user.first_name;
