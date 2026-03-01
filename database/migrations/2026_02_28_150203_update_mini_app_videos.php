44f67d469eb80c6f74f72e9959b2f76fcc74745a
Evgenii Plita <evgeniiplita@gmail.com>
2026-03-01 12:27:16 +0700

    fix video

---
+++ database/migrations/2026_02_28_150203_update_mini_app_videos.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('mini_app_pages', function (Blueprint $table) {
+            $table->unsignedBigInteger('mini_app_page_right_id', 255)->after('url')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
