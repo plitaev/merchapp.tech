c7db4642bfb314e121ab1bfc6634b4a2e55dcdb2
Evgenii Plita <evgeniiplita@gmail.com>
2026-03-08 13:43:07 +0700

    VideoBannerCloseByRights

--- app/Filament/Resources/MiniAppBanners/Pages/AdminMiniAppBanner.php
+++ app/Filament/Resources/MiniAppBanners/Pages/AdminMiniAppBanner.php
@@ -89,14 +89,14 @@ class AdminMiniAppBanner extends Page implements HasForms, HasTable
 
                 $mini_apps_video = MiniApp::select('id')->where('bot_id', $mini_app_page->miniapp->bot_id)->where('class_id', 2)->pluck('id')->toArray();
 
-                Notification::make()
-                    ->title(implode(',', $mini_apps_video))
-                    ->success()
-                    ->send();
-
                 if (count($mini_apps_video) > 0) {
 
                     $mini_apps_video_pages = MiniAppPage::select('id')->whereIn('mini_app_id', $mini_apps_video)->pluck('id')->toArray();
+
+                    Notification::make()
+                        ->title(implode(',', $mini_apps_video_pages))
+                        ->success()
+                        ->send();
 
                     if (count($mini_apps_video_pages) > 0) {
                         $this->video_pages_on_this_bot = $mini_apps_video_pages;
