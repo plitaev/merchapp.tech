feb2bbdc810dc94441bc175cdb0399cd6396d62e
Evgenii Plita <evgeniiplita@gmail.com>
2026-03-08 18:02:47 +0700

    VideoBannerCloseByRights

--- app/Http/Controllers/Core/MiniAppPageController.php
+++ app/Http/Controllers/Core/MiniAppPageController.php
@@ -46,8 +46,8 @@ class MiniAppPageController
 
         if ($mini_app_page->miniapp->class_id == 2) {
 
-            $access = $miniAppVideoCheckAccess->handle($bot_user, $mini_app, $mini_app_page);
-            if ($access) return $access;
+            $restrict_access = $miniAppVideoCheckAccess->handle($bot_user, $mini_app, $mini_app_page);
+            if ($restrict_access) return $restrict_access;
 
             $video_ids = MiniAppVideoLinkPage::select('mini_app_video_id')->where('mini_app_page_id', $mini_app_page->id)->pluck('mini_app_video_id')->toArray();
             $videos = MiniAppVideo::whereIn('id', $video_ids)->get();
