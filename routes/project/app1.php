fe79031f7c5bb7754acc0f93ef7e8d2d9f050a4c
Evgenii Plita <evgeniiplita@gmail.com>
2026-03-12 23:41:05 +0700

    app1

--- routes/project/app1.php
+++ routes/project/app1.php
@@ -4,6 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Project\App1Controller;
 
 Route::controller(App1Controller::class)->group(function() {
-    Route::get('/app1', 'app1');
     Route::get('/app1_guides', 'app1_guides');
 });
