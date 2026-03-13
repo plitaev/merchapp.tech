e549ec88e8f0ac202be55981b948df9d6a9128db
Evgenii Plita <evgeniiplita@gmail.com>
2026-03-13 17:57:17 +0700

    VideoPageManagement

--- resources/views/filament/resources/mini-app-video-resource/pages/admin-mini-app-video-link-pages.blade.php
+++ resources/views/filament/resources/mini-app-video-resource/pages/admin-mini-app-video-link-pages.blade.php
@@ -5,7 +5,7 @@
     </style>
 
     <div class="fi-header flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
-        <h1 class="fi-header-heading text-2xl font-bold tracking-tight text-gray-950 dark:text-white sm:text-3xl">Забаненные пользователи</h1>
+        <h1 class="fi-header-heading text-2xl font-bold tracking-tight text-gray-950 dark:text-white sm:text-3xl">Страницы</h1>
         <div class="flex shrink-0 items-center gap-3">
             <x-filament::modal id="add-page-modal">
                 <x-slot name="trigger">
