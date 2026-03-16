9d9b9fb32c8f6bb92a5417e98ef82059d6154eb6
Evgenii Plita <evgeniiplita@gmail.com>
2026-03-16 12:26:33 +0700

    MaxFix

--- app/Actions/Core/Max/MaxWebhookInfo.php
+++ app/Actions/Core/Max/MaxWebhookInfo.php
@@ -4,7 +4,7 @@ namespace App\Actions\Core\Max;
 
 class MaxWebhookInfo
 {
-    public function handle(string $token, string $webhook) {
+    public function handle(?string $token, ?string $webhook) {
 
         $webhook=str_replace("-","/",$webhook);
 
