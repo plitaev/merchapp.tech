ad55a4a3984aabdc38822fc599e9680f12d8982e
Evgenii Plita <evgeniiplita@gmail.com>
2026-03-19 16:51:07 +0700

    MaxSendMessage

--- app/Http/Controllers/Project/ClubAccessController.php
+++ app/Http/Controllers/Project/ClubAccessController.php
@@ -222,7 +222,12 @@ class ClubAccessController extends Controller
             //== Запускаем основной скрипт клуба
             //== Проверяем, получал ли юзер приветственное сообщение
 
-            $botWelcomeMessage->handle($bot_user); //== Обрабатываем первичное стартовое сообщение (до ввода имени)
+            if ($messenger == 'max') {
+                return $botWelcomeMessage->handle($bot_user); //== Обрабатываем первичное стартовое сообщение (до ввода имени)
+            } else {
+                $botWelcomeMessage->handle($bot_user); //== Обрабатываем первичное стартовое сообщение (до ввода имени)
+            }
+
             $botHandName->handle($bot_user, $webhook); //== Обрабатываем HandName - вручную введенное юзером имя
             $botEighteen->handle($bot_user); //== Обрабатываем подтверждение 18 лет
             $botMainMenuMessage->handle($bot_user); //== Обрабатываем сообщение с главным меню
