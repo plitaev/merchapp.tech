c96e49b4b3a505bb0a22400b61689c1274f141e4
Evgenii Plita <evgeniiplita@gmail.com>
2026-03-19 16:59:38 +0700

    MaxSendMessage

--- app/Actions/Core/BotSendMessage/BotSendMessage.php
+++ app/Actions/Core/BotSendMessage/BotSendMessage.php
@@ -23,8 +23,6 @@ class BotSendMessage
 
         $bot_message_appointment = BotMessageAppointment::where('alias', $bot_message_appointment_name)->first();
 
-        return 'ok';
-
         if ($bot_message_appointment) {
 
             if (isset($bot_user->bot_branch_id)) {
