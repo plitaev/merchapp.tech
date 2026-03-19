9978c2b4c157a74e3a2d786f8b6eb624be858105
Evgenii Plita <evgeniiplita@gmail.com>
2026-03-19 17:00:09 +0700

    MaxSendMessage

--- app/Actions/Core/BotSendMessage/BotSendMessage.php
+++ app/Actions/Core/BotSendMessage/BotSendMessage.php
@@ -55,8 +55,8 @@ class BotSendMessage
 
                 //== Отправляем само сообщение
                 if ($bot_user->max_user_id) {
-                    return 'max';
                     $message = $maxSendMessage->handle($bot_user, $bot_message->id, $bot_message_appointment_name);
+                    return $message;
                 }
 
                 if ($bot_user->telegram_chat_id) {
