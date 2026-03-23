a21ac6f792f2344e52c2f9c6603a16d233fa78b4
Evgenii Plita <evgeniiplita@gmail.com>
2026-03-23 13:37:27 +0700

    MaxFileUpload

--- app/Actions/Core/BotSendMessage/BotSendMessage.php
+++ app/Actions/Core/BotSendMessage/BotSendMessage.php
@@ -78,9 +78,6 @@ class BotSendMessage
                 //== Проверяем паузу, и отправляем, если есть
                 if ($bot_message->pause_after_message > 0) sleep($bot_message->pause_after_message);
 
-                //== Убираем листенер перехода к платежу, если это SUCCESS_MESSAGE
-                if ($bot_message_appointment_name == 'SYS_SUCCESS_MESSAGE') $botUserSetListener->handle('sys_go_to_pay', 0, $bot_user->id);
-
                 //== Завершаем отправку
                 return $message;
 
