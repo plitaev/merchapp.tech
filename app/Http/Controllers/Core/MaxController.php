67328f89925d1e487c95d2aa9c987cda11b3c710
Evgenii Plita <evgeniiplita@gmail.com>
2026-03-25 01:11:03 +0700

    MaxFileUpload

--- app/Http/Controllers/Core/MaxController.php
+++ app/Http/Controllers/Core/MaxController.php
@@ -47,7 +47,7 @@ class MaxController
             $upload_url = $maxQuery->handle($bot_message->bot, 'POST', 'uploads', [], true, ['type' => $type]);
             $upload_url = $upload_url['url'];
 
-            $upload_url = $maxQuery->handle($bot_message->bot, 'POST', 'uploads', [], true, ['type' => 'file']);
+            $upload_url = $maxQuery->handle($bot_message->bot, 'POST', 'uploads', [], true, ['type' => $type]);
             $upload_url = $upload_url['url'];
 
             $cfile = curl_file_create(public_path().'/content/bot_message_videos/01KMFDV813EMZMHCJ1EV86CXVE.mp4', 'video/mp4', '01KMFDV813EMZMHCJ1EV86CXVE.mp4');
