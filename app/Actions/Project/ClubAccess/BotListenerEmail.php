79aa5b095587ae33b318cfe1e1ed0e06c46c7cd6
Evgenii Plita <evgeniiplita@gmail.com>
2026-03-30 16:56:51 +0700

    fix

--- app/Actions/Project/ClubAccess/BotListenerEmail.php
+++ app/Actions/Project/ClubAccess/BotListenerEmail.php
@@ -58,6 +58,8 @@ class BotListenerEmail
 
                             $bot_user = BotUser::find($bot_user->id);
 
+                            return 'kkk';
+
                             if ($date_end > date('Y-m-d', time())) {
                                 $botSendMessage->handle($bot_user, 'SYS_SUCCESS_MESSAGE');
 
