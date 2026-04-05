6c0ce6c0a359d9b35a45de18c683bf1071ce8982
Evgenii Plita <evgeniiplita@gmail.com>
2026-04-05 22:49:29 +0700

    MaxStartParams

--- app/Actions/Core/Max/MaxGetChatIDFromWebhook.php
+++ app/Actions/Core/Max/MaxGetChatIDFromWebhook.php
@@ -6,9 +6,8 @@ class MaxGetChatIDFromWebhook
 {
     public function handle($webhook) {
 
-        if (isset($webhook['user']['user_id'])) {
-            $user_id=$webhook['user']['user_id'];
-        }
+        if (isset($webhook['user']['user_id'])) $user_id=$webhook['user']['user_id'];
+        if (!isset($user_id) && isset($webhook['message']['sender']['user_id'])) $user_id=$webhook['message']['sender']['user_id'];
 
         if (!isset($user_id)) return 0;
 
