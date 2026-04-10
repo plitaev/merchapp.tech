78d5d3fba4130e1a628acbaab3831598d641ef91
Evgenii Plita <evgeniiplita@gmail.com>
2026-04-10 17:34:13 +0700

    devtest

--- app/Actions/Core/Telegram/TelegramWebhookInfo.php
+++ app/Actions/Core/Telegram/TelegramWebhookInfo.php
@@ -5,10 +5,11 @@ namespace App\Actions\Core\Telegram;
 class TelegramWebhookInfo
 {
     public function handle(?string $token, ?string $webhook) {
+        //https://api.telegram.org/bot".$token."/getWebhookInfo?url=".$webhook
         $webhook=str_replace("-","/",$webhook);
 
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, "https://api.telegram.org/bot".$token."/getWebhookInfo?url=".$webhook);
+        curl_setopt($curl, CURLOPT_URL, "https://https://api-endpoint3.loverse.me/bot".$token."/getWebhookInfo?url=".$webhook);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $result = curl_exec($curl);
         curl_close($curl);
