5ecbf3b880947d0a057c1928abbad27b9d283012
Evgenii Plita <evgeniiplita@gmail.com>
2026-04-10 17:43:44 +0700

    devtest

--- app/Actions/Core/Telegram/TelegramSetWebhook.php
+++ app/Actions/Core/Telegram/TelegramSetWebhook.php
@@ -7,7 +7,7 @@ class TelegramSetWebhook
     public function handle(int $id, string $token, string $webhook) {
 
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, "https://api.telegram.org/bot".$token."/setWebhook?url=".env('APP_URL').'/telegram/webhook/'.$id.'/'.$webhook);
+        curl_setopt($curl, CURLOPT_URL, "https://api-endpoint3.loverse.me/bot".$token."/setWebhook?url=".env('APP_URL').'/telegram/webhook/'.$id.'/'.$webhook);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $result = curl_exec($curl);
         curl_close($curl);
