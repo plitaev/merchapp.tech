c31da9829a56c11a1907d9deb8d150205685c057
Evgenii Plita <evgeniiplita@gmail.com>
2026-04-10 17:49:04 +0700

    devtest

--- app/Http/Controllers/Project/ClubAccessController.php
+++ app/Http/Controllers/Project/ClubAccessController.php
@@ -144,9 +144,9 @@ class ClubAccessController extends Controller
             //== Если это /start, тут обрабатываем старт
             if ($Astart[0] == "/start") {
 
-                return 'test';
-
                 if (count($Astart) == 2) {
+
+                    return 'test 2';
 
                     $branch_data = base64_decode($Astart[1]);
                     $branch_data = explode("|", $branch_data);
