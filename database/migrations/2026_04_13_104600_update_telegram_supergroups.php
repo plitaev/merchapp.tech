a4058d72c78190ee4fd5c4d35bc45df79124764b
Evgenii Plita <evgeniiplita@gmail.com>
2026-04-13 10:49:51 +0700

    UpgradeTelegramSupergroups

--- database/migrations/2026_04_13_104600_update_telegram_supergroups.php
+++ database/migrations/2026_04_13_104600_update_telegram_supergroups.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('telegram_supergroups', function (Blueprint $table) {
-            $table->BigInteger('max_id')->change();
+            $table->BigInteger('max_id')->nullable()->change();
         });
     }
 
