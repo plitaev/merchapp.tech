016ce1a957585e807a6c5ba7fcb2f13223621449
Evgenii Plita <evgeniiplita@gmail.com>
2026-04-14 21:58:14 +0700

    WebhookFix

--- app/Actions/Core/Telegram/TelegramDeleteWebhook.php
+++ app/Actions/Core/Telegram/TelegramDeleteWebhook.php
@@ -7,7 +7,7 @@ class TelegramDeleteWebhook
     public function handle(string $token, string $webhook) {
 
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, "https://api.telegram.org/bot".$token."/deleteWebhook?url=".env('APP_URL').'/telegram/webhook/'.$webhook);
+        curl_setopt($curl, CURLOPT_URL, "https://api-endpoint5.loverse.me/bot".$token."/deleteWebhook?url=".env('APP_URL').'/telegram/webhook/'.$webhook);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $result = curl_exec($curl);
         curl_close($curl);
