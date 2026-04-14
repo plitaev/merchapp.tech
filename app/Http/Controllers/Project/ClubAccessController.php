2134d644ac8643acbebfc20b70469bfaf34daa6f
Evgenii Plita <evgeniiplita@gmail.com>
2026-04-14 17:56:37 +0700

    MaxNeedsPrivacyChange

--- app/Http/Controllers/Project/ClubAccessController.php
+++ app/Http/Controllers/Project/ClubAccessController.php
@@ -166,7 +166,7 @@ class ClubAccessController extends Controller
                 }
 
                 if ($bot_user->date_end != NULL && $bot_user->date_end > date('Y-m-d', time())) {
-                    $botSendMessage->handle($bot_user, 'SYS_SUCCESS_MESSAGE');
+                    return $botSendMessage->handle($bot_user, 'SYS_SUCCESS_MESSAGE');
                     die();
                 }
 
