ef7e8601e1b4a18fb1c3666ead9e8fa2674b252e
Evgenii Plita <evgeniiplita@gmail.com>
2026-04-17 19:25:16 +0700

    MaxApp

--- resources/views/core/mini-app/mini-app-banner-page.blade.php
+++ resources/views/core/mini-app/mini-app-banner-page.blade.php
@@ -11,23 +11,12 @@
 
 @section('content')
 
-
-    <script src="https://st.max.ru/js/max-web-app.js"></script>
-
-    <script>
-        window.onload = function() {
-            var max_id = window.WebApp.initDataUnsafe.user.id;
-            document.getElementById('max-test').innerHTML = max_id;
-        };
-    </script>
+    <script src="{{env('APP_URL')}}/js/telegram-web-app.js"></script>
 
     <script>
         window.onload = function() {
             let app = window.Telegram.WebApp;
             let id = app.initDataUnsafe.user.id;
-
-            let max_id = window.WebApp.initDataUnsafe.user.id;
-            document.getElementById('max-test').innerHTML = max_id;
 
             const items = document.querySelectorAll(".ref-to-banner");
 
@@ -65,8 +54,6 @@
     <div class="isolate overflow-y-scroll bg-[#f1f1f1] h-[100vh]">
         <div class="flow-root pb-24 sm:pb-32">
             <div id="username" class="mt-2 mb-2 ml-4 font-semibold text-xl"></div>
-
-            <div id="max-test">max-test</div>
 
             {{------}}
 
