e67d9620ced85fb3c0fba0aa10600712a1d9efd9
Evgenii Plita <evgeniiplita@gmail.com>
2026-04-23 13:36:21 +0700

    ref_from_telegram_to_max

--- resources/views/core/devtest/devtest.blade.php
+++ resources/views/core/devtest/devtest.blade.php
@@ -32,7 +32,7 @@
             <td>{{$bot_user->first_name}}</td>
             <td>{{$bot_user->last_name}}</td>
             <td>{{$bot_user->email}}</td>
-            <td>{{$bot_user->date_end}}</td>
+            <td>{{date('d.m.Y', strtotime($bot_user->date_end))}}</td>
             <td>{{$bot_user->ref_from_telegram_to_max}}</td>
         </tr>
     @endforeach
