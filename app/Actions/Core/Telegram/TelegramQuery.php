bb91ee51e94b86d1acaf32957ef2583846208094
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2026-04-27 18:27:19 +0700

    devtesttelegram

--- app/Actions/Core/Telegram/TelegramQuery.php
+++ app/Actions/Core/Telegram/TelegramQuery.php
@@ -6,60 +6,89 @@ use App\Models\Core\TelegramWebhook;
 
 class TelegramQuery
 {
-    public function handle($bot, string $method, string $api_function, $request_data, bool $return_array = true, array $post_add_to_url = []) {
+    // public function handle($bot,string $api_function, $request_data, bool $return_array = true, array $post_add_to_url = []) {
+//        $headers = [
+//            'Content-Type: application/json',
+//            'Authorization: '.$bot->Telegram_token
+//        ];
+//
+//        $curl = curl_init();
+//
+//        (string) $api_url = 'https://api-endpoint5.loverse.me/' . $api_function;
+//
+//        if ($method != 'GET' && $method != 'POST') {
+//            $add_to_url = [];
+//
+//            foreach ($request_data as $key => $value) {
+//                $item = $key . "=" . $value;
+//                $add_to_url[] = $item;
+//            }
+//
+//            $api_url.= '?' . implode('&', $add_to_url);
+//        }
+//
+//
+//        if ($method == "POST" && count($post_add_to_url) > 0) {
+//            $add_to_url = [];
+//
+//            foreach ($post_add_to_url as $key => $value) {
+//                $item = $key . "=" . $value;
+//                $add_to_url[] = $item;
+//            }
+//
+//            $api_url.= '?' . implode('&', $add_to_url);
+//        }
+//
+//        curl_setopt($curl, CURLOPT_URL, $api_url);
+//
+//        if ($method == 'POST') {
+//            curl_setopt($curl, CURLOPT_POST, true);
+//            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($request_data));
+//        }
+//
+//        if ($method != 'GET9' && $method != 'POST') {
+//            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+//        }
+//
+//        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+//        curl_setopt($curl, CURLOPT_HEADER, true);
+//        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+//        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+//        $response = curl_exec($curl);
+//        curl_close($curl);
+//
+//        $response = explode('includeSubdomains', $response);
+//        $response = $response[0];
+//
+//        return ($return_array?json_decode($response, true):$response);
+    function handle($bot, string $api_function, array $params = []): array|false
+    {
         $headers = [
             'Content-Type: application/json',
             'Authorization: '.$bot->Telegram_token
         ];
 
-        $curl = curl_init();
+        $api_url = "https://api.telegram.org/bot".$bot->telegram_token."/" . $api_function;
 
-        (string) $api_url = 'https://api-endpoint5.loverse.me/' . $api_function;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $api_url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 
-        if ($method != 'GET' && $method != 'POST') {
-            $add_to_url = [];
+        $response = curl_exec($ch);
 
-            foreach ($request_data as $key => $value) {
-                $item = $key . "=" . $value;
-                $add_to_url[] = $item;
-            }
-
-            $api_url.= '?' . implode('&', $add_to_url);
+        if ($response === false) {
+            $errno = curl_errno($ch);
+            $error = curl_error($ch);
+            error_log("cURL error $errno: $error");
+            curl_close($ch);
+            return false;
         }
 
+        curl_close($ch);
 
-        if ($method == "POST" && count($post_add_to_url) > 0) {
-            $add_to_url = [];
-
-            foreach ($post_add_to_url as $key => $value) {
-                $item = $key . "=" . $value;
-                $add_to_url[] = $item;
-            }
-
-            $api_url.= '?' . implode('&', $add_to_url);
-        }
-
-        curl_setopt($curl, CURLOPT_URL, $api_url);
-
-        if ($method == 'POST') {
-            curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($request_data));
-        }
-
-        if ($method != 'GET' && $method != 'POST') {
-            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-        }
-
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HEADER, true);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        $response = curl_exec($curl);
-        curl_close($curl);
-
-        $response = explode('includeSubdomains', $response);
-        $response = $response[0];
-
-        return ($return_array?json_decode($response, true):$response);
+        return json_decode($response, true);
     }
 }
