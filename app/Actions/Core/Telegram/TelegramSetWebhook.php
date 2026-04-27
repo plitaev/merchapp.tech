8592313e2b8ea55cbc3aacc7ca50cf2c485f09cd
Evgenii Plita <evgeniiplita@gmail.com>
2026-04-27 09:13:51 +0700

    SetProxyTest

--- app/Actions/Core/Telegram/TelegramSetWebhook.php
+++ app/Actions/Core/Telegram/TelegramSetWebhook.php
@@ -7,7 +7,7 @@ class TelegramSetWebhook
     public function handle(int $id, string $token, string $webhook) {
 
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, "https://api-endpoint5.loverse.me/bot".$token."/setWebhook?url=".env('APP_URL').'/telegram/webhook/'.$id.'/'.$webhook);
+        curl_setopt($curl, CURLOPT_URL, "https://api-endpoint5.loverse.me/bot".$token."/setWebhook?url=".(isset(env('APP_PROXY_URL'))?env('APP_PROXY_URL'):env('APP_URL')).'/telegram/webhook/'.$id.'/'.$webhook);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $result = curl_exec($curl);
         curl_close($curl);
