2a86e67136698b9bbfc1f24e1d3d6a8a278a755e
Evgenii Plita <evgeniiplita@gmail.com>
2026-04-27 18:04:30 +0700

    devtest

--- resources/views/core/devtest/devtest.blade.php
+++ resources/views/core/devtest/devtest.blade.php
@@ -2,11 +2,12 @@
 <table>
     @foreach ($pays as $pay)
         <tr>
-            <td>{{$pay->bot_user->first_name}}</td>
-            <td>{{$pay->bot_user->last_name}}</td>
-            <td>{{$pay->bot_user->email}}</td>
-            <td>{{$pay->price}}</td>
-            <td>{{date('d.m.Y', strtotime($pay->created_at))}}</td>
+            <td>{{$bot_user->first_name}}</td>
+            <td>{{$bot_user->last_name}}</td>
+            <td>{{$bot_user->email}}</td>
+            <td>{{$bot_user->date_end}}</td>
+            <td>{{date('d.m.Y', strtotime($bot_user->date_end))}}</td>
+            <td>{{$bot_user->ref_from_telegram_to_max}}</td>
         </tr>
     @endforeach
 </table>
