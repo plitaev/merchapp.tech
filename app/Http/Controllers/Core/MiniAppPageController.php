304a0124e1d614fee89fdfa5584868809f668f34
Evgenii Plita <evgeniiplita@gmail.com>
2026-05-06 15:55:37 +0700

    MaxApp

--- app/Http/Controllers/Core/MiniAppPageController.php
+++ app/Http/Controllers/Core/MiniAppPageController.php
@@ -69,7 +69,7 @@ class MiniAppPageController
 
         if ($mini_app_page->miniapp->class_id == 2) {
 
-            return 'aa';
+            return $telegram_chat_id.' | '.$max_user_id;
 
             $restrict_access = $miniAppVideoCheckAccess->handle($bot_user, $mini_app, $mini_app_page);
             if ($restrict_access) return $restrict_access;
