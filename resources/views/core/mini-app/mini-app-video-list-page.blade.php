84f005284ac48fcf3827d5f2ae2911f825f705da
Evgenii Plita <evgeniiplita@gmail.com>
2026-05-06 18:17:42 +0700

    MaxApp

--- resources/views/core/mini-app/mini-app-video-list-page.blade.php
+++ resources/views/core/mini-app/mini-app-video-list-page.blade.php
@@ -58,15 +58,8 @@
                 app.BackButton.onClick(function() {
                     window.location.href="{{$mini_app_page->back_button_url}}?platform=max&max_user_id="+id;
                 });
-
-
-
                 @else
-
                 app.BackButton.hide();
-
-
-
                 @endif
 
                 let first_name = app.initDataUnsafe.user.first_name;
@@ -89,12 +82,6 @@
     <div class="isolate overflow-y-scroll bg-white h-[100vh]">
         <div class="flow-root pb-24 sm:pb-32">
             <div id="username" class="mt-2 mb-5 ml-4 font-semibold text-xl"></div>
-
-            @if ($mini_app_platform == 'max')
-                <div id="max-desktop-back-button-container" class="text-center mt-10" style="display: none">
-                    <a id="max-desktop-back-button" href="" class="inline-block mx-auto rounded-md bg-indigo-50 px-3 py-2 text-sm font-semibold text-indigo-600 shadow-xs hover:bg-indigo-100 dark:bg-indigo-500/20 dark:text-indigo-400 dark:shadow-none dark:hover:bg-indigo-500/30">Вернуться назад</a>
-                </div>
-            @endif
 
             @php
                 $bscount = 0;
