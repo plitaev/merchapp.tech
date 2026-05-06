744884e49006a555fa76a04c5fa171a7edca0c53
Evgenii Plita <evgeniiplita@gmail.com>
2026-05-06 15:37:00 +0700

    MaxApp

--- app/Http/Controllers/Core/MiniAppPageController.php
+++ app/Http/Controllers/Core/MiniAppPageController.php
@@ -35,8 +35,6 @@ class MiniAppPageController
         $telegram_chat_id = (isset($_GET['telegram_chat_id'])?$_GET['telegram_chat_id']:0);
         $max_user_id = (isset($_GET['max_user_id'])?$_GET['max_user_id']:0);
 
-        return $mini_app_platform." | ".$telegram_chat_id." | ".$max_user_id;
-
         $mini_app = MiniApp::select('bot_id')->find($mini_app_page->mini_app_id);
 
         $bot_user = BotUser::with('bot')
