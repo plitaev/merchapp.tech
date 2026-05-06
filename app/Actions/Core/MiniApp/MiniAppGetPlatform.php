0e23faab22a1779ac4ced4e62d02440a3d5d34b0
Evgenii Plita <evgeniiplita@gmail.com>
2026-05-06 12:45:27 +0700

    MaxApp

--- app/Actions/Core/MiniApp/MiniAppGetPlatform.php
+++ app/Actions/Core/MiniApp/MiniAppGetPlatform.php
@@ -5,11 +5,11 @@ namespace App\Actions\Core\MiniApp;
 class MiniAppGetPlatform
 {
     public function handle() {
-        return implode(',', $_GET);
+
         if (isset($_GET['platform'])) {
 
             $platform = str_replace('?WebAppStartParam=', '', $_GET['platform']);
-            $A = json_decode($platform, true);
+            return $platform;
 
             $platform = (isset($A['platform'])?$A['platform']:'telegram');
         } else {
