838481a0560ea6828dbd5cc476ecbcea8b752b71
Evgenii Plita <evgeniiplita@gmail.com>
2026-05-06 16:12:40 +0700

    MaxApp

--- resources/views/core/mini-app/mini-app-player-page.blade.php
+++ resources/views/core/mini-app/mini-app-player-page.blade.php
@@ -35,22 +35,4 @@
         </script>
     @endif
 
-
-    <script src="/js/video.min.7.20.1.js"></script>
-    <script src="/js/videojs-http-streaming.2.14.2.js"></script>
-    <script src="/js/silvermine-videojs-quality-selector.min.js"></script>
-    <script src="/js/videojs-seek-buttons.min.js"></script>
-
-    <link rel='stylesheet' href="/css/video-js.css"/>
-    <link rel='stylesheet' href="/css/video-quality-selector.css"/>
-    <link rel='stylesheet' href="/css/vjs-svoiludi.css"/>
-    <link rel='stylesheet' href="/css/vjs-quality-selector.css">
-    <link rel='stylesheet' href="/css/videojs-seek-buttons.css">
-
-    <style>
-        .vjs-big-play-button{background: #b2b9e2 !important;}
-    </style>
-
-    i am player page
-
 @endsection
