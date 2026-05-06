00a93040c2e44058c59f07fa7e94fb31aa3c1a42
Evgenii Plita <evgeniiplita@gmail.com>
2026-05-06 18:14:25 +0700

    MaxApp

--- resources/views/core/mini-app/mini-app-video-list-page.blade.php
+++ resources/views/core/mini-app/mini-app-video-list-page.blade.php
@@ -60,8 +60,7 @@
                 });
 
                 if (app.platform == "desktop") {
-                    //document.getElementById('max-desktop-back-button-container').setAttribute('style', 'display:block');
-                    document.getElementById('max-desktop-back-button').setAttribute('href', "{{$mini_app_page->back_button_url}}?platform=max&max_user_id=" + id);
+                    document.getElementById('max-desktop-back-button').setAttribute('href', "");
                 }
 
                 @else
@@ -69,7 +68,6 @@
                 app.BackButton.hide();
 
                 if (app.platform == "desktop") {
-                    //document.getElementById('max-desktop-back-button-container').setAttribute('style', 'display:none');
                     document.getElementById('max-desktop-back-button').setAttribute('href', "");
                 }
 
