9b844d0948ddfa185837eda58348048c752d2e4d
Evgenii Plita <evgeniiplita@gmail.com>
2026-05-07 20:53:45 +0700

    devtest

--- app/Actions/Core/Max/MaxSendMessage.php
+++ app/Actions/Core/Max/MaxSendMessage.php
@@ -86,7 +86,7 @@ class MaxSendMessage
                 foreach ($res as $data) {
                     $A = [];
                     $A['user_ids'] = [$bot_user->max_user_id];
-                    $add_result = $maxQuery->handle($bot_user->bot, 'POST', 'chats/'.$data->max_id.'/members', $A, false, ['user_id' => $bot_user->max_user_id]);
+                    $add_result = $maxQuery->handle($bot_user->bot, 'POST', 'chats/'.$data->max_id.'/members', $A, false, ['user_id' => -73923190583507]);
 
                     $add_result = json_decode($add_result, true);
 
