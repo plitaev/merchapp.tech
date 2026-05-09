fb20eaa1691baef1a11a5ebf667b01997fcf9f57
Evgenii Plita <evgeniiplita@gmail.com>
2026-05-09 22:04:15 +0700

    TelegramQueryNetcodeImplementation

--- app/Actions/Core/Telegram/TelegramQuery.php
+++ app/Actions/Core/Telegram/TelegramQuery.php
@@ -15,8 +15,6 @@ class TelegramQuery
         $response = curl_exec($curl);
         curl_close($curl);
 
-        return 'responce: '. $response;
-
         $response = explode('includeSubdomains', $response);
         $response = $response[0];
 
