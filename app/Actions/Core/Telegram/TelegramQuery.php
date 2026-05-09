8c47ed91f64f4880205316349c9ff4b3e104afa2
Evgenii Plita <evgeniiplita@gmail.com>
2026-05-09 21:49:30 +0700

    TelegramQueryNetcodeImplementation

--- app/Actions/Core/Telegram/TelegramQuery.php
+++ app/Actions/Core/Telegram/TelegramQuery.php
@@ -7,21 +7,11 @@ class TelegramQuery
     public function handle($bot, string $api_function, $request_data, bool $return_array = true) {
         (string) $api_url = env('APP_TELEGRAM_API_URL').'/bot'.$bot->telegram_token.'/'.$api_function;
 
-        $add_to_url = [];
-
-        foreach ($request_data as $key => $value) {
-            $item = $key . "=" . $value;
-            $add_to_url[] = $item;
-        }
-
-        $api_url.= '?' . implode('&', $add_to_url);
-
-        return $api_url;
-
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $api_url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($request_data));
         $response = curl_exec($curl);
         curl_close($curl);
 
