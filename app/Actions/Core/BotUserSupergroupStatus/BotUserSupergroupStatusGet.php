7dfb1d19fe7fbaf258e06c3c0c5df8a88c6749b8
Evgenii Plita <evgeniiplita@gmail.com>
2026-05-09 22:45:04 +0700

    TelegramQueryNetcodeImplementation

--- app/Actions/Core/BotUserSupergroupStatus/BotUserSupergroupStatusGet.php
+++ app/Actions/Core/BotUserSupergroupStatus/BotUserSupergroupStatusGet.php
@@ -2,21 +2,20 @@
 
 namespace App\Actions\Core\BotUserSupergroupStatus;
 
-use Telegram\Bot\Api;
+use App\Actions\Core\Telegram\TelegramQuery;
 
-use App\Models\Core\BotUser;
 use App\Models\Core\BotUserSupergroupStatus;
 use App\Models\Core\TelegramSupergroup;
 
 class BotUserSupergroupStatusGet
 {
     public function handle($bot_user) {
-        $telegram = new Api($bot_user->bot->telegram_token);
+        $telegramQuery = new TelegramQuery();
 
         $supergroups = TelegramSupergroup::where('bot_id', $bot_user->bot_id)->get();
 
         foreach ($supergroups as $supergroup) {
-            $member = $telegram->getChatMember(['chat_id' => $supergroup->telegram_id, 'user_id' => $bot_user->telegram_chat_id]);
+            $member = $telegramQuery->handle($bot_user->bot, 'getChatMember', ['chat_id' => $supergroup->telegram_id, 'user_id' => $bot_user->telegram_chat_id]);
             BotUserSupergroupStatus::create(['bot_user_id' => $bot_user->id, 'telegram_supergroup_id' => $supergroup->id, 'status' => $member->status]);
         }
     }
--- app/Actions/Core/Telegram/TelegramChatJoinRequestCreate.php
+++ app/Actions/Core/Telegram/TelegramChatJoinRequestCreate.php
@@ -1,25 +1,25 @@
 <?php
 namespace App\Actions\Core\Telegram;
-use Telegram\Bot\Api;
 
 use App\Actions\Core\BotSendMessage\BotSendMessage;
+use App\Actions\Core\Telegram\TelegramQuery;
 
-use App\Models\Core\TelegramSupergroup;
 use App\Models\Core\TelegramChatJoinRequestLog;
+use App\Models\Core\TelegramSupergroup;
 
 class TelegramChatJoinRequestCreate
 {
     public function handle($bot_user, $json, int $status) {
         $botSendMessage = new BotSendMessage();
+        $telegramQuery = new TelegramQuery();
 
         $supergroup = TelegramSupergroup::where('telegram_id', $json['chat_join_request']['chat']['id'])->first();
-        $telegram = new Api($bot_user->bot->telegram_token);
 
         if ($status === 1 && $supergroup && $supergroup->decline_chat_join_request == 0) {
             $botSendMessage->handle($bot_user, 'SYS_APPROVE_CHAT_JOIN_REQUEST');
 
             try {
-                $result = $telegram->approveChatJoinRequest(['chat_id' => $json['chat_join_request']['chat']['id'], 'user_id' => $json['chat_join_request']['user_chat_id']]);
+                $result = $telegramQuery->handle($bot_user->bot, 'approveChatJoinRequest', ['chat_id' => $json['chat_join_request']['chat']['id'], 'user_id' => $json['chat_join_request']['user_chat_id']]);
             } catch (\Exception $exception) {
                 $result = '{"merchApp Error": "approveChatJoinRequest"}';
             }
@@ -28,7 +28,7 @@ class TelegramChatJoinRequestCreate
             $botSendMessage->handle($bot_user, 'SYS_DECLINE_CHAT_JOIN_REQUEST');
 
             try {
-                $result = $telegram->declineChatJoinRequest(['chat_id' => $json['chat_join_request']['chat']['id'], 'user_id' => $json['chat_join_request']['user_chat_id']]);
+                $result = $telegramQuery->handle($bot_user->bot, 'declineChatJoinRequest', ['chat_id' => $json['chat_join_request']['chat']['id'], 'user_id' => $json['chat_join_request']['user_chat_id']]);
             } catch (\Exception $exception) {
                 $result = '{"merchApp Error": "declineChatJoinRequest"}';
             }
