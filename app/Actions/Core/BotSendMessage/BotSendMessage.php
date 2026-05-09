a917861db1c0c8774093b97b455413ebd5b728e1
Evgenii Plita <evgeniiplita@gmail.com>
2026-05-09 22:16:26 +0700

    TelegramQueryNetcodeImplementation

--- app/Actions/Core/BotSendMessage/BotSendMessage.php
+++ app/Actions/Core/BotSendMessage/BotSendMessage.php
@@ -90,7 +90,7 @@ class BotSendMessage
                 }
 
                 if ($bot_user->telegram_chat_id && ($send_to == 'all' || $send_to == 'telegram')) {
-                    return $telegramSendMessage->handle($bot_user, $bot_message->id, $bot_message_appointment_name);
+                    $message = $telegramSendMessage->handle($bot_user, $bot_message->id, $bot_message_appointment_name);
                 }
 
                 //== Проверяем статусы, и ставим, если есть
