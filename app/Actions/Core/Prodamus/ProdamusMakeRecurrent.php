90ddbc078c02a891a17a40758045c777209e192c
Evgenii Plita <evgeniiplita@gmail.com>
2026-05-10 20:21:15 +0700

    devtest

--- app/Actions/Core/Prodamus/ProdamusMakeRecurrent.php
+++ app/Actions/Core/Prodamus/ProdamusMakeRecurrent.php
@@ -71,6 +71,7 @@ class ProdamusMakeRecurrent
 
         $responce_array = json_decode($responce, true);
 
+
         if ($responce_array['success'] == true) {
 
             BotUserBanSchedule::where('bot_user_id', $data->bot_user_id)->where('run_status', 0)->update(['run_status' => 3]);
