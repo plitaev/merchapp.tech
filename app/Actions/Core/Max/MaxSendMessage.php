b9ac6314205dfe3fcdcf9e1b4c668e0a4ee7c700
Evgenii Plita <evgeniiplita@gmail.com>
2026-05-11 12:35:16 +0700

    devtest

--- app/Actions/Core/Max/MaxSendMessage.php
+++ app/Actions/Core/Max/MaxSendMessage.php
@@ -92,7 +92,7 @@ class MaxSendMessage
 
                     if (isset($add_result['failed_user_details'][0]['error_code']) && $add_result['failed_user_details'][0]['error_code'] == 'add.participant.privacy') {
                         $botSendMessage->handle($bot_user, 'SYS_SUCCESS_MESSAGE_MAX_NEED_PRIVACY_CHANGE', 'max');
-                        die();
+                        //die();
                     }
                 }
 
