555f1198cf40cbb80141a0ca50e2734f9a6dadb1
Evgenii Plita <evgeniiplita@gmail.com>
2026-05-11 12:19:44 +0700

    devtest

--- app/Actions/Core/BotUser/BotUserRepeatRecurrent.php
+++ app/Actions/Core/BotUser/BotUserRepeatRecurrent.php
@@ -28,13 +28,13 @@ class BotUserRepeatRecurrent
 
             $one_day_product = Product::where('bot_id', $bot_user->bot_id)->where('days', '1')->first();
             if ($one_day_product) {
-                return '3';
                 $additional_data = [];
                 $additional_data['pay_system_id'] = 4;
 
                 $new_pay = $payCreateIntoBot->handle($bot_user, $one_day_product, $additional_data);
                 $payMakeSuccessful->handle('{"auto_generated_payment_finished": 1}', $new_pay->id, NULL, NULL, NULL);
                 $dateEnd->handle($bot_user, 'Y-m-d');
+                return '3';
             }
 
             $next_day = Carbon::now()->addDay()->format('Y-m-d');
