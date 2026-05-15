a298ff7ff20247150a95f84d25363d4d0028de76
AnastasiyaB1 <68107317+AnastasiyaB1@users.noreply.github.com>
2026-05-15 22:51:57 +0700

    Воссоздание по этим логам удаление елок

--- app/Actions/Project/ClubAccess/BotListenerPayCount.php
+++ app/Actions/Project/ClubAccess/BotListenerPayCount.php
@@ -8,19 +8,16 @@ use App\Models\Core\BotUser;
 
 class BotListenerPayCount
 {
-    public function handle(string $messenger, $webhook, $bot_user) {
+    public function handle($webhook, $bot_user) {
         $botSendMessage = new BotSendMessage();
         $botUserSetListener = new BotUserSetListener();
 
         if ($bot_user->listen_pay_count_status == 1) {
 
-            if (($messenger == 'telegram' && isset($webhook['message']['text'])) || ($messenger == 'max' && (isset($webhook['message']['body']['text'])))) {
+            if (isset($webhook['message']['text'])) {
 
-                if ($messenger == 'telegram') (string) $pay_count = $webhook['message']['text'];
-                if ($messenger == 'max') (string) $pay_count = $webhook['message']['body']['text'];
-
-                if (is_numeric($pay_count)) {
-                    BotUser::where('id', $bot_user->id)->update(['pay_count' => $pay_count]);
+                if (is_numeric($webhook['message']['text'])) {
+                    BotUser::where('id', $bot_user->id)->update(['pay_count' => $webhook['message']['text']]);
                     $botSendMessage->handle($bot_user, 'SYS_USER_ENTERED_PAY_COUNT');
                     $botUserSetListener->handle('listen_pay_count', 0, $bot_user->id);
 
