a1f64e9c333c6967d7280d8de995ecdaa867ddb1
Evgenii Plita <evgeniiplita@gmail.com>
2026-06-11 11:36:21 +0700

    BotUserUnbanFix

--- app/Filament/Resources/Bots/Pages/BotTelegramUnBanSchedules.php
+++ app/Filament/Resources/Bots/Pages/BotTelegramUnBanSchedules.php
@@ -153,7 +153,7 @@ class BotTelegramUnBanSchedules extends Page implements HasTable, HasForms
                             ])
                             ->searchable()
                             ->options(BotUser::where('bot_id', $this->bot_id)->get()->map(function ($bot_user) {
-                                return ['key' => $bot_user->id, 'value' => (isset($bot_user->first_name) && $bot_user->first_name!='none'?$bot_user->first_name:'')." ".(isset($bot_user->last_name) && $bot_user->last_name!='none'?$bot_user->last_name:'')." ".(isset($bot_user->username) && $bot_user->username!='none'?"(".$bot_user->username.")":'')];
+                                return ['key' => $bot_user->id, 'value' => (isset($bot_user->first_name) && $bot_user->first_name!='none'?$bot_user->first_name:'')." ".(isset($bot_user->last_name) && $bot_user->last_name!='none'?$bot_user->last_name:'')." ".(isset($bot_user->username) && $bot_user->username!='none'?"(".$bot_user->username.")":'')." ".(isset($bot_user->email) && $bot_user->email!='none'?"(".$bot_user->email.")":'')];
                             })->pluck('value', 'key')->toArray())
                             ->disabled(auth()->user()->hasPermissionTo('Update:TelegramSupergroup')?0:1),
 
