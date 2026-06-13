d25c7885a004830977c2b78ac1b62f694bb7804c
Evgenii Plita <evgeniiplita@gmail.com>
2026-06-14 00:51:54 +0700

    NavigatorUpdate

--- app/Http/Controllers/Core/MiniAppPageController.php
+++ app/Http/Controllers/Core/MiniAppPageController.php
@@ -47,7 +47,6 @@ class MiniAppPageController
         $pages = MiniAppPage::all();
         (string) $navigator = '';
         $navigator = $treeFindParentsDirectly->handle($pages, $mini_app_page->id, $navigator);
-        return $navigator;
 
         $telegram_chat_id = (isset($_GET['telegram_chat_id'])?$_GET['telegram_chat_id']:0);
         $max_user_id = (isset($_GET['max_user_id'])?$_GET['max_user_id']:0);
--- resources/views/core/mini-app/mini-app-video-list-page.blade.php
+++ resources/views/core/mini-app/mini-app-video-list-page.blade.php
@@ -5,7 +5,7 @@
 
 @section('content')
 
-    {{implode(', ', $navigator)}}
+    {{$navigator}}
 
     @if ($mini_app_platform == 'telegram')
         <script src="{{env('APP_URL')}}/js/telegram-web-app.js"></script>
