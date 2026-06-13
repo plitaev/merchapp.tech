64d34568a7f7b19dd453cf7829c85c6b76907645
Evgenii Plita <evgeniiplita@gmail.com>
2026-06-14 00:35:50 +0700

    NavigatorUpdate

--- app/Actions/Core/MiniAppPage/TreeFindParentsDirectly.php
+++ app/Actions/Core/MiniAppPage/TreeFindParentsDirectly.php
@@ -6,20 +6,19 @@ use App\Models\Core\MiniAppPage;
 
 class TreeFindParentsDirectly
 {
-    public function handle($items, int $id) {
-        $result = [];
+    public function handle($items, int $id, array $navigator) {
 
         foreach ($items as $item) {
 
             if ($item->id == $id && $item->back_button_url) {
                 $back_button_url = str_replace(env('APP_URL').'/', '', $item->back_button_url);
                 $parent_page = MiniAppPage::select('id', 'back_button_url')->where('url', $back_button_url)->first();
-                $result[] = $parent_page->id;
-                if ($parent_page->back_button_url) $result[] = $this->handle($items, $parent_page->id);
+                $navigator[] = $parent_page->id;
+                if ($parent_page->back_button_url) $navigator[] = $this->handle($items, $parent_page->id);
             }
 
         }
 
-        return $result;
+        return $navigator;
     }
 }
